d0dc8cb2bb77ce4e69ce95d202c96d9800e85f39
Boris Gurvich <unirgy@gmail.com>
2012-01-16 23:47:09 -0500

    realpath for buckyball lib

--- FCom/FCom.php
+++ FCom/FCom.php
@@ -4,7 +4,7 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL | E_STRICT);
 
-require __DIR__."/../lib/b/buckyball.php";
+require realpath(__DIR__."/../lib/b/buckyball.php");
 
 class FCom extends BClass
 {
