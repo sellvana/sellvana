36bb76b327bb3d654618d157a5010735bec0a2a1
Minu <minu@hummingray.com>
2012-01-19 01:48:28 -0500

    product view style

--- FCom/Catalog/Admin/views/products/view.php
+++ FCom/Catalog/Admin/views/products/view.php
@@ -1,1 +1,50 @@
-VIEW
+<header class="adm-page-title">
+	<span class="title">Edit Product</span>
+</header>
+<section class="adm-content-box info-view-mode">
+	<section class="adm-product-summary adm-section-group">
+		<button class="btn st2 sz2 btn-edit"><span>Edit</span></button>
+		<a href="#" class="product-image"><img src="" width="118" height="118" alt=""/></a>
+		<h1>Bur Block Autoclavable 8 Hole Blue Ec</h1>
+		<span class="manuf-name attr-item">Detsply</span>
+		<span class="manuf-sku attr-item">#123457890</span>
+	</section>
+	<div class="adm-content-inner">
+		<div class="adm-tabs-left-bg"></div>
+		<nav class="adm-tabs-left">
+			<ul>
+				<li class="active"><a href="#">General Info</a></li>
+				<li><a href="#">Attributes</a></li>
+				<li><a href="#">Related Products</a></li>
+				<li><a href="#">Family Products</a></li>
+				<li><a href="#">Similar Products</a></li>
+				<li><a href="#">Categories</a></li>
+				<li><a href="#">Attachments</a></li>
+				<li><a href="#">Images</a></li>
+				<li><a href="#">Vendors</a></li>
+				<li><a href="#">Product Reviews</a></li>
+				<li><a href="#">Promotions</a></li>
+			</ul>
+		</nav>
+		<section class="adm-tabs-content">
+			<form method="#" action="#" class="adm-section-group">
+				<fieldset>
+					<button class="btn st2 sz2 btn-edit"><span>Edit</span></button>
+					<ul class="form-list">
+						<li>
+							<h4 class="label">Short Description</h4>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vestibulum convallis varius. Donec et odio quis est blandit mattis.
+						</li>
+						<li>
+							<h4 class="label">Long Description</h4>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vestibulum convallis varius. Donec et odio quis est blandit mattis. Aliquam ac nisl magna, sit amet vestibulum ipsum. Vestibulum ultrices justo sagittis ante interdum volutpat. Curabitur ullamcorper, neque pulvinar commodo gravida, augue tellus interdum nulla, a pulvinar leo nisi ac nisl. Nullam bibendum luctus sem, eget interdum leo blandit auctor. Integer ullamcorper tellus non justo ultrices tempor. Vivamus eu augue justo. Suspendisse ut neque nec neque ultrices aliquam dictum sed orci.
+						</li>
+						<li>
+							<h4 class="label">Unit of Measures</h4>
+						</li>
+					</ul>
+				</fieldset>
+			</form>	
+		</section>
+	</div>
+</section>
