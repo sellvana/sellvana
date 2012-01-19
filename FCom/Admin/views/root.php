315fb66e9b012af1ec5f0b0f0ef0253e980daf55
Minu <minu@hummingray.com>
2012-01-19 00:30:47 -0500

    header styling

--- FCom/Admin/views/root.php
+++ FCom/Admin/views/root.php
@@ -15,12 +15,35 @@ window.appConfig = {
 			<span class="adm-logo">Denteva Admin</span>
 			<nav class="sup-links">
 				<ul>
-					<li class="sup-updates"><a href="#">Updates</a></li>
-					<li class="sup-shortcuts"><a href="#">Shortcuts</a></li>
-					<li class="sup-account"><a href="#">Scott Walsh</a></li>
-                    <li><a href="<?php echo BApp::m('FCom_Admin')->baseHref()?>/logout">Log Out</a></li>
+					<li class="sup-quicksearch"><a href="#"><span>Quicksearch</span></a>
+						<form action="#" method="post" class="sub-section">
+							<fieldset>
+								<ul class="form-list">
+									<li>
+										<select>
+											<option value="#">Customers</option>
+											<option value="#">Products</option>
+											<option value="#">Orders</option>
+										</select>
+									</li>
+									<li><input type="text" name=""/></li>
+								</ul>
+								<input type="submit" value="Search" class="btn st2 sz2"/>
+							</fieldset>
+						</form>
+					</li>
+					<li class="sup-shortcuts"><a href="#"><span>Shortcuts</span></a></li>
+					<li class="sup-updates"><a href="#"><span>Updates &nbsp;<em class="count">10</em></span></a></li>
+					<li class="sup-account"><a href="#"><span>Scott Walsh</span></a>
+						<ul class="sub-section">
+							<li><a href="">My Account</a></li>
+							<li><a href="">My Reports</a></li>
+							<li><a href="<?php echo BApp::m('FCom_Admin')->baseHref()?>/logout">Log Out</a></li>
+						</ul>
+					</li>
 				</ul>
 			</nav>
+			<strong class="adm-group-title">Catalog</strong>
 		</header>
 	    <section class="adm-nav-bg"></section>
         <nav class="adm-nav">
