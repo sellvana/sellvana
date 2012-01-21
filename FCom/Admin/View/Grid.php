7a4c8307b8d2d67e2f331936a588e1232106e73d
Minu <minu@hummingray.com>
2012-01-21 16:59:37 -0500

    custom test button

--- FCom/Admin/View/Grid.php
+++ FCom/Admin/View/Grid.php
@@ -61,6 +61,15 @@ class FCom_Admin_View_Grid extends BView
         }
         $html .= "<script>jQuery('#grid-{$id}')";
         foreach ($cfg as $k=>$opt) {
+            if (is_numeric($k)) {
+                $k = array_shift($opt);
+            }
+            if (!empty($opt['_pager'])) {
+                $localPagerId = $opt['_pager'];
+                unset($opt['_pager']);
+            } else {
+                $localPagerId = $pagerId;
+            }
             $opt = BUtil::toJavaScript($opt);
             switch ($k) {
                 case 'grid':
@@ -68,9 +77,10 @@ class FCom_Admin_View_Grid extends BView
                     break;
                 case 'navGrid':
                 case 'navButtonAdd':
-                    $html .= ".jqGrid('navGrid', '#{$pagerId}', {$opt})";
+                    $html .= ".jqGrid('{$k}', '#{$localPagerId}', {$opt})";
                     break;
-                default: $html .= ".jqGrid('{$k}', {$opt})";
+                default:
+                    $html .= ".jqGrid('{$k}', {$opt})";
             }
         }
         $html .= "</script>";
