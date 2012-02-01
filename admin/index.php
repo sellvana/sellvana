3beb30ba66a6c4ae84d293c8a14a899202959650
Minu <minu@hummingray.com>
2012-02-01 07:08:44 -0500

    generalized index.php

--- admin/index.php
+++ admin/index.php
@@ -8,10 +8,6 @@ $r = BRequest::i();
 BConfig::i()->add(array(
     'root_dir' => $rootDir,
     'web' => array('base_store' => BRequest::i()->webRoot(1)),
-    'modules' => array(
-        'Denteva_Admin' => array('run_level'=>BModule::REQUIRED),
-        'Denteva_Merge' => array('run_level'=>BModule::REQUIRED),
-    )
 ));
 
 BDebug::backtraceOn('MODULE UPDATE: FCom_Catalog');
