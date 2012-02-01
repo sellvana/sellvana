edcf6c01e46afc40dc0c65a8403518afbc09c4dc
Minu <minu@hummingray.com>
2012-02-01 07:08:44 -0500

    generalized index.php

--- index.php
+++ index.php
@@ -2,9 +2,4 @@
 
 require __DIR__.'/FCom/FCom.php';
 
-BConfig::i()->add(array('modules'=>array(
-    'Denteva_Catalog' => array('run_level'=>BModule::REQUIRED),
-    'Denteva_Discover' => array('run_level'=>BModule::REQUIRED),
-)));
-
 FCom::i()->run('FCom_Frontend');
