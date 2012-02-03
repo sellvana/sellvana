d40a92448cef611c4fc602b68698fde1de182c0d
Boris Gurvich <unirgy@gmail.com>
2012-02-03 08:47:40 -0500

    enabled Expires header in .htaccess, removed ", array())" abomination

--- FCom/Frontend/DefaultTheme.php
+++ FCom/Frontend/DefaultTheme.php
@@ -23,7 +23,7 @@ class FCom_Frontend_DefaultTheme extends BClass
                     //array('hook', 'nav', 'views'=>array('nav')),
                     array('hook', 'footer', 'views'=>array('footer')),
                     array('view', 'head', 'do'=>array(
-                        array('js', '{FCom_Core}/js/lib/head.min.js', array()),
+                        array('js', '{FCom_Core}/js/lib/head.min.js'),
                     )),
                 ),
 
