892c570368e1c2083414500bd8520ccbf47622e2
Yaroslav Vorozhko <vorozhko@gmail.com>
2012-04-06 18:30:00 +0200

    added indextank api; added first model; added service class; added sample report page at admin

--- FCom/FCom.php
+++ FCom/FCom.php
@@ -166,7 +166,6 @@ class FCom extends BClass
                 'version' => '0.1.0',
                 'root_dir' => 'Core',
                 'bootstrap' => array('file'=>'Core.php', 'callback'=>'FCom_Core::bootstrap'),
-                'depends' => array('FCom_Admin'),
                 'run_level' => BModule::REQUIRED,
                 'description' => "Base Fulleron classes and JS libraries",
             ))
