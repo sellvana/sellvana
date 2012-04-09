e4a44898ba1a8bc475bf087d6a962b572c8cb9e7
Yaroslav Vorozhko <vorozhko@gmail.com>
2012-04-09 22:12:34 +0200

    setup frontend

--- FCom/IndexTank/Frontend/views/indextank/product/list.php
+++ FCom/IndexTank/Frontend/views/indextank/product/list.php
@@ -4,15 +4,14 @@
 
 <? else: ?>
 
-    <?=$this->view('catalog/product/pager')->set('state', $this->products_data['state'])?>
-    <?=$this->view('catalog/compare/block')?>
+    <?=$this->view('indextank/product/pager')->set('state', $this->products_data['state'])?>
     <table class="product-list">
         <col width="30"/>
         <col width="60"/>
         <col/>
         <col width="180"/>
         <tbody>
-            <?=$this->view('catalog/product/rows')
+            <?=$this->view('indextank/product/rows')
                 ->set('products', $this->products_data['rows'])
                 ->set('category', $this->category) ?>
         </tbody>
