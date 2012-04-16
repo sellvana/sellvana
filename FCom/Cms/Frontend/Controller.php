ec0797ba8de5c45b3e8583556005d5704a4f2147
Boris Gurvich <unirgy@gmail.com>
2012-04-16 05:11:03 -0400

    fixing CMS routing

--- FCom/Cms/Frontend/Controller.php
+++ FCom/Cms/Frontend/Controller.php
@@ -2,21 +2,27 @@
 
 class FCom_Cms_Frontend_Controller extends FCom_Frontend_Controller_Abstract
 {
-    public function action_index()
+    public function action_page()
     {
-        $this->layout('base');
-
         $handle = BRequest::i()->params('page');
         $page = FCom_Cms_Model_Page::i()->load($handle, 'handle');
-        if ($page) {
-            $page->render();
+        if (!$page || !$page->validate()) {
+            $this->forward(true);
             return;
         }
+        $this->layout('base');
+        $page->render();
+    }
+
+    public function action_nav()
+    {
+        $handle = BRequest::i()->params('nav');
         $nav = FCom_Cms_Model_Nav::i()->load($handle, 'url_path');
-        if ($nav) {
-            $nav->render();
+        if (!$nav || !$nav->validate()) {
+            $this->forward(true);
             return;
         }
-        $this->forward(true);
+        $this->layout('base');
+        $nav->render();
     }
 }
---
+++ FCom/Core/js/core.js
@@ -0,0 +1,25 @@
+var FCom = {};
+
+FCom.tabs = function(options) {
+    var tabs = $(options.tabs);
+    var curLi = $(options.tabs+'[class=active]');
+    var curPane = $(options.panes+':not([hidden])');
+
+    $('a', tabs).click(function(ev) {
+        curLi.removeClass('active');
+        curPane.removeClass('active');
+        ev.stopPropagation();
+
+        var a = $(ev.currentTarget), li = a.parent('li');
+        if (curLi===li) {
+            return false;
+        }
+        var pane = $(a.attr('href'));
+        li.addClass('active');
+        pane.addClass('active');
+        curLi = li;
+        curPane = pane;
+        var tabId = a.attr('href').replace(/^#/,'');
+        return false;
+    });
+}
