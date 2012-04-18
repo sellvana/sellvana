8b288e112faa485db474e6dfc71bd2e6fd678fc9
Yaroslav Vorozhko <vorozhko@gmail.com>
2012-04-18 19:58:25 +0200

    added comments; added abstract index class; redecalred paginate

--- FCom/IndexTank/IndexTankAdmin.php
+++ FCom/IndexTank/IndexTankAdmin.php
@@ -2,25 +2,15 @@
 
 class FCom_IndexTank_Admin extends BClass
 {
+    /**
+     * Bootstrap IndexTank routes, events and layout for Admin part
+     */
     static public function bootstrap()
     {
         $module = BApp::m();
         $module->base_src .= '/Admin';
 
-        BGanon::i()->ready(function($args) {
-            $insert = '<button class="st1 sz2 btn" onclick="ajax_index_all_products();"><span>Index All Products</span></button>
-                <button class="st1 sz2 btn" onclick="ajax_products_clear_all();"><span>Clear Products Index</span></button>
-<script type="text/javascript">
-    function ajax_index_all_products() { $.ajax({ type: "GET", url: "'.BApp::href('indextank/products/index').'"})
-        .done(function( msg ) { alert( msg ); }); }
-    function ajax_products_clear_all() { $.ajax({ type: "DELETE", url: "'.BApp::href('indextank/products/index').'"})
-        .done(function( msg ) { alert( msg ); }); }
-</script>
-';
-            if (($el = BGanon::i()->find('header.adm-page-title div.btns-set', 0))) {
-                $el->setInnerText($insert.$el->getInnerText());
-            }
-        }, array('on_path'=>'/catalog/products'));
+        self::_initButtonsOnProductsPage();
 
         BFrontController::i()
             ->route('GET /indextank/dashboard', 'FCom_IndexTank_Admin_Controller.dashboard')
@@ -45,6 +35,11 @@ class FCom_IndexTank_Admin extends BClass
             ;
     }
 
+
+
+    /**
+     * Delete all indexed products
+     */
     static public function productsDeleteAll()
     {
         $orm = FCom_Catalog_Model_Product::i()->orm('p')->select('p.*');
@@ -65,6 +60,9 @@ class FCom_IndexTank_Admin extends BClass
         echo $counter . ' products deleted';
     }
 
+    /**
+     * Index all products
+     */
     static public function productsIndexAll()
     {
         $orm = FCom_Catalog_Model_Product::i()->orm('p')->select('p.*');
@@ -84,18 +82,33 @@ class FCom_IndexTank_Admin extends BClass
         echo $counter . ' products indexed';
     }
 
+    /**
+     * Catch event FCom_Catalog_Model_Product::afterSave
+     * to reindex given product
+     * @param array $args contain product model
+     */
     static public function onProductAfterSave($args)
     {
         $product = $args['model'];
         FCom_IndexTank_Index_Product::i()->add($product);
     }
 
+    /**
+     * Catch event FCom_Catalog_Model_Product::BeforeDelete
+     * to delete given product from index
+     * @param array $args contain product model
+     */
     static public function onProductBeforeDelete($args)
     {
         $product = $args['model'];
         FCom_IndexTank_Index_Product::i()->delete($product);
     }
 
+    /**
+     * Catch event FCom_Catalog_Model_Category::afterSave
+     * to update given category in products index
+     * @param array $args contain category model
+     */
     static public function onCategoryAfterSave($args)
     {
         $category = $args['model'];
@@ -105,6 +118,11 @@ class FCom_IndexTank_Admin extends BClass
         }
     }
 
+    /**
+     * Catch event FCom_Catalog_Model_Category::BeforeDelete
+     * to delete given category from products index
+     * @param array $args contain category model
+     */
     static public function onCategoryBeforeDelete($args)
     {
         $category = $args['model'];
@@ -114,6 +132,11 @@ class FCom_IndexTank_Admin extends BClass
         }
     }
 
+    /**
+     * Catch event FCom_CustomField_Model_Field::afterSave
+     * to update given custom field in products index
+     * @param array $args contain custom field model
+     */
     static public function onCustomFieldAfterSave($args)
     {
         $cf_model = $args['model'];
@@ -122,6 +145,12 @@ class FCom_IndexTank_Admin extends BClass
             FCom_IndexTank_Index_Product::i()->update_categories($product);
         }
     }
+
+    /**
+     * Catch event FCom_CustomField_Model_Field::BeforeDelete
+     * to delete given custom field from products index
+     * @param array $args contain custom field model
+     */
     static public function onCustomFieldBeforeDelete($args)
     {
         $cf_model = $args['model'];
@@ -132,6 +161,9 @@ class FCom_IndexTank_Admin extends BClass
     }
 
 
+    /**
+     * Itialized base layout, navigation links and page views scripts
+     */
     static public function layout()
     {
         $baseHref = BApp::href('indextank');
@@ -157,4 +189,21 @@ class FCom_IndexTank_Admin extends BClass
 
     }
 
+    static protected function _initButtonsOnProductsPage()
+    {
+        BGanon::i()->ready(function($args) {
+            $insert = '<button class="st1 sz2 btn" onclick="ajax_index_all_products();"><span>Index All Products</span></button>
+                <button class="st1 sz2 btn" onclick="ajax_products_clear_all();"><span>Clear Products Index</span></button>
+<script type="text/javascript">
+    function ajax_index_all_products() { $.ajax({ type: "GET", url: "'.BApp::href('indextank/products/index').'"})
+        .done(function( msg ) { alert( msg ); }); }
+    function ajax_products_clear_all() { $.ajax({ type: "DELETE", url: "'.BApp::href('indextank/products/index').'"})
+        .done(function( msg ) { alert( msg ); }); }
+</script>
+';
+            if (($el = BGanon::i()->find('header.adm-page-title div.btns-set', 0))) {
+                $el->setInnerText($insert.$el->getInnerText());
+            }
+        }, array('on_path'=>'/catalog/products'));
+    }
 }
