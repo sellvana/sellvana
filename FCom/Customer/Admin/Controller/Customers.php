cd10ec5c3f988d874b5b143720b922b1bf5c52f9
Boris Gurvich <unirgy@gmail.com>
2012-04-22 02:57:21 -0400

    customer grid config

--- FCom/Customer/Admin/Controller/Customers.php
+++ FCom/Customer/Admin/Controller/Customers.php
@@ -10,4 +10,15 @@ class FCom_Customer_Admin_Controller_Customers extends FCom_Admin_Controller_Abs
     protected $_modelClassName = 'FCom_Customer_Model_Customer';
     protected $_mainTableAlias = 'c';
 
+    public function gridConfig()
+    {
+        $config = parent::gridConfig();
+        $config['grid']['columns'] += array(
+            'firstname' => array('label'=>'First Name'),
+            'lastname' => array('label'=>'Last Name'),
+            'email' => array('label'=>'Email'),
+        );
+        $config['custom']['dblClickHref'] = BApp::href('customers/form/?id=');
+        return $config;
+    }
 }
