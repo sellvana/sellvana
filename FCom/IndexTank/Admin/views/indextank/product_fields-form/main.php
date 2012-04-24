8e0d2d14e8531f95d9bff54d7d5a4bc151d46919
Yaroslav Vorozhko <vorozhko@gmail.com>
2012-04-24 21:15:13 +0200

    made controller and views/filters work with new schema of indexden; added form for managing look and feel of filters

--- FCom/IndexTank/Admin/views/indextank/product_fields-form/main.php
+++ FCom/IndexTank/Admin/views/indextank/product_fields-form/main.php
@@ -2,29 +2,48 @@
 <fieldset class="adm-section-group">
     <ul class="form-list">
         <li>
-            <h4 class="label">Field</h4>
-            <input type="text" name="model[field_name]" value="<?php echo $this->q($m->field_name) ?>"/>
+            <h2 class="label">Field</h2>
+            <input type="text" readonly="true" name="model[field_name]" value="<?php echo $this->q($m->field_name) ?>"/>
         </li>
-        <?php if ($m->type == 'fulltext'):?>
+
+        <?php if ($m->facets):?>
         <li>
-            <h4 class="label">Priority</h4>
-            <input type="text" id="main-content" name="model[priority]" value="<?php echo $this->q($m->priority) ?>">
+            <h4 class="label">Label</h4>
+            <input type="text" name="model[field_nice_name]" value="<?php echo $this->q($m->field_nice_name) ?>"/>
         </li>
         <?php endif; ?>
-        <?php if ($m->type == 'category'):?>
+
+        <li>
+            <h4 class="label">Search</h4>
+            <input type="checkbox" name="model[search]" value="1" <?= $m->search ?'checked' : '' ?>/>
+        </li>
+        <li>
+            <h4 class="label">Facets</h4>
+            <input type="checkbox" name="model[facets]" value="1" <?= $m->facets ?'checked' : '' ?>/>
+        </li>
+
+        <?php if ($m->search):?>
+        <li>
+            <h4 class="label">Priority</h4>
+            <input type="text" size="3" id="main-content" name="model[priority]" value="<?php echo $this->q($m->priority) ?>">
+            (Default 1)
+        </li>
+        <?php endif; ?>
+
+        <?php if ($m->facets):?>
         <li>
             <h4 class="label">Display as</h4>
             <select name="model[show]">
-                <option <?=('checkbox' == $m->show || '' == $m->show)?'selected':''?> value="checkbox">Checkbox</option>
-                <option <?=('link' == $m->show)?'selected':''?> value="link">Link</option>
+                <option <?=('checkbox' == $m->show)?'selected':''?> value="checkbox">Checkbox</option>
+                <option <?=('link' == $m->show || '' == $m->show)?'selected':''?> value="link">Link</option>
             </select>
         </li>
 
         <li>
             <h4 class="label">Filter type</h4>
             <select name="model[filter]">
-                <option <?=('exclusive' == $m->filter || '' == $m->filter)?'selected':''?> value="exclusive">Exclusive</option>
-                <option <?=('inclusive' == $m->filter)?'selected':''?> value="inclusive">Inclusive</option>
+                <option <?=('exclusive' == $m->filter)?'selected':''?> value="exclusive">Exclusive</option>
+                <option <?=('inclusive' == $m->filter || '' == $m->filter)?'selected':''?> value="inclusive">Inclusive</option>
             </select>
         </li>
         <?php endif; ?>
