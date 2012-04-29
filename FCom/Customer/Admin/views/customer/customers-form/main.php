e40865d3989cca5487fe4e7b1921d201b333b4a3
Boris Gurvich <unirgy@gmail.com>
2012-04-28 23:08:18 -0700

    Moved ng-app from <html> to avoid angular.js conflicts

--- FCom/Customer/Admin/views/customer/customers-form/main.php
+++ FCom/Customer/Admin/views/customer/customers-form/main.php
@@ -1,12 +1,14 @@
 <?php $m = $this->model ?>
 <script>
+//var custModule = angular.module('custModule', []);
+
 function CustomerAddressesCtrl($scope) {
     $scope.addresses = <?=BUtil::toJson(BDb::many_as_array($m->addresses()))?>;
     $scope.countries = <?=BUtil::toJson(FCom_Geo_Model_Country::options())?>;
     $scope.regions = <?=BUtil::toJson(FCom_Geo_Model_Region::allOptions())?>;
 
     $scope.addAddress = function() {
-        $scope.addresses.push({edit_mode:true, customer_id:<?=$m->id?>});
+        $scope.addresses.push({edit_mode:true, customer_id:<?=$m->id?$m->id:'null'?>});
     }
 
     $scope.delAddress = function(addr) {
@@ -26,7 +28,8 @@ function CustomerAddressCtrl($scope) {
 <img src="<?=BUtil::gravatar($m->email)?>"/>
 <?=$this->q($m->firstname)?> <?=$this->q($m->lastname)?>
 
-<div ng-controller="CustomerAddressesCtrl">
+
+<div ng-app ng-controller="CustomerAddressesCtrl">
     <div ng-repeat="a in addresses">
         <a href ng-click="delAddress(a)">[X]</a>
         <label><input type="checkbox" ng-model="a.edit_mode"/> Edit</label>
