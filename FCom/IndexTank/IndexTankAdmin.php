86bb91172f14a7eefeb227b1b554e5848364b77c
Yaroslav Vorozhko <vorozhko@gmail.com>
2012-05-10 19:15:24 +0200

    init buttons for indextank using events

--- FCom/IndexTank/IndexTankAdmin.php
+++ FCom/IndexTank/IndexTankAdmin.php
@@ -10,13 +10,6 @@ class FCom_IndexTank_Admin extends BClass
         $module = BApp::m();
         $module->base_src .= '/Admin';
 
-
-        BGanon::i()->ready('FCom_IndexTank_Admin::initIndexButtons', array('on_path'=>array(
-            '/catalog/products',
-            '/indextank/product_fields',
-            '/indextank/product_functions',
-        )));
-
         BFrontController::i()
             ->route('GET /indextank/product_fields', 'FCom_IndexTank_Admin_Controller_ProductFields.index')
             ->route('GET|POST /indextank/product_fields/.action', 'FCom_IndexTank_Admin_Controller_ProductFields')
@@ -326,26 +319,5 @@ class FCom_IndexTank_Admin extends BClass
             ));
     }
 
-    public static function initIndexButtons($args)
-    {
-        try {
-            FCom_IndexTank_Index_Product::i()->status();
-        } catch (Exception $e){
-            return false;
-        }
-
-        $insert = '<button class="st1 sz2 btn" onclick="ajax_index_all_products();"><span>Index All Products</span></button>
-            <button class="st1 sz2 btn" onclick="ajax_products_clear_all();"><span>Clear Products Index</span></button>
-<script type="text/javascript">
-function ajax_index_all_products() { $.ajax({ type: "GET", url: "'.BApp::href('indextank/products/index').'"})
-    .done(function( msg ) { alert( msg ); }); }
-function ajax_products_clear_all() { $.ajax({ type: "DELETE", url: "'.BApp::href('indextank/products/index').'"})
-    .done(function( msg ) { alert( msg ); }); }
-</script>
-';
-        if (($el = BGanon::i()->find('header.adm-page-title div.btns-set', 0))) {
-            $el->setInnerText($insert.$el->getInnerText());
-        }
-
-    }
+    
 }
