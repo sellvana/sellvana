0f9b3c57d002a7493709384f3ae874df026711cf
Yaroslav Vorozhko <vorozhko@gmail.com>
2012-05-10 18:40:41 +0200

    added calendar for date and datetime fields

--- FCom/CustomField/Admin/views/customfields/products/fields-partial.php
+++ FCom/CustomField/Admin/views/customfields/products/fields-partial.php
@@ -7,19 +7,40 @@ $m = $this->model;
 <?php if(!empty($this->fields)):?>
     <?php foreach($this->fields as $field):?>
         <legend><?=$field->frontend_label?></legend>
+
+
         <?php if($field->admin_input_type == 'select'):?>
             <select name="model[<?= $field->field_code ?>]">
                 <?php foreach($this->fields_options[$field->id] as $field_option):?>
-                    <option value="<?=$field_option->label?>" <?= ($field_option->label == $m->{$field->field_code})?'selected':''?> ><?=$field_option->label?></option>
+                    <option value="<?=$field_option->label?>"
+                        <?= ($field_option->label == $m->{$field->field_code})?'selected':''?> ><?=$field_option->label?></option>
                 <?php endforeach; ?>
             </select>
+
+
         <?php elseif($field->admin_input_type == 'text'):?>
-            <input type="text" name="model[<?= $field->field_code ?>]" value="<?=$m->{$field->field_code}?>" />
+            <?php if($field->table_field_type == 'date' || $field->table_field_type == 'datetime'):?>
+                <div class="datepicker_wrapper">
+                    <input type="text" name="model[<?= $field->field_code ?>]" value="<?=$m->{$field->field_code}?>" id="datepicker" />
+                </div>
+                <script>
+                    $(function() {
+                        $( "#datepicker" ).datepicker({ dateFormat: "yy-mm-dd", constrainInput: false });
+                    });
+                </script>
+            <?php else: ?>
+                <input type="text" name="model[<?= $field->field_code ?>]" value="<?=$m->{$field->field_code}?>" />
+            <?php endif; ?>
+
+
         <?php elseif($field->admin_input_type == 'textarea'):?>
             <textarea name="model[<?= $field->field_code ?>]" ><?=$m->{$field->field_code}?></textarea>
+
+
         <?php elseif($field->admin_input_type == 'boolean'):?>
             <input type="hidden" name="model[<?= $field->field_code ?>]" value="0" />
-            <input type="checkbox" name="model[<?= $field->field_code ?>]" value="1" />
+            <input type="checkbox" name="model[<?= $field->field_code ?>]" value="1"
+                <?= (!empty($m->{$field->field_code}))?'checked':''?> />
         <?php endif; ?>
     <?php endforeach; ?>
 <?php endif; ?>
