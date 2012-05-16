f36e47daa6818aa4f70836802222e915948ff3c6
Boris Gurvich <unirgy@gmail.com>
2012-05-16 05:42:11 -0400

    fixed ORIG_PATH_INFO; BModel::dirty_fields()

--- FCom/buckyball/com/controller.php
+++ FCom/buckyball/com/controller.php
@@ -238,11 +238,12 @@ class BRequest extends BClass
     {
 #echo "<pre>"; print_r($_SERVER); exit;
         return !empty($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] :
-            (!empty($_SERVER['ORIG_PATH_INFO']) ? $_SERVER['ORIG_PATH_INFO'] :
+            (!empty($_SERVER['ORIG_PATH_INFO']) ? $_SERVER['ORIG_PATH_INFO'] : '/');
+            /*
                 (!empty($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] :
                     (!empty($_SERVER['SERVER_URL']) ? $_SERVER['SERVER_URL'] : '/')
                 )
-            );
+            );*/
     }
 
     /**
