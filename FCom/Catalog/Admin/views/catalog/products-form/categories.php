8e37a446b2ea2a9c37d219ee2733cf1aba95805f
Yaroslav Vorozhko <vorozhko@gmail.com>
2012-05-16 20:43:26 +0200

    add/delete categories for product using jstree

--- FCom/Catalog/Admin/views/catalog/products-form/categories.php
+++ FCom/Catalog/Admin/views/catalog/products-form/categories.php
@@ -1,6 +1,7 @@
 <?php
 $prodCtrl = FCom_Catalog_Admin_Controller_Products::i();
 ?>
+<div id="categories_unset"></div>
 <div id="categories"></div>
 <script>
 head(function() {
@@ -12,24 +13,24 @@ head(function() {
             checked_parent_open:true,
             real_checkboxes:true,
             two_state: true
-        }/*,
-        initially_open: <?php echo BUtil::toJson(array('check_3', 'check_2','check_5','check_7',)) ?>*/
+        },
+        initially_open: <?=$prodCtrl->linked_categories_data($this->model) ?>
     });
     $('#categories').bind('loaded.jstree', function(event, data) {
-        var checked = <?=$prodCtrl->linked_categories_data() ?>;
+        var checked = <?=$prodCtrl->linked_categories_data($this->model) ?>;
         for (var id in checked) {
-            console.log('check ' + checked[id]);
+            jQuery("<input>").attr("type", "hidden").attr("name", checked[id]).val(0).appendTo('#categories_unset');
             data.inst.check_node('#'+checked[id]);
         }
     });
     $('#categories').bind('after_open.jstree', function(event, data) {
-        var checked = <?=$prodCtrl->linked_categories_data() ?>;
+        var checked = <?=$prodCtrl->linked_categories_data($this->model) ?>;
         for (var id in checked) {
-            console.log('check ' + checked[id]);
             data.inst.check_node('#'+checked[id]);
         }
     });
 
     $('#categories').css({overflowY:'auto'}).resizeWithWindow({initBy:'.adm-content-box'});
 })
+
 </script>
