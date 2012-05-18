123c85b48f58ab1819f14d34b23b02e25bb20924
Boris Gurvich <unirgy@gmail.com>
2012-05-18 19:34:52 -0400

    fix for tests entry point;

--- tests/index.php
+++ tests/index.php
@@ -4,6 +4,7 @@
 
 require_once dirname(__DIR__).'/FCom/Core/Core.php';
 
+BConfig::i()->set('fs/root_dir', dirname(__DIR__));
 FCom_Core::i()->init('FCom_Tests');
 BModuleRegistry::i()->bootstrap();
 
