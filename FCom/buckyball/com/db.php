c346da91947fd855c6fa435356e7294d0923042e
Yaroslav Vorozhko <vorozhko@gmail.com>
2012-05-18 19:52:14 +0200

    fixed total count

--- FCom/buckyball/com/db.php
+++ FCom/buckyball/com/db.php
@@ -1080,6 +1080,7 @@ exit;
             'rs' => !empty($r['rs']) ? $r['rs'] : null,
             'rc' => !empty($r['rc']) ? $r['rc'] : null,
             'q'  => !empty($r['q'])  ? $r['q'] : null,
+            'c'  => !empty($r['c'])  ? $r['c'] : null,
         );
 #print_r($r); print_r($d); print_r($s); exit;
         $s['sc'] = $s['s'].'|'.$s['sd']; // sort combined for state
