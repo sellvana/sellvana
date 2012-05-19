56355fd1520d79917209d8582285fc0c7283089f
Boris Gurvich <unirgy@gmail.com>
2012-05-18 21:16:51 -0400

    added category edit > products tab

--- FCom/Catalog/Admin/Controller/Categories.php
+++ FCom/Catalog/Admin/Controller/Categories.php
@@ -9,4 +9,44 @@ class FCom_Catalog_Admin_Controller_Categories extends FCom_Admin_Controller_Abs
     protected $_formLayoutName = '/catalog/categories/tree_form';
     protected $_formViewName = 'catalog/categories-tree-form';
 
+
+    public function categoryProductGridConfig($model)
+    {
+        $orm = FCom_Catalog_Model_Product::i()->orm()->table_alias('p')
+            ->select(array('p.id', 'p.product_name', 'p.manuf_sku'))
+            ->join('FCom_Catalog_Model_CategoryProduct', array('cp.category_id','=','p.id'), 'cp')
+            ->where('cp.category_id', $model ? $model->id : 0)
+        ;
+
+        BPubSub::i()->fire(__METHOD__.'.orm', array('orm'=>$orm));
+
+        $config = array(
+            'grid' => array(
+                'id'            => 'category_products',
+                'data'          => BDb::many_as_array($orm->find_many()),
+                'datatype'      => 'local',
+                'caption'       => 'Category Products',
+                'colModel'      => array(
+                    array('name'=>'id', 'label'=>'ID', 'index'=>'p.id', 'width'=>40, 'hidden'=>true),
+                    array('name'=>'product_name', 'label'=>'Name', 'index'=>'product_name', 'width'=>250),
+                    array('name'=>'manuf_sku', 'label'=>'Mfr Part #', 'index'=>'manuf_sku', 'width'=>70),
+                ),
+                'rowNum'        => 10,
+                'sortname'      => 'p.product_name',
+                'sortorder'     => 'asc',
+                'autowidth'     => false,
+                'multiselect'   => true,
+                'shrinkToFit' => true,
+                'forceFit' => true,
+            ),
+            'navGrid' => array('add'=>false, 'edit'=>false, 'search'=>false, 'del'=>false, 'refresh'=>false),
+            array('navButtonAdd', 'caption' => 'Add', 'buttonicon'=>'ui-icon-plus', 'title' => 'Add Products'),
+            array('navButtonAdd', 'caption' => 'Remove', 'buttonicon'=>'ui-icon-trash', 'title' => 'Remove Products'),
+        );
+
+        BPubSub::i()->fire(__METHOD__.'.config', array('config'=>&$config));
+
+        return $config;
+    }
+
 }
