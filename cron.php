66265c3106600e1766005c8a9ff749637403f8ee
xp <xp@unirgy.com>
2012-05-23 19:16:21 -0400

    fixed index.local.php calls

--- cron.php
+++ cron.php
@@ -4,8 +4,8 @@ require_once __DIR__.'/FCom/Core/Core.php';
 
 BConfig::i()->set('cookie/session_disable', true);
 
-if (file_exists(__DIR__.'cron.local.php')) {
-    require_once __DIR__.'cron.local.php';
+if (file_exists(__DIR__.'/cron.local.php')) {
+    require_once __DIR__.'/cron.local.php';
 }
 FCom_Core::i()->run('FCom_Cron');
 
