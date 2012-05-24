238588648b09d205e37ac9fd40830b16f67f0a2e
xp <xp@unirgy.com>
2012-05-23 21:20:45 -0400

    restored IndexTank version

--- FCom/manifest.php
+++ FCom/manifest.php
@@ -243,7 +243,7 @@
         ),
         // IndexTank integration
         'FCom_IndexTank' => array(
-            'version' => '0.1.0',
+            'version' => '0.1.2',
             'root_dir' => 'IndexTank',
             'depends' => array('FCom_Core'),
             'description' => "IndexTank API integration",
