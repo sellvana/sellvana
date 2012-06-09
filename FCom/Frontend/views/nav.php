7dda83825721853768bd7382800c767a3a6fe425
Yaroslav Vorozhko <vorozhko@gmail.com>
2012-06-09 15:16:13 +0200

    decouple cms/nav and category/nav from global header

--- FCom/Frontend/views/nav.php
+++ FCom/Frontend/views/nav.php
@@ -1,7 +1,7 @@
 <?php if (BConfig::i()->get('modules/FCom_Frontend/nav_top/type') == 'cms'): ?>
-                        <?=$this->view('cms/nav')->set('root_id', BConfig::i()->get('modules/FCom_Frontend/nav_top/root_cms'))?>
-                    <?php elseif (BConfig::i()->get('modules/FCom_Frontend/nav_top/type') == 'categories') :?>
-                        <?=$this->view('category/nav')->set('root_id', BConfig::i()->get('modules/FCom_Frontend/nav_top/root_category'))?>
-                    <?php else: ?>
-                        Custom menu here
-                    <?php endif; ?>
+    <?=$this->view('cms/nav')->set('root_id', BConfig::i()->get('modules/FCom_Frontend/nav_top/root_cms'))?>
+<?php elseif (BConfig::i()->get('modules/FCom_Frontend/nav_top/type') == 'categories') :?>
+    <?=$this->view('catalog/category/nav')->set('root_id', BConfig::i()->get('modules/FCom_Frontend/nav_top/root_category'))?>
+<?php else: ?>
+    <li>Custom menu</li>
+<?php endif; ?>
