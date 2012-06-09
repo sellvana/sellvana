351a4906ced23cf78946ed87a6efda27206b5808
Yaroslav Vorozhko <vorozhko@gmail.com>
2012-06-09 15:11:01 +0200

    decouple cart from global header; setup view injection

---
+++ FCom/Checkout/Frontend/views/cart/header.php
@@ -0,0 +1,2 @@
+<li class="header-sup-cart"><a href="<?=BApp::href('cart')?>">Your Cart <span class="count">(<?=$this->cartItemNum?>)</span>
+| Total: $<?=$this->cartItemPrice?></a></li>
--- FCom/Frontend/views/header.php
+++ FCom/Frontend/views/header.php
@@ -1,13 +1,3 @@
-<?php
-//TODO: check that cart module is loaded
-$cart = FCom_Checkout_Model_Cart::sessionCart();
-$itemNum = 0;
-$itemPrice = 0;
-if($cart){
-    $itemPrice = round($cart->subtotal,2);
-    $itemNum = ceil($cart->item_num);
-}
-?>
 <header class="header">
     <div class="header-top">
         <strong class="logo">Fulleron</strong>
@@ -20,7 +10,7 @@ if($cart){
                     <li class="header-sup-signin">Hello there! <strong><a href="<?=BApp::href('login')?>">Sign in</a></strong></li>
                     <li class="header-sup-wishlist"><a href="<?=BApp::href('customer/register')?>">Sign up</a></li>
                 <?php endif; ?>
-                <li class="header-sup-cart"><a href="<?=BApp::href('cart')?>">Your Cart <span class="count">(<?=$itemNum?>)</span> | Total: $<?=$itemPrice?></a></li>
+                <?=$this->view('cart/header')?>
                 <li class="header-sup-wishlist"><a href="<?=BApp::href('wishlist')?>">Your Wishlist</a></li>
             </ul>
         </nav>
