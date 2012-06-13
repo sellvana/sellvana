1fe808e8745c2443d74a208dd76681917eb0c39c
Yaroslav Vorozhko <vorozhko@gmail.com>
2012-06-13 19:51:40 +0200

    added upgrade to cart

--- FCom/Checkout/Migrate.php
+++ FCom/Checkout/Migrate.php
@@ -6,17 +6,13 @@ class FCom_Checkout_Migrate extends BClass
         BMigrate::install('0.1.0', array($this, 'install'));
         BMigrate::upgrade('0.1.0', '0.1.1', array($this, 'upgrade_0_1_1'));
         BMigrate::upgrade('0.1.1', '0.1.2', array($this, 'upgrade_0_1_2'));
+        BMigrate::upgrade('0.1.2', '0.1.3', array($this, 'upgrade_0_1_3'));
     }
 
     public function install()
     {
         FCom_Checkout_Model_Cart::install();
         FCom_Checkout_Model_CartItem::install();
-    }
-
-    public function upgrade_0_1_4()
-    {
-        FCom_Checkout_Model_Cart::upgrade_0_1_4();
     }
 
     public function upgrade_0_1_3()
