5175ec91613205a0757c838a1e693ba1bc914d06
Yaroslav Vorozhko <vorozhko@gmail.com>
2012-06-20 15:36:34 +0200

    show custom fields on fronend

--- FCom/CustomField/CustomFieldFrontend.php
+++ FCom/CustomField/CustomFieldFrontend.php
@@ -5,5 +5,7 @@ class FCom_CustomField_Frontend extends BClass
     public static function bootstrap()
     {
         FCom_CustomField_Common::bootstrap();
+
+        BLayout::i()->addAllViews('Frontend/views');
     }
 }
