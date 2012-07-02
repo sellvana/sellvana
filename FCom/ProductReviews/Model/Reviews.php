f64b22db2bec3861e73c1bb065800378d39e7652
Yaroslav Vorozhko <vorozhko@gmail.com>
2012-07-02 12:43:26 +0200

    added migration script

--- FCom/ProductReviews/Model/Reviews.php
+++ FCom/ProductReviews/Model/Reviews.php
@@ -26,7 +26,7 @@ class FCom_ProductReviews_Model_Reviews extends FCom_Core_Model_Abstract
         $this->save();
     }
 
-    public static function install()
+    public function install()
     {
         BDb::run("
 CREATE TABLE IF NOT EXISTS ".static::table()." (
