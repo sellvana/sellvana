48b99c83575d35c91edda79ac0edde97e3633fe2
Yaroslav Vorozhko <vorozhko@gmail.com>
2012-07-20 15:39:18 +0200

    added support of different file types like: json and php

--- FCom/Disqus/DisqusFrontend.php
+++ FCom/Disqus/DisqusFrontend.php
@@ -7,7 +7,7 @@ class FCom_Disqus_Frontend extends BClass
         BLayout::i()->addAllViews('Frontend/views');
         BPubSub::i()->on('BLayout::theme.load.after', 'FCom_Disqus_Frontend::layout');
         setLocale(LC_ALL, 'ru_RU.UTF-8');
-        BLocale::addTranslationsFile('tr.csv');
+        BLocale::addTranslationsFile('tr.json');
     }
 
     static public function layout()
