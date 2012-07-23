1f1484cd6569dd4a80284c4db81a68e8bd90285e
Yaroslav Vorozhko <vorozhko@gmail.com>
2012-07-23 14:25:38 +0200

    added ability to edit translations

--- FCom/Translation/Admin/Controller.php
+++ FCom/Translation/Admin/Controller.php
@@ -34,7 +34,7 @@ class FCom_Translation_Admin_Controller extends FCom_Admin_Controller_Abstract_G
         foreach($modules as $module){
             if (!empty($module->translations)) {
                 foreach($module->translations as $trfile) {
-                    $data[] = array('module' => $module->name, 'file' => $trfile);
+                    $data[] = array('module' => $module->name, 'file' => $trfile, 'id'=>$module->name.'/'.$trfile);
                 }
             }
         }
@@ -43,23 +43,69 @@ class FCom_Translation_Admin_Controller extends FCom_Admin_Controller_Abstract_G
         $config['grid']['data'] = $data;
         return $config;
     }
-/*
+
+    public function action_form()
+    {
+        $id = BRequest::i()->params('id', true);
+        list($module, $file) = explode("/", $id);
+
+        if (!$file) {
+            BDebug::error('Invalid Filename: '.$id);
+        }
+        $moduleClass = BApp::m($module);
+        $filename = $moduleClass->baseDir().'/'.$file;
+
+        $model = new stdClass();
+        $model->id = $id;
+        $model->source = file_get_contents($filename);
+        $view = $this->view($this->_formViewName)->set('model', $model);
+        $this->formViewBefore(array('view'=>$view, 'model'=>$model));
+        $this->layout($this->_formLayoutName);
+        $this->processFormTabs($view, $model, 'edit');
+    }
+
     public function formViewBefore($args)
     {
-
-        parent::formViewBefore($args);
         $m = $args['model'];
         $args['view']->set(array(
-            'title' => $m->id ? 'Edit Product Field: '.$m->field_nice_name : 'Create New Product Field',
+            'form_id' => BLocale::transliterate($this->_formLayoutName),
+            'form_url' => BApp::href($this->_formHref).'?id='.$m->id,
+            'actions' => array(
+                'back' => '<button type="button" class="st3 sz2 btn" onclick="location.href=\''.BApp::href($this->_gridHref).'\'"><span>Back to list</span></button>',
+                'save' => '<button type="submit" class="st1 sz2 btn" onclick="return adminForm.saveAll(this)"><span>Save</span></button>',
+            ),
         ));
+        BPubSub::i()->fire(static::$_origClass.'::formViewBefore', $args);
     }
 
     public function action_form__POST()
     {
-        $r = BRequest::i();
-        $class = $this->_modelClass;
-        $id = $r->params('id', true);
+        if (empty($_POST)) {
+            return;
+        }
+        $id = $_POST['file'];
 
+        list($module, $file) = explode("/", $id);
+
+        if (!$file) {
+            BDebug::error('Invalid Filename: '.$id);
+        }
+        $moduleClass = BApp::m($module);
+        if (is_object($moduleClass)) {
+            BDebug::error('Invalid Module name: '.$id);
+        }
+
+        $filename = $moduleClass->baseDir().'/'.$file;
+
+        if (!is_writable($filename)) {
+            BDebug::error('Not writeable filename: '.$filename);
+        }
+
+        if (!empty($_POST['source'])) {
+            file_put_contents($filename, $_POST['source']);
+        }
+
+        BResponse::i()->redirect(BApp::href($this->_gridHref));
     }
-*/
+
 }
--- FCom/manifest.php
+++ FCom/manifest.php
@@ -296,7 +296,7 @@
             'version' => '0.1.0',
             'root_dir' => 'Disqus',
             'depends' => array('FCom_Core'),
-            'translations' => array('tr.csv'),
+            'translations' => array('tr.json', 'tr.csv', 'tr.php'),
             'description' => "Disqus comments",
             'bootstrap' => array('file'=>'DisqusFrontend.php', 'callback'=>'FCom_Disqus_Frontend::bootstrap'),
             'areas' => array(
