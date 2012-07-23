ae03d6c38c4d260701910dcc130082a6ea30f017
Yaroslav Vorozhko <vorozhko@gmail.com>
2012-07-23 15:37:17 +0200

    load language file based on locale settings

--- FCom/Disqus/tr.php
+++
@@ -1,3 +0,0 @@
-<?php return array('Comments' => 'PHP комментарии2',
-'posts' => 'Посты 2',
-'days' => 'Дни 2');
--- FCom/manifest.php
+++ FCom/manifest.php
@@ -296,7 +296,7 @@
             'version' => '0.1.0',
             'root_dir' => 'Disqus',
             'depends' => array('FCom_Core'),
-            'translations' => array('tr.json', 'tr.csv', 'tr.php'),
+            'translations' => array('ru_RU.UTF-8' => 'ru.json', 'es_ES.UTF-8' => 'es.csv', 'gr_GR.UTF-8' => 'gr.php'),
             'description' => "Disqus comments",
             'bootstrap' => array('file'=>'DisqusFrontend.php', 'callback'=>'FCom_Disqus_Frontend::bootstrap'),
             'areas' => array(
