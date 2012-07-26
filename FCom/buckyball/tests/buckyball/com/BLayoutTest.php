c1cfc24f294aded6c374fdf3191a9d8242ca5b48
Yaroslav Vorozhko <vorozhko@gmail.com>
2012-07-26 14:21:57 +0200

    integrated BB tests into Admin

--- FCom/buckyball/tests/buckyball/com/BLayoutTest.php
+++ FCom/buckyball/tests/buckyball/com/BLayoutTest.php
@@ -2,15 +2,6 @@
 
 class BLayout_Test extends PHPUnit_Framework_TestCase
 {
-    public function SetUp()
-    {
-        BLayout::i()->unsetInstance();
-    }
-
-    public function tearDown()
-    {
-        BLayout::i()->unsetInstance();
-    }
 
     public function testViewRootDirSetGet()
     {
