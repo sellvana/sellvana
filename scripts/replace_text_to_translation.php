3361e04318077893bb5a604ad0038ed583957588
Yaroslav Vorozhko <vorozhko@gmail.com>
2012-07-31 15:58:00 +0200

    translation scripts

--- scripts/replace_text_to_translation.php
+++ scripts/replace_text_to_translation.php
@@ -5,9 +5,20 @@ require_once __DIR_.'/../tests/index.php';
 $content = file_get_contents("/tmp/latest.json");
 $data = json_decode($content);
 
-foreach ($data as $line) {
-    $filename = $line[0];
-    $line = $line[1];
-    $pattern = $line[4];
-    $replace = $line[3];
+foreach ($data as $lineData) {
+    $filename = $lineData[0];
+    $line = $lineData[1];
+    $pattern = $lineData[4];
+    $replace = $lineData[3];
+
+    $cont = file_get_contents($filename);
+    $cont = str_replace($pattern, $replace, $cont);
+    file_put_contents($filename, $cont);
+    /*
+    $file = file($filename);
+    $file[$line] = $replace;
+    print_r($lineData);
+    print_r($file);exit;
+     *
+     */
 }
