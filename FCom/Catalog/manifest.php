19b04da4eb0e17c1841a0cc5f2345d57ab5346ac
Yaroslav Vorozhko <vorozhko@gmail.com>
2012-08-10 11:08:32 +0200

    added manifest to each module

---
+++ FCom/Catalog/manifest.php
@@ -0,0 +1,26 @@
+<?php return array(
+    'modules' => array(
+        // catalog views and controllers
+        'FCom_Catalog' => array(
+            'version' => '0.1.2',
+            'root_dir' => '',
+            'depends' => array('FCom_Core'),
+            'translations' => array('de' => 'de.csv'),
+            'tests' => "FCom_Catalog_Tests_AllTests",
+            'description' => "Categories and products management, admin and frontend",
+            'migrate' => 'FCom_Catalog_Migrate',
+            //'bootstrap' => array('file'=>'CatalogFrontend.php', 'callback'=>'FCom_Catalog_Frontend::bootstrap'),
+            'areas' => array(
+                'FCom_Admin' => array(
+                    'bootstrap' => array('file'=>'CatalogAdmin.php', 'callback'=>'FCom_Catalog_Admin::bootstrap'),
+                ),
+                'FCom_Frontend' => array(
+                    'bootstrap' => array('file'=>'CatalogFrontend.php', 'callback'=>'FCom_Catalog_Frontend::bootstrap'),
+                ),
+            ),
+        ),
+
+
+
+    ),
+);
