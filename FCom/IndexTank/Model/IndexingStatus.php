9edf2000411aefdb015d565fb65b35f9efa7afa2
Yaroslav Vorozhko <vorozhko@gmail.com>
2012-08-20 15:00:47 +0200

    improved indexing control panel statuses

--- FCom/IndexTank/Model/IndexingStatus.php
+++ FCom/IndexTank/Model/IndexingStatus.php
@@ -34,4 +34,21 @@ class FCom_IndexTank_Model_IndexingStatus extends FCom_Core_Model_Abstract
         $indexingStatus->save();
         return $indexingStatus;
     }
+
+    public function updateInfoStatus()
+    {
+        $countNotIndexed = FCom_Catalog_Model_Product::orm()->where('indextank_indexed', 0)->count();
+        $countTotal = FCom_Catalog_Model_Product::orm()->count();
+        $percent =  (($countTotal - $countNotIndexed)/$countTotal)*100;
+        $indexed= $countTotal - $countNotIndexed;
+
+        $indexingStatus = $this->getIndexingStatus();
+        $indexingStatus->status = 'start';
+        $indexingStatus->percent = ceil($percent);
+        $indexingStatus->to_index = $countNotIndexed;
+        $indexingStatus->indexed = $indexed;
+        $indexingStatus->info = "{$countNotIndexed} documents left";
+        $indexingStatus->updated_at = date("Y-m-d H:i:s");
+        $indexingStatus->save();
+    }
 }
