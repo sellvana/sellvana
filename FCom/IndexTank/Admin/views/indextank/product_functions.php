5a9521a3abcff747a3ebce1145e28ad97c5206aa
Yaroslav Vorozhko <vorozhko@gmail.com>
2012-08-20 14:32:59 +0200

    improved indexing control panel statuses

--- FCom/IndexTank/Admin/views/indextank/product_functions.php
+++ FCom/IndexTank/Admin/views/indextank/product_functions.php
@@ -8,14 +8,8 @@
 <?php if(!$this->status):?>
     <h3>Warning: IndexDen API URL isn't set</h3>
     <h3>Please visit <a href="/admin/settings#tab-FCom_IndexTank">setting page</a> to setup API URL</h3>
-<?php else:?>
-<h3>Index <?=$this->status['name']?> (created at <?=date("Y-m-d", strtotime($this->status['date']))?>)
-        Status: <?=$this->status['status']?>
-</h3>
-<h3>Size: <?=$this->status['size']?> documents</h3>
-<h3>Indexing: <?=$this->indexing_status?></h3>
 <?php endif; ?>
 
 <?php echo $this->view('jqgrid') ?>
 
-<?php echo $this->view('indextank/control_index_dialog') ?>
+<?php echo $this->view('indextank/control_index_dialog')->set('status', $this->status) ?>
