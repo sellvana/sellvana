40d5da4cae03237ec4e856121039dc5a8669dc0f
Yaroslav Vorozhko <vorozhko@gmail.com>
2012-09-03 16:45:12 +0200

    renamed module CreditCard to PaymentCC

--- FCom/PaymentCC/manifest.php
+++ FCom/PaymentCC/manifest.php
@@ -1,16 +1,16 @@
 <?php return array(
     'modules' => array(
         // test CreditCard module
-        'FCom_CreditCard' => array(
+        'FCom_PaymentCC' => array(
             'version' => '0.1.0',
             'root_dir' => '',
             'depends' => array('FCom_Core'),
             'translations' => array('de' => 'de.php'),
-            'description' => "Credit Card test module",
-            'bootstrap' => array('file'=>'CreditCardFrontend.php', 'callback'=>'FCom_CreditCard_Frontend::bootstrap'),
+            'description' => "Payment Credit Card test module",
+            'bootstrap' => array('file'=>'PaymentCCFrontend.php', 'callback'=>'FCom_PaymentCC_Frontend::bootstrap'),
             'areas' => array(
                 'FCom_Frontend' => array(
-                    'bootstrap' => array('file'=>'CreditCardFrontend.php', 'callback'=>'FCom_CreditCard_Frontend::bootstrap'),
+                    'bootstrap' => array('file'=>'PaymentCCFrontend.php', 'callback'=>'FCom_PaymentCC_Frontend::bootstrap'),
                 ),
             ),
         ),
