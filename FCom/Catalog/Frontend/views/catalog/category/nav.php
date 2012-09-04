eaa9f3ee1f2b26727faad844b8b388c76f672167
Yaroslav Vorozhko <vorozhko@gmail.com>
2012-09-04 16:50:34 +0200

    added option in category - show in top menu

--- FCom/Catalog/Frontend/views/catalog/category/nav.php
+++ FCom/Catalog/Frontend/views/catalog/category/nav.php
@@ -1,8 +1,5 @@
 <?php
-if (!$this->root_id){
-    $this->root_id = 1;
-}
-$categories = FCom_Catalog_Model_Category::i()->orm()->where('parent_id', $this->root_id)->find_many();
+$categories = FCom_Catalog_Model_Category::i()->orm()->where('top_menu', 1)->find_many();
 ?>
 
 <?php foreach($categories as $cat): ?>
--- FCom/Frontend/views/nav.php
+++ FCom/Frontend/views/nav.php
@@ -1,7 +1,8 @@
 <?php if (BConfig::i()->get('modules/FCom_Frontend/nav_top/type') == 'cms'): ?>
     <?=$this->view('cms/nav')->set('root_id', BConfig::i()->get('modules/FCom_Frontend/nav_top/root_cms'))?>
 <?php elseif (BConfig::i()->get('modules/FCom_Frontend/nav_top/type') == 'categories') :?>
-    <?=$this->view('catalog/category/nav')->set('root_id', BConfig::i()->get('modules/FCom_Frontend/nav_top/root_category'))?>
+    <?=$this->view('catalog/category/nav')?>
+    <?//=$this->view('catalog/category/nav')->set('root_id', BConfig::i()->get('modules/FCom_Frontend/nav_top/root_category'))?>
 <?php else: ?>
     <li>Custom menu</li>
 <?php endif; ?>
