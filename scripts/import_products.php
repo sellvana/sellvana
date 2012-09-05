2d0cf0bfd39f4b52f22e61fec997927e0ded30a2
Yaroslav Vorozhko <vorozhko@gmail.com>
2012-09-05 12:20:44 +0200

    added configuration setting to Product import function and Product import script

--- scripts/import_products.php
+++ scripts/import_products.php
@@ -2,21 +2,50 @@
 
 require_once __DIR_.'/../shell/index.php';
 
-$handle = fopen(FULLERON_ROOT_DIR.'/storage/products.csv', 'r');
-$data = array();
-$header = array();
-while($row = fgetcsv($handle, 1024, "\t", '"')) {
-    if (empty($header)) {
-        $header = $row;
-        continue;
-    }
-    foreach($header as $i => $h) {
-        $row[$h] = $row[$i];
-        unset($row[$i]);
-    }
-    $data[] = $row;
-}
+
+
 $config = array();
-$config['import_actions'] = 'create_or_update';
-$config['import_categories'] = true;
-FCom_Catalog_Model_Product::i()->import($data, $config);
+$config['file']['filename'] = FULLERON_ROOT_DIR.'/storage/products.csv';
+$config['import']['actions'] = 'create_or_update';
+$config['import']['categories'] = true;
+$config['format']['encoding'] = 'UTF-8';
+$config['format']['delimiter'] = "\t";
+$config['format']['enclosure'] = '"';
+$config['format']['multivalue_separator'] = ';';
+$config['format']['nesting_separator'] = '>';
+$config['data']['batch_size'] = 1024*4; //100K
+
+importProductsTask($config);
+
+function importProductsTask($config)
+{
+    if (!file_exists($config['file']['filename']) || !is_readable($config['file']['filename'])) {
+        die("Filename is not readable: ".$config['file']['filename']);
+    }
+    $handle = fopen($config['file']['filename'], 'r');
+    $data = array();
+    $header = array();
+    $readBytes = 0;
+    while($row = fgetcsv($handle, 0, $config['format']['delimiter'], $config['format']['enclosure'])) {
+        if (empty($header)) {
+            $header = $row;
+            continue;
+        }
+
+        foreach($header as $i => $h) {
+            $row[$h] = $row[$i];
+            $readBytes += mb_strlen($row[$i]);
+            unset($row[$i]);
+        }
+        $data[] = $row;
+
+        if ($readBytes > $config['data']['batch_size']) {
+            FCom_Catalog_Model_Product::i()->import($data, $config);
+            $data = array();
+            $readBytes = 0;
+        }
+    }
+    if (!empty($data)) {
+        FCom_Catalog_Model_Product::i()->import($data, $config);
+    }
+}
