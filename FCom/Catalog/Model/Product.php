b884ebd6c363b38bf515a31f3630fb9901587949
Yaroslav Vorozhko <vorozhko@gmail.com>
2012-09-11 15:52:09 +0200

    fixed resize script and changed imageUrl function

--- FCom/Catalog/Model/Product.php
+++ FCom/Catalog/Model/Product.php
@@ -32,13 +32,14 @@ class FCom_Catalog_Model_Product extends FCom_Core_Model_Abstract
 
     public function imageUrl($full=false)
     {
-        $url = $full ? BApp::src('FCom_Catalog').'/' : '';
-        return $url.'media/'.($this->image_url ? $this->image_url : 'DC642702.jpg');
+        $media = BConfig::i()->get('web/media_dir') ? BConfig::i()->get('web/media_dir') : 'media/';
+        $url = $full ? BApp::href('/') : '';
+        return $url.$media.($this->image_url ? $this->image_url : 'DC642702.jpg');
     }
 
     public function thumbUrl($w, $h=null)
     {
-        return FCom_Core::i()->resizeUrl().'?f='.urlencode($this->imageUrl()).'&s='.$w.'x'.$h;
+        return FCom_Core::i()->resizeUrl().'?f='.urlencode($this->imageUrl(true)).'&s='.$w.'x'.$h;
     }
 
     public function beforeSave()
