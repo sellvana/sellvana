05475582f47102d1737ff8dfe03ab8649bf5f0a0
Yaroslav Vorozhko <vorozhko@gmail.com>
2012-10-10 17:00:40 +0200

    adding edit of orders

--- FCom/Sales/Admin/Controller/Orders.php
+++ FCom/Sales/Admin/Controller/Orders.php
@@ -16,7 +16,9 @@ class FCom_Sales_Admin_Controller_Orders extends FCom_Admin_Controller_Abstract_
             'id' => array('index'=>'o.id', 'label' => 'Order id', 'width' =>70),
             'purchased_dt' => array('index'=>'o.purchased_dt', 'label' => 'Purchased on'),
             'billing_name' => array('label'=>'Bill to Name', 'index'=>'ab.billing_name'),
+            'billing_address' => array('label'=>'Bill to Address', 'index'=>'ab.billing_address'),
             'shipping_name' => array('label'=>'Ship to Name', 'index'=>'as.shipping_name'),
+            'shipping_address' => array('label'=>'Ship to Address', 'index'=>'as.shipping_address'),
             'gt_base' => array('label'=>'GT (base)', 'index'=>'o.gt_base'),
             'balance' => array('label'=>'GT (paid)', 'index'=>'o.balance'),
             'discount' => array('label'=>'Discount', 'index'=>'o.discount_code'),
@@ -33,9 +35,11 @@ class FCom_Sales_Admin_Controller_Orders extends FCom_Admin_Controller_Abstract_
 
         $orm->left_outer_join('FCom_Sales_Model_Address', 'o.id = ab.order_id and ab.atype="billing"', 'ab') //array('o.id','=','a.order_id')
             ->select_expr('CONCAT_WS(" ", ab.firstname,ab.lastname)','billing_name')
+            ->select_expr('CONCAT_WS(" \n", ab.street1,ab.city,ab.country,ab.phone)','billing_address')
         ;
         $orm->left_outer_join('FCom_Sales_Model_Address', 'o.id = as.order_id and as.atype="shipping"', 'as') //array('o.id','=','a.order_id')
             ->select_expr('CONCAT_WS(" ", as.firstname,as.lastname)','shipping_name')
+            ->select_expr('CONCAT_WS(" \n", as.street1,as.city,as.country,as.phone)','shipping_address')
         ;
     }
 
@@ -52,14 +56,17 @@ class FCom_Sales_Admin_Controller_Orders extends FCom_Admin_Controller_Abstract_
     public function action_form()
     {
         $orderId = BRequest::i()->params('id', true);
+        $act = BRequest::i()->params('act', true);
 
         $order = FCom_Sales_Model_Order::load($orderId);
         $shipping = FCom_Sales_Model_Address::i()->findByOrder($orderId,'shipping');
         $billing = FCom_Sales_Model_Address::i()->findByOrder($orderId,'billing');
         if ($shipping) {
+            $order->shipping_name = $shipping->firstname.' '.$shipping->lastname;
             $order->shipping_address = FCom_Sales_Model_Address::i()->as_html($shipping);
         }
         if ($billing) {
+            $order->billing_name = $billing->firstname.' '.$billing->lastname;
             $order->billing_address = FCom_Sales_Model_Address::i()->as_html($billing);
         }
 
@@ -74,11 +81,37 @@ class FCom_Sales_Admin_Controller_Orders extends FCom_Admin_Controller_Abstract_
         $order->customer = $customer;
 
         $model = $order;
+        $model->act = $act;
 
         $view = $this->view($this->_formViewName)->set('model', $model);
+
         $this->formViewBefore(array('view'=>$view, 'model'=>$model));
 
         $this->layout($this->_formLayoutName);
         $this->processFormTabs($view, $model, 'edit');
+    }
+
+    public function formViewBefore($args)
+    {
+        $m = $args['model'];
+        $act = $m->act;
+        if ('edit' == $act) {
+            $actions =array(
+                'back' => '<button type="button" class="st3 sz2 btn" onclick="location.href=\''.BApp::href($this->_gridHref).'\'"><span>Back to list</span></button>',
+                'delete' => '<button type="submit" class="st2 sz2 btn" name="do" value="DELETE" onclick="return confirm(\'Are you sure?\') && adminForm.delete(this)"><span>Delete</span></button>',
+                'save' => '<button type="submit" class="st1 sz2 btn" onclick="return adminForm.saveAll(this)"><span>Save</span></button>',
+            );
+        } else {
+            $actions =array(
+                'back' => '<button type="button" class="st3 sz2 btn" onclick="location.href=\''.BApp::href($this->_gridHref).'\'"><span>Back to list</span></button>',
+                'edit' => '<button type="button" class="st1 sz2 btn" onclick="location.href=\''.BApp::href('orders/form').'?id='.$m->id.'&act=edit'.'\'"><span>Edit</span></button>',
+            );
+        }
+        $args['view']->set(array(
+            'form_id' => BLocale::transliterate($this->_formLayoutName),
+            'form_url' => BApp::href($this->_formHref).'?id='.$m->id,
+            'actions' => $actions,
+        ));
+        BPubSub::i()->fire(static::$_origClass.'::formViewBefore', $args);
     }
 }
--- FCom/Sales/Admin/views/order/orders-form/main.php
+++ FCom/Sales/Admin/views/order/orders-form/main.php
@@ -1,104 +1,240 @@
 <?php $m = $this->model; ?>
-<fieldset class="adm-section-group">
-    <ul class="form-list">
-        <li>
-            <h4 class="label">Order: <?=$m->id?> </h4>
-        </li>
-        <li>
-            <h4 class="label">Order Date: <?=$m->purchased_dt?> </h4>
-        </li>
-        <li>
-            <h4 class="label">Order Status: <?=$m->status?> </h4>
-        </li>
-    </ul>
-</fieldset>
+<?php if('edit' == $m->act):
 
-<fieldset class="adm-section-group">
-    <ul class="form-list">
-        <li>
-            <h4 class="label">Account information </h4>
-        </li>
+    /**
+     * Edit
+     * **/?>
+    <fieldset class="adm-section-group">
+        <ul class="form-list">
+            <li>
+                <h4 class="label">Order: <?=$m->id?> </h4>
+            </li>
+            <li>
+                <h4 class="label">Order Date </h4>
+                <input type="text" name="model[purchased_dt]" value="<?=$m->purchased_dt?>">
+            </li>
+            <li>
+                <h4 class="label">Order Status</h4>
+                <select name="model[status]">
+                    <option value="new" <?='new'==$m->status?'selected':''?>>New</option>
+                    <option value="paid"<?='paid'==$m->status?'selected':''?>>Paid</option>
+                    <option value="pending"<?='pending'==$m->status?'selected':''?>>Pending</option>
+                </select>
+            </li>
+        </ul>
+    </fieldset>
 
-            <?php if(false == $m->customer->guest):?>
-                <li>Customer name: <?=$m->customer->firstname?> <?=$m->customer->lastname?></li>
-                <li>Email: <?=$m->customer->email?></li>
-            <?php else:?>
-                <li>Purchased by guest</li>
+    <fieldset class="adm-section-group">
+        <ul class="form-list">
+            <li>
+                <h4 class="label">Account information </h4>
+            </li>
+
+                <?php if(false == $m->customer->guest):?>
+                    <li>Customer name: <?=$m->customer->firstname?> <?=$m->customer->lastname?></li>
+                    <li>Email: <?=$m->customer->email?></li>
+                    <li><a href="<?=BApp::href('customers/form?id='.$m->customer->id)?>">Edit customer account</a></li>
+                <?php else:?>
+                    <li>Purchased by guest</li>
+                <?php endif; ?>
+
+        </ul>
+    </fieldset>
+
+    <fieldset class="adm-section-group">
+        <ul class="form-list">
+            <li>
+                <h4 class="label">Billing info </h4>
+            </li>
+            <li>
+                <?=$m->billing_name?>
+            </li>
+            <li>
+                <?=$m->billing_address?>
+            </li>
+        </ul>
+    </fieldset>
+
+
+    <fieldset class="adm-section-group">
+        <ul class="form-list">
+            <li>
+                <h4 class="label">Shipping info </h4>
+            </li>
+            <li>
+                <?=$m->shipping_name?>
+            </li>
+            <li>
+                <?=$m->shipping_address?>
+            </li>
+        </ul>
+    </fieldset>
+
+    <fieldset class="adm-section-group">
+        <ul class="form-list">
+            <li>
+                <h4 class="label">Payment info </h4>
+            </li>
+            <li>
+                Payment method: <?=$m->payment_method?>
+            </li>
+            <?php if(BUtil::fromJson($m->payment_details)):?>
+                <?php foreach(BUtil::fromJson($m->payment_details) as $paymentKey => $paymentValue):?>
+                    <li><?=$paymentKey?>: <?=$paymentValue?></li>
+                <?php endforeach; ?>
             <?php endif; ?>
-
-    </ul>
-</fieldset>
-
-<fieldset class="adm-section-group">
-    <ul class="form-list">
-        <li>
-            <h4 class="label">Billing info </h4>
-        </li>
-        <li>
-            <?=$m->billing_address?>
-        </li>
-    </ul>
-</fieldset>
+        </ul>
+    </fieldset>
 
 
-<fieldset class="adm-section-group">
-    <ul class="form-list">
-        <li>
-            <h4 class="label">Shipping info </h4>
-        </li>
-        <li>
-            <?=$m->shipping_address?>
-        </li>
-    </ul>
-</fieldset>
+    <fieldset class="adm-section-group">
+        <ul class="form-list">
+            <li>
+                <h4 class="label">Shipping &amp; Handling information </h4>
+            </li>
+            <li>
+                Shipping method: <?=$m->shipping_method?>
+            </li>
+            <li>
+                Shipping service: <?=$m->shipping_service_title?>
+            </li>
+        </ul>
+    </fieldset>
 
-<fieldset class="adm-section-group">
-    <ul class="form-list">
-        <li>
-            <h4 class="label">Payment info </h4>
-        </li>
-        <li>
-            Payment method: <?=$m->payment_method?>
-        </li>
-        <?php if(BUtil::fromJson($m->payment_details)):?>
-            <?php foreach(BUtil::fromJson($m->payment_details) as $paymentKey => $paymentValue):?>
-                <li><?=$paymentKey?>: <?=$paymentValue?></li>
-            <?php endforeach; ?>
-        <?php endif; ?>
-    </ul>
-</fieldset>
+    <fieldset class="adm-section-group">
+        <ul class="form-list">
+            <li>
+                <h4 class="label">Items ordered </h4>
+            </li>
+            <?php if($m->items):?>
+                <?php foreach($m->items as $item):
+                    $product = BUtil::fromJson($item->product_info);
+                    ?>
+                    <li>Product: <?=$product['product_name']?></li>
+                    <li>SKU: <?=$product['manuf_sku']?></li>
+                    <li>Price: <?=$product['base_price']?></li>
+                    <li>Qty: <?=$item->qty?></li>
+                    <li>Total: <?=$item->total?></li>
+                    <li>---------------------------</li>
+                <?php endforeach; ?>
+            <?php endif; ?>
+        </ul>
+    </fieldset>
+
+<?php else:
+    /**
+     * View
+     * **/
+    ?>
+
+    <fieldset class="adm-section-group">
+        <ul class="form-list">
+            <li>
+                <h4 class="label">Order: <?=$m->id?> </h4>
+            </li>
+            <li>
+                <h4 class="label">Order Date: <?=$m->purchased_dt?> </h4>
+            </li>
+            <li>
+                <h4 class="label">Order Status: <?=$m->status?> </h4>
+            </li>
+        </ul>
+    </fieldset>
+
+    <fieldset class="adm-section-group">
+        <ul class="form-list">
+            <li>
+                <h4 class="label">Account information </h4>
+            </li>
+
+                <?php if(false == $m->customer->guest):?>
+                    <li>Customer name: <?=$m->customer->firstname?> <?=$m->customer->lastname?></li>
+                    <li>Email: <?=$m->customer->email?></li>
+                <?php else:?>
+                    <li>Purchased by guest</li>
+                <?php endif; ?>
+
+        </ul>
+    </fieldset>
+
+    <fieldset class="adm-section-group">
+        <ul class="form-list">
+            <li>
+                <h4 class="label">Billing info </h4>
+            </li>
+            <li>
+                <?=$m->billing_name?>
+            </li>
+            <li>
+                <?=$m->billing_address?>
+            </li>
+        </ul>
+    </fieldset>
 
 
-<fieldset class="adm-section-group">
-    <ul class="form-list">
-        <li>
-            <h4 class="label">Shipping &amp; Handling information </h4>
-        </li>
-        <li>
-            Shipping method: <?=$m->shipping_method?>
-        </li>
-        <li>
-            Shipping service: <?=$m->shipping_service_title?>
-        </li>
-    </ul>
-</fieldset>
+    <fieldset class="adm-section-group">
+        <ul class="form-list">
+            <li>
+                <h4 class="label">Shipping info </h4>
+            </li>
+            <li>
+                <?=$m->shipping_name?>
+            </li>
+            <li>
+                <?=$m->shipping_address?>
+            </li>
+        </ul>
+    </fieldset>
 
-<fieldset class="adm-section-group">
-    <ul class="form-list">
-        <li>
-            <h4 class="label">Items ordered </h4>
-        </li>
-        <?php if($m->items):?>
-            <?php foreach($m->items as $item):
-                $product = BUtil::fromJson($item->product_info);
-                ?>
-                <li>Product: <?=$product['product_name']?></li>
-                <li>SKU: <?=$product['manuf_sku']?></li>
-                <li>Price: <?=$product['base_price']?></li>
-                <li>Qty: <?=$item->qty?></li>
-                <li>Total: <?=$item->total?></li>
-                <li>---------------------------</li>
-            <?php endforeach; ?>
-        <?php endif; ?>
-    </ul>
-</fieldset>
+    <fieldset class="adm-section-group">
+        <ul class="form-list">
+            <li>
+                <h4 class="label">Payment info </h4>
+            </li>
+            <li>
+                Payment method: <?=$m->payment_method?>
+            </li>
+            <?php if(BUtil::fromJson($m->payment_details)):?>
+                <?php foreach(BUtil::fromJson($m->payment_details) as $paymentKey => $paymentValue):?>
+                    <li><?=$paymentKey?>: <?=$paymentValue?></li>
+                <?php endforeach; ?>
+            <?php endif; ?>
+        </ul>
+    </fieldset>
+
+
+    <fieldset class="adm-section-group">
+        <ul class="form-list">
+            <li>
+                <h4 class="label">Shipping &amp; Handling information </h4>
+            </li>
+            <li>
+                Shipping method: <?=$m->shipping_method?>
+            </li>
+            <li>
+                Shipping service: <?=$m->shipping_service_title?>
+            </li>
+        </ul>
+    </fieldset>
+
+    <fieldset class="adm-section-group">
+        <ul class="form-list">
+            <li>
+                <h4 class="label">Items ordered </h4>
+            </li>
+            <?php if($m->items):?>
+                <?php foreach($m->items as $item):
+                    $product = BUtil::fromJson($item->product_info);
+                    ?>
+                    <li>Product: <?=$product['product_name']?></li>
+                    <li>SKU: <?=$product['manuf_sku']?></li>
+                    <li>Price: <?=$product['base_price']?></li>
+                    <li>Qty: <?=$item->qty?></li>
+                    <li>Total: <?=$item->total?></li>
+                    <li>---------------------------</li>
+                <?php endforeach; ?>
+            <?php endif; ?>
+        </ul>
+    </fieldset>
+<?php endif; ?>
+
