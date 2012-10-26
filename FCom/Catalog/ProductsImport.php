c9a5c92b2ba62e560fbea94f7600ac4432040c0a
Yaroslav Vorozhko <vorozhko@gmail.com>
2012-10-26 14:48:22 +0300

    improved products import for Magento CSV

--- FCom/Catalog/ProductsImport.php
+++ FCom/Catalog/ProductsImport.php
@@ -3,20 +3,20 @@
 class FCom_Catalog_ProductsImport extends BImport
 {
     protected $fields = array(
-        'product.manuf_sku' => array('pattern'=>'manuf.*sku'),
-        'product.product_name' => array('pattern'=>'product.*name'),
+        'product.manuf_sku' => array('pattern'=>'sku'),
+        'product.product_name' => array('pattern'=>'product.*name|name'),
         'product.short_description' => array('pattern'=>'short.*description'),
         'product.description' => array('pattern'=>'description'),
         'product.url_key' => array('pattern'=>'url.*key'),
-        'product.base_price' => array('pattern'=>'base.*price'),
+        'product.base_price' => array('pattern'=>'base.*price|price'),
         'product.notes' => array('pattern'=>'notes'),
         'product.weight' => array('pattern'=>'weight'),
-        'product.image_url' => array('pattern'=>'image*url'),
+        'product.image_url' => array('pattern'=>'image*url|thumbnail'),
         'product.avg_rating' => array('pattern'=>'avg*rating'),
         'product.num_reviews' => array('pattern'=>'num*reviews'),
-        'product.disabled' => array('pattern'=>'disabled'),
-        'product.categories' => array('pattern'=>'categories'),
-        'product.images' => array('pattern'=>'images'),
+        'product.disabled' => array('pattern'=>'product*disabled'),
+        'product.categories' => array('pattern'=>'categories|category'),
+        'product.images' => array('pattern'=>'images|image'),
     );
 
     protected $dir = 'products';
