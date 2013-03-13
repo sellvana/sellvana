593a2f594f9730c641cb9e23e4a73464337b0994
jamby77 <jamby77@gmail.com>
2013-03-13 22:04:17 +0200

    layout.php - reformatted a bit, added missing php Docs BViewTest.php - added some tests

--- FCom/buckyball/tests/buckyball/com/BViewTest.php
+++ FCom/buckyball/tests/buckyball/com/BViewTest.php
@@ -2,7 +2,19 @@
 
 class BView_Test extends PHPUnit_Framework_TestCase
 {
-    
+    public function testViewIsInstanceOfBView()
+    {
+        $view = BView::factory('my', array());
+        $this->assertInstanceOf('BView', $view, sprintf("Expected instance is 'BView', but got %s", get_class($view)));
+    }
+
+    public function testViewIsInstanceOfOtherView()
+    {
+        $class = 'stdClass';
+
+        $view = BView::factory('my', array('view_class' => $class));
+        $this->assertInstanceOf($class, $view, sprintf("Expected instance is %s, but got %s", $class, get_class($view)));
+    }
 
     public function testViewFactory()
     {
@@ -31,13 +43,42 @@ class BView_Test extends PHPUnit_Framework_TestCase
     //
     public function testGetView()
     {
-        $this->markTestIncomplete(
-          'what is for BLayout::i()->view() method?'
-        );
-
         $view = BView::factory('my', array('key' => 'value'));
+        BLayout::i()->addView('new', array('key' => 'newValue')); // have to have it in layout to get it.
         $viewNew = $view->view('new', array('new' => 'value'));
 
-        $this->assertEquals('value', $viewNew->param('new'));
+        $this->assertEquals('value', $viewNew->get('new'));
+    }
+
+    public function testCanClearParams()
+    {
+        $view = BView::factory('my', array('key' => 'value'));
+        $this->assertEquals('value', $view->param('key'));
+        $view->clear();
+        $this->assertNull($view->param('key'));
+    }
+
+    public function testSetAndGetParams()
+    {
+        $view = BView::factory('my', array());
+        $this->assertNull($view->getParam('test'));
+        $view->setParam('test', true);
+        $this->assertEquals(true, $view->getParam('test'));
+    }
+
+    public function testSetGetArgParams()
+    {
+        $view = BView::factory('my', array());
+        $this->assertNull($view->get('test'));
+        $view->set('test', 'value');
+        $this->assertEquals('value', $view->get('test'));
+    }
+
+    public function testMagicGetSetMethods()
+    {
+        $view = BView::factory('my', array());
+        $this->assertNull($view->test);
+        $view->test =  'value';
+        $this->assertEquals('value', $view->test);
     }
 }
