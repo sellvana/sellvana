9f82865f484cc7dc2ad4da238f571d7631e86bab
Boris Gurvich <unirgy@gmail.com>
2013-03-13 17:12:41 -0400

    finished indexer logic

--- FCom/CatalogIndex/Frontend/Controller.php
+++ FCom/CatalogIndex/Frontend/Controller.php
@@ -9,7 +9,8 @@ class FCom_CatalogIndex_Frontend_Controller extends FCom_Frontend_Controller_Abs
         $result = FCom_CatalogIndex::i()->findProducts('lorem', array('color'=>'Green', 'size'=>'Medium'), 'product_name');
         echo "<pre>";
         print_r($result['facets']);
-        print_r($result['orm']->find_many());
+        $pageData = $result['orm']->paginate();
+        print_r($pageData);
         echo "</pre>";
     }
 }
