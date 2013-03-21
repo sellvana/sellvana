082fa6eb75c6429b1ff3bcfe5b5a4fca63d0cc3b
Boris Gurvich <unirgy@gmail.com>
2013-03-21 19:21:36 -0400

    cleanup

--- FCom/Catalog/Frontend/views/catalog/category/sidebar.php
+++ FCom/Catalog/Frontend/views/catalog/category/sidebar.php
@@ -2,12 +2,14 @@
 $category = $this->category ? $this->category : BApp::i()->get('current_category');
 $parent = false;
 $siblings = false;
-if ($category->parent_id) {
-    $parent = FCom_Catalog_Model_Category::load($category->parent_id);
-    $siblings = $category->siblings();
-}
+if ($category) {
+    if ($category->parent_id) {
+        $parent = FCom_Catalog_Model_Category::load($category->parent_id);
+        $siblings = $category->siblings();
+    }
 
-$children = $category->children();
+    $children = $category->children();
+}
 ?>
 
 <section class="block block-filter">
@@ -17,6 +19,7 @@ $children = $category->children();
 	<header class="block-sub-title"><span class="title"><?= BLocale::_("Categories") ?></span></header>
         <a href="<?=BApp::href('catalog/search').'?'.BRequest::rawGet()?>">&lt; <?= BLocale::_("All categories") ?></a>
             <ul>
+            <?php if($category) :?>
                     <?php if ($parent && $parent->node_name) :?>
                         <li style="padding-left: 10px;"><a href="<?=$this->q($parent->url().'?'.BRequest::rawGet())?>">&lt; <?=$this->q($parent->node_name)?></a></li>
                     <?php endif; ?>
@@ -27,6 +30,7 @@ $children = $category->children();
                                 <a href="<?=$this->q($c->url().'?'.BRequest::rawGet())?>"><?=$this->q($c->node_name)?></a>
                             </li>
                         <?php endforeach; ?>
+                    <?php endif ?>
                     <?php if($siblings) :?>
                         <?php foreach ($siblings as $c): ?>
                             <li style="padding-left: 20px;">
--- FCom/Customer/Frontend/views/email/new-customer.php
+++ FCom/Customer/Frontend/views/email/new-customer.php
@@ -6,13 +6,4 @@
 
 Hello, <?=$d->firstname?>.
 
-<p>Thank you for submitting your request to become an authorized XP Dealer. Your request will be reviewed by the authorized XP distributor for your country which is:</p>
-
-<p><strong>Detector Electronics Corp.</strong>
-<address>23 Turnpike Road<br>
-Southborough, MA 01772</address>
-(800) 446-0244 US<br/>
-(508) 460-6244 US<br/>
-(888) 627-5295 Canada</p>
-
-<p>A representative will contact you during normal business hours which are Monday through Friday from 9-5 EST. We look forward to welcoming you to the growing family of XP dealers.</p>
+<p>Thank you for your registration</p>
