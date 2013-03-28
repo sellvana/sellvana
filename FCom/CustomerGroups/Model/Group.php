8f1022331adcb4bf63c5bff3bc8f825706e40903
Petar Dzhambazov <jamby77@gmail.com>
2013-03-28 21:11:11 +0200

    customer groups grid added

--- FCom/CustomerGroups/Model/Group.php
+++ FCom/CustomerGroups/Model/Group.php
@@ -9,4 +9,14 @@ class FCom_CustomerGroups_Model_Group
 {
     protected static $_table = "fcom_customer_groups";
     protected static $_origClass = __CLASS__;
+
+    /**
+     * @param bool  $new
+     * @param array $args
+     * @return FCom_CustomerGroups_Model_Group
+     */
+    public static function i($new = false, array $args = array())
+    {
+        return parent::i($new, $args); // auto completion helper
+    }
 }
