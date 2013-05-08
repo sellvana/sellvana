a8a4e369570b4e671c6f368ee99cf8e6583e858f
Petar Dzhambazov <jamby77@gmail.com>
2013-05-09 00:23:38 +0300

    added admin settings for Google Checkout, started building form functionality

--- FCom/GoogleCheckout/GoogleCheckoutAdmin.php
+++ FCom/GoogleCheckout/GoogleCheckoutAdmin.php
@@ -4,6 +4,6 @@ class FCom_GoogleCheckout_Admin extends BClass
 {
     static public function bootstrap()
     {
-
+        BLayout::i()->addAllViews('Admin/views');
     }
 }
--- FCom/GoogleCheckout/GoogleCheckoutFrontend.php
+++ FCom/GoogleCheckout/GoogleCheckoutFrontend.php
@@ -4,6 +4,21 @@ class FCom_GoogleCheckout_Frontend extends BClass
 {
     static public function bootstrap()
     {
+        BFrontController::i()->route('GET /gcheckout/.action', 'FCom_PayPal_Frontend_Controller');
 
+        BLayout::i()->addAllViews('Frontend/views');
+
+        FCom_Checkout_Model_Cart::i()->addPaymentMethod('gcheckout', 'FCom_GoogleCheckout_Frontend');
+    }
+
+    public function getName()
+    {
+        return 'Google Checkout';
+    }
+
+    public function processPayment()
+    {
+        $href = BApp::href('gcheckout/redirect');
+        BResponse::i()->redirect($href);
     }
 }
