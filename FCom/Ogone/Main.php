b3a45412ac852e63747062081f47224e52aaf0c6
Boris Gurvich <unirgy@gmail.com>
2013-06-06 07:35:05 -0700

    Added db/implicit_migration: true by default Added 'autoload' manifest directive, changed Ogone to use it

--- FCom/Ogone/Main.php
+++ FCom/Ogone/Main.php
@@ -4,8 +4,6 @@ class FCom_Ogone_Main extends BClass
 {
     static public function bootstrap()
     {
-        BApp::m('FCom_Ogone')->autoload('lib');
-
         FCom_Sales_Main::i()
             ->addPaymentMethod('ogone', 'FCom_Ogone_PaymentMethod')
             ->addCheckoutMethod('ogone', 'FCom_Ogone_CheckoutMethod')
