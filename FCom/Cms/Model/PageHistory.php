ba978d321c9f90f6227aa088eccabe48fa9e07dd
Boris Gurvich <unirgy@gmail.com>
2013-06-20 09:23:10 -0700

    Deleted CMS_Page related files

--- FCom/Cms/Model/PageHistory.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class FCom_Cms_Model_PageHistory extends FCom_Core_Model_Abstract
-{
-    protected static $_table = 'fcom_cms_page_history';
-    protected static $_origClass = __CLASS__;
-}
