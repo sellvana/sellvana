3796c0ab24dc89d20fc10875ecdba0c128d7f74d
Petar Dzhambazov <jamby77@gmail.com>
2013-07-09 20:40:16 +0300

    merged files from develop branch

--- FCom/AuthorizeNet/PaymentMethod.php
+++ FCom/AuthorizeNet/PaymentMethod.php
@@ -9,11 +9,11 @@ class FCom_AuthorizeNet_PaymentMethod extends FCom_Sales_Method_Payment_Abstract
 
     public function getCheckoutFormView()
     {
-        return BLayout::i()->view('ogone/form');
+        return BLayout::i()->view('authorize/form');
     }
 
     public function payOnCheckout()
     {
-        die("Ogone payment not imlemented yet");
+        die("Authorize.net payment not implemented yet");
     }
 }
---
+++ FCom/AuthorizeNet/lib/AuthorizeNetException.php
@@ -0,0 +1,5 @@
+<?php
+
+class AuthorizeNetException  extends Exception{
+
+}
