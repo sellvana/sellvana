c71bed485c028ee7509a55511a4d80022bac4978
Petar Dzhambazov <jamby77@gmail.com>
2013-07-22 23:04:34 +0300

    added cart controller removed redundand method, use APP.href instead

--- FCom/Catalog/Model/Product.php
+++ FCom/Catalog/Model/Product.php
@@ -57,11 +57,6 @@ class FCom_Catalog_Model_Product extends FCom_Core_Model_Abstract
         return FCom_Core_Main::i()->resizeUrl().'?f='.urlencode(trim($this->imageUrl($full), '/')).'&s='.$w.'x'.$h;
     }
 
-    public function addToCartUrl()
-    {
-        return BApp::href('cart');
-    }
-
     public function onBeforeSave()
     {
         if (!parent::onBeforeSave()) return false;
