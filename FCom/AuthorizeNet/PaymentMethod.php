8f36aa8d7a7c78513104e54a01d182d49a6381a1
Petar Dzhambazov <jamby77@gmail.com>
2013-08-08 23:01:35 +0300

    refactored forms, added more content to authorize net workflow

--- FCom/AuthorizeNet/PaymentMethod.php
+++ FCom/AuthorizeNet/PaymentMethod.php
@@ -3,6 +3,8 @@
 class FCom_AuthorizeNet_PaymentMethod extends FCom_Sales_Method_Payment_Abstract
 {
 
+    const PAYMENT_METHOD_KEY = "authorizenet_aim";
+
     function __construct()
     {
         $this->_name = 'Authorize.net';
@@ -10,12 +12,34 @@ class FCom_AuthorizeNet_PaymentMethod extends FCom_Sales_Method_Payment_Abstract
 
     public function getCheckoutFormView()
     {
-        return BLayout::i()->view('authorizenet/form');
+        return BLayout::i()->view('authorizenet/form')->set('key', static::PAYMENT_METHOD_KEY);
     }
 
     public function payOnCheckout()
     {
-        die("Authorize.net payment not implemented yet");
+        $config = $this->config();
+        if(!$config['enabled']){
+            // log this and eventually show a message
+            return null;
+        }
+        $action = $config['payment_action'];
+
+        /* @var $api FCom_AuthorizeNet_AimApi */
+        $api = FCom_AuthorizeNet_AimApi::i();
+        switch ($action) {
+            case 'AUTH_ONLY':
+                $response = $api->authorize($this);
+                break;
+            case 'AUTH_CAPTURE':
+                $response = $api->sale($this);
+                break;
+            default :
+                // log and show message
+                return null;
+                break;
+        }
+        $this->clear();
+        return $response;
     }
 
     public function getOrder()
@@ -25,15 +49,24 @@ class FCom_AuthorizeNet_PaymentMethod extends FCom_Sales_Method_Payment_Abstract
 
     public function getCardNumber()
     {
-        // todo return card number
-        $cc_number = null;
-        return $cc_number;
+        if(isset($this->details['cc_num'])){
+            return $this->details['cc_num'];
+        }
+        return null;
+    }
+
+    public function getDetail($key)
+    {
+        if(isset($this->details[$key])){
+            return $this->details[$key];
+        }
+        return null;
     }
 
     /**
      * @return array
      */
-    public function cardTypes($config = array())
+    public function cardTypes()
     {
         return FCom_AuthorizeNet_Model_Settings::cardTypes();
     }
@@ -46,4 +79,28 @@ class FCom_AuthorizeNet_PaymentMethod extends FCom_Sales_Method_Payment_Abstract
         $config = BConfig::i();
         return $config->get('modules/FCom_AuthorizeNet/aim');
     }
+
+    public function setDetails($details)
+    {
+        $details = isset($details[static::PAYMENT_METHOD_KEY]) ?$details[static::PAYMENT_METHOD_KEY]: array();
+
+        return parent::setDetails($details);
+    }
+
+    public function getPublicData()
+    {
+        $data = $this->details;
+        if(!empty($data)){
+            $ccFour = substr($data['cc_num'], -4);
+            unset($data['cc_num']);
+            $data['last_four'] = $ccFour;
+        }
+        return $data;
+    }
+
+    protected function clear()
+    {
+        unset($this->details['cc_num']);
+    }
+
 }
