c165bdd4961713cd3273a2a1196652f7badfed8f
Boris Gurvich <unirgy@gmail.com>
2013-08-13 13:59:54 -0700

    Added .htaccess DirectoryIndex index.php Renamed admin/market/ to admin/marketclient/

--- FCom/MarketClient/Admin/views/marketclient/remote.php
+++ FCom/MarketClient/Admin/views/marketclient/remote.php
@@ -1,7 +1,7 @@
 <?php $conf = BConfig::i()->get('modules/FCom_MarketClient') ?>
 <script>
     require(['jquery'], function($) {
-        $.get("<?=BApp::href('market/site/request_nonce')?>", function(response) {
+        $.get("<?=BApp::href('marketclient/site/request_nonce')?>", function(response) {
             $('#market-remote-container').html('<iframe width="100%" height="100%" src="' + response.url + '"></iframe>');
         });
     })
