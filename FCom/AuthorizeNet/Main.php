ce2589e32b90fc5167c58671c0c3808a18d22cbf
Petar Dzhambazov <jamby77@gmail.com>
2013-08-27 20:49:35 +0300

    started DPM method

--- FCom/AuthorizeNet/Main.php
+++ FCom/AuthorizeNet/Main.php
@@ -8,7 +8,8 @@ class FCom_AuthorizeNet_Main extends BClass
 {
     public static function bootstrap()
     {
-        FCom_Sales_Main::i()->addPaymentMethod('authnet', 'FCom_AuthorizeNet_PaymentMethod');
+        FCom_Sales_Main::i()->addPaymentMethod('authnetaim', 'FCom_AuthorizeNet_PaymentMethod_Aim');
+        FCom_Sales_Main::i()->addPaymentMethod('authnetdpm', 'FCom_AuthorizeNet_PaymentMethod_Dpm');
 //        die(__METHOD__);
     }
 }
