e538fefe59512ff46fe5f18ae9c1bee055e3bea5
Petar Dzhambazov <jamby77@gmail.com>
2013-09-10 23:13:58 +0300

    base for iDEAL

---
+++ FCom/PaymentIdeal/PaymentMethod.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by pp
+ * @project fulleron
+ */
+
+class FCom_PaymentIdeal_PaymentMethod
+    extends FCom_Sales_Method_Payment_Abstract
+{
+
+    public function payOnCheckout()
+    {
+        // TODO: Implement payOnCheckout() method.
+    }
+}
