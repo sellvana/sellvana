e0affdccb584252e1d46fa63488bf04f6253d2b9
Boris Gurvich <unirgy@gmail.com>
2013-10-09 12:28:00 -0400

    Fixed backup files naming Fixed some admin settings pages errors Fixed admin modules page js error

---
+++ FCom/MultiLanguage/Model/Translation.php
@@ -0,0 +1,7 @@
+<?php
+
+class FCom_MultiLanguage_Model_Translation extends FCom_Core_Model_Abstract
+{
+    static protected $_table = 'fcom_multilanguage_translation';
+    static protected $_origClass = __CLASS__;
+}
