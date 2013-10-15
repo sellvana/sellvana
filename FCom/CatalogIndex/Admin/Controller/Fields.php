8886a858add3a07b0000b6e09b73a3d233c349e9
pdtkame <pdtkame@gmail.com>
2013-10-15 17:48:19 +0700

    Chg: - update layout for Catalog - Product Import - minor fix in Catalog - Index Fields

--- FCom/CatalogIndex/Admin/Controller/Fields.php
+++ FCom/CatalogIndex/Admin/Controller/Fields.php
@@ -11,6 +11,7 @@ class FCom_CatalogIndex_Admin_Controller_Fields extends FCom_Admin_Controller_Ab
 
     public function gridConfig()
     {
+	    $fieldHlp = FCom_CatalogIndex_Model_Field::i();
         $config = parent::gridConfig();
         $config['columns'] += array(
             'id'           => array('index' => 'idxf.id'),
