5481dc1b7f1e3b1ba564bd6f124024ec945547dd
chenzhang1290 <chenzhang1290@outlook.com>
2013-10-17 02:42:25 +0800

    10/17 - wen(admin chat)

--- FCom/AdminChat/Admin/js/fcom.adminchat.js
+++ FCom/AdminChat/Admin/js/fcom.adminchat.js
@@ -1,4 +1,160 @@
-define(['jquery', 'underscore', 'backbone', 'fcom.pushclient', 'exports','fcom.adminchat','slimscroll'], function($, _, Backbone, PushClient, exports,AdminChat,slimscroll)
+(function(ns) {
+var utils = {
+    amplify: function(gain) {
+        return function(sample) {
+            return sample * gain;
+        };
+    },
+    ushort: function(sample) {
+        return String.fromCharCode(255 & sample,
+                                   255 & sample >> 8);
+    },
+    ulong: function(sample) {
+        return String.fromCharCode(255 & sample,
+                                   255 & sample >> 8,
+                                   255 & sample >> 16,
+                                   255 & sample >> 24);
+    },
+    gcd: function(a, b) {
+        while (b) {
+            var a_ = a;
+            a = b, b = a_ % b;
+        }
+        return a;
+    },
+    lcm: function(a, b) {
+        return Math.floor(a * b / utils.gcd(a, b));
+    },
+    compose: function(fns) {
+        return function(a) {
+            for (var i = 0; i < fns.length; i++) {
+                a = fns[i](a);
+            }
+            return a;
+        };
+    },
+    map: function(fn, items) {
+        var result = [];
+        for (var i = 0; i < items.length; i++) {
+            result.push(fn.call(this, items[i]));
+        }
+        return result;
+    },
+    getattr: function(attr) {
+        return function(items) {
+            return items[attr];
+        };
+    },
+    zip: function() {
+        if (arguments.length == 0) return [];
+        var lists = Array.prototype.slice.call(arguments);
+        var result = [];
+        var min = Math.min.apply(null, utils.map(utils.getattr("length"), lists));
+        for (var i = 0; i < min; i++) {
+            result.push(utils.map(utils.getattr(i), lists));
+        }
+        return result;
+    },
+    sum: function(numbers) {
+        return utils.foldl(function(a, b) { return a + b; }, numbers);
+    },
+    bind: function(ctx, fn) {
+        return function() {
+            var args = Array.prototype.slice.call(arguments);
+            return fn.apply(ctx, args);
+        };
+    },
+    foldl: function(fn, items) {
+        if (items.length == 1) return items[0];
+        var result = fn(items[0], items[1]);
+        for (var i = 2; i < items.length; i++) {
+            result = fn(result, items[i]);
+        }
+        return result;
+    },
+    mulmod: function(a, b, c) {
+        return (a * b) % c;
+    },
+    range: function(len) {
+        var result = [];
+        for (var i = 0; i < len; i++) {
+            result.push(i);
+        }
+        return result;
+    }
+};
+function Beep(samplerate) {
+    if (!(this instanceof Beep)) return new Beep(samplerate);
+    if (typeof samplerate != "number" || samplerate < 1) return null;
+    this.channels = 1;
+    this.bitdepth = 16;
+    this.samplerate = samplerate;
+    this.sine = [];
+    var factor = (2 * Math.PI) / parseFloat(samplerate);
+    for (var n = 0; n < samplerate; n++) {
+        this.sine.push(Math.sin(n * factor));
+    }
+}
+Beep.prototype = {
+    generate: function(freqs) {
+        freqs = freqs instanceof Array ? freqs : [freqs];
+        var map = utils.bind(this, utils.map);
+        var periods = map(function(a) {
+            return utils.lcm(this.samplerate, a) / a; }, freqs);
+        var lcm = utils.foldl(utils.lcm, periods);
+        var sample = function(time) {
+            return function(freq) {
+                return this.sine[utils.mulmod(time, freq, this.samplerate)];
+            };
+        };
+        return map(function(t) { return utils.sum(map(sample(t), freqs)); },
+                   utils.range(lcm));
+    },
+    encode: function(freqs, duration, filters) {
+        freqs = freqs instanceof Array ? freqs : [freqs];
+        var transforms = utils.compose(
+            (filters || []).concat([utils.ushort]));
+        var samples = utils.map(transforms, this.generate(freqs));
+        var reps = Math.ceil(duration * this.samplerate / samples.length);
+        var fulldata = new Array(reps + 1).join(samples.join(""));
+        var data = fulldata.substr(0, this.samplerate * duration * 2);
+        var fmtChunk = [
+            ["f", "m", "t", " "].join(""),
+            utils.ulong(Beep.PCM_CHUNK_SIZE),
+            utils.ushort(Beep.LINEAR_QUANTIZATION),
+            utils.ushort(this.channels),
+            utils.ulong(this.samplerate),
+            utils.ulong(this.samplerate * this.channels * this.bitdepth / 8),
+            utils.ushort(this.bitdepth / 8),
+            utils.ushort(this.bitdepth)
+        ].join("");
+        var dataChunk = [
+            ["d", "a", "t", "a"].join(""),
+            utils.ulong(data.length * this.channels * this.bitdepth / 8),
+            data
+        ].join("");
+        var header = [
+            ["R", "I", "F", "F"].join(""),
+            utils.ulong(4 + (8 + fmtChunk.length) + (8 + dataChunk.length)),
+            ["W", "A", "V", "E"].join("")
+        ].join("");
+        return [header, fmtChunk, dataChunk].join("");
+    },
+    play: function(freq, duration, filters) {
+        filters = filters || [];
+        var data = btoa(this.encode(freq, duration, filters));
+        var audio = document.createElement("audio");
+        audio.src = "data:audio/x-wav;base64," + data;
+        audio.play();
+    }
+};
+Beep.LINEAR_QUANTIZATION = 1;
+Beep.PCM_CHUNK_SIZE = 16;
+Beep.utils = utils;
+ns.Beep = Beep;
+})(window[window.NS_BEEP] || window);
+
+define(['jquery', 'underscore', 'backbone', 'fcom.pushclient', 'exports','fcom.adminchat','slimscroll','timeago'], function($, _, Backbone, PushClient, exports,AdminChat,slimscroll)
 {
     var setScrollable = function(selector) {
     if (selector == null) {
@@ -12,9 +168,21 @@ define(['jquery', 'underscore', 'backbone', 'fcom.pushclient', 'exports','fcom.a
         });
       });
     }
-  };
+    };
 
-  
+    var setTimeAgo = function(selector) {
+        if (selector == null) {
+          selector = $(".timeago");
+        }
+        if (jQuery().timeago) {
+          jQuery.timeago.settings.allowFuture = true;
+          jQuery.timeago.settings.refreshMillis = 60000;
+          selector.timeago();
+          return selector.addClass("in");
+        }
+    };
+
+    var username='';
        
     ChatUserList = {
         Models: {},
@@ -31,9 +199,38 @@ define(['jquery', 'underscore', 'backbone', 'fcom.pushclient', 'exports','fcom.a
         model: ChatUserList.Models.User
     });
     
+    //View for a current login user status 
+    ChatUserList.Views.Status = Backbone.View.extend({
+        el: 'li#adminStatus',
+        template: _.template($('#statusTemplate').html()),
+        events: {
+            'change #adminuser-status': 'changeStatus',
+            'click #adminuser-status': 'preventDefault'
+        },
+        initialize: function(){
+            this.model.on('change', this.render,this);
+        },
+        render: function(){
+            this.$el.html(this.template(this.model.toJSON()));
+            this.$el.find('#adminuser-status').val(this.model.get('status'));
+            return this;
+        },
+        changeStatus: function(ev){
+            var status=this.$el.find('select#adminuser-status:first').val();            
+            AdminChat.status({status:status});
+            return true;
+        },
+        preventDefault: function(ev){
+            
+            ev.stopPropagation();
+        }
+    });
     //View for all user list
     ChatUserList.Views.Users = Backbone.View.extend({
         el: '#adminUserList',
+        initialize: function(){
+            this.collection.on('add', this.addOne, this);
+        },
         render: function(){            
             this.collection.each(this.addOne, this);
             return this;
@@ -49,6 +246,9 @@ define(['jquery', 'underscore', 'backbone', 'fcom.pushclient', 'exports','fcom.a
     ChatUserList.Views.User = Backbone.View.extend({
         tagName: 'li',
         template: _.template($('#userTemplate').html()),        
+        initialize: function(){
+            this.model.on('change', this.render, this);
+        },        
         events: {
             'click' :'initChat'
         },
@@ -122,8 +322,7 @@ define(['jquery', 'underscore', 'backbone', 'fcom.pushclient', 'exports','fcom.a
             'click .btn.box-remove' :'closeChatWin',           
             'submit' :'say'
         },        
-        toggleChatWin: function(e){
-            alert('abc');
+        toggleChatWin: function(e){            
             var box = this.$el.find(".box");
             box.toggleClass("box-collapsed");
             e.preventDefault();
@@ -141,12 +340,15 @@ define(['jquery', 'underscore', 'backbone', 'fcom.pushclient', 'exports','fcom.a
         },
         say: function(ev)
         {
-            ev.preventDefault();            
-            say({channel:this.model.get('channel'), text:this.$el.find('#message_body').val()});
+            ev.preventDefault();
+            var msg_id='id' + (new Date()).getTime();            
+            var msg_item=new ChatWindows.Models.Item({channel:this.model.get('channel'),username:username, text:this.$el.find('#message_body').val(),msg_id:msg_id,time:-1});
+            this.collection.add(msg_item);                        
+            say({channel:this.model.get('channel'),text:this.$el.find('#message_body').val(),msg_id:msg_id});
             this.$el.find('#message_body').val('');
         },
         render: function(){                             
-            this.$el.html(this.template(this.model.toJSON()));
+            this.$el.html(this.template(this.model.toJSON()));            
             return this;
         },
         addOne: function(item)
@@ -159,18 +361,51 @@ define(['jquery', 'underscore', 'backbone', 'fcom.pushclient', 'exports','fcom.a
             $(scrollable).slimScroll({
                 scrollTo: scrollable.prop('scrollHeight') + "px"
             });
-            this.$el.find('ul li:last').effect("highlight", {}, 500);            
+            this.$el.find('ul li:last').effect("highlight", {}, 500); 
 
+            new Beep(22050).play(150, 1, [Beep.utils.amplify(3000)]);        
+
+        },
+        messageSent: function(msg)
+        {
+            var itemModel=this.collection.findWhere({msg_id:msg.msg_id});
+
+            if(itemModel)
+            {
+                itemModel.set('time',msg.time);
+                return true;
+            }
+            else
+                return false;
         }
+
     });
 
+    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     //view for single chat line(item)
     ChatWindows.Views.Item = Backbone.View.extend({        
         template: _.template($('#chatItemTempate').html()),
         tagName: 'li',
         className: 'message',
+        initialize: function(){
+            this.model.on('change',this.render,this);
+        },
         render: function(){                             
             this.$el.html(this.template(this.model.toJSON()));
+            if(this.model.get('time')!== 'undefined' && this.model.get('time')!== -1)
+            {
+
+                var date = new Date();
+                timeago = this.$el.find(".timeago");
+                timeago.attr('title',this.model.get('time'));                                
+                timeago.html("" + months[date.getMonth()] + " " + (date.getDate()) + ", " + (date.getFullYear()) + " " + (date.getHours()) + ":" + (date.getMinutes()));
+                //timeago.removeClass("in");
+                setTimeAgo(timeago);
+            }
+
+            if(this.model.get('time')===-1)            
+                this.$el.find('i').attr('class','icon-spinner');
+            
             return this;
         }
     });
@@ -179,6 +414,7 @@ define(['jquery', 'underscore', 'backbone', 'fcom.pushclient', 'exports','fcom.a
     var chatMainWin = new ChatWindows.Views.Main({collection:chatWins});
     var loadedWins=[];
     function init(options) {
+        console.log(options);
         username = options.username;
         PushClient.send({channel:'adminuser', signal:'subscribe'});
         PushClient.send({channel:'adminuser', signal:'init'})
@@ -200,7 +436,7 @@ define(['jquery', 'underscore', 'backbone', 'fcom.pushclient', 'exports','fcom.a
 
     function say(options) {
         //add message to dom
-        PushClient.send({channel:options.channel, signal:'say', text:options.text});
+        PushClient.send({channel:options.channel, signal:'say', text:options.text, msg_id:options.msg_id});
     }
 
     function leave(options) {
@@ -228,15 +464,14 @@ define(['jquery', 'underscore', 'backbone', 'fcom.pushclient', 'exports','fcom.a
 
     function add_history(msg)
     {
-        console.log(loadedWins);
-        console.log(msg);
+        
         var json=_.where(loadedWins,{channel:msg.channel});        
-        if(json[0])
-        {            
-            var chatItem= new ChatWindows.Models.Item(msg);
-            json[0].win.collection.add(chatItem);
-        }
-
+        if(json[0])                
+            if(!(msg.msg_id && json[0].win.messageSent(msg)))
+            {
+                var chatItem= new ChatWindows.Models.Item(msg);
+                json[0].win.collection.add(chatItem);
+            }
         /*var $h = chatWindows[msg.channel].$history, h = $h.get(0);
         $h.append($('<div>').html(msg.text));
         h.scrollTop = h.scrollHeight;*/
@@ -254,20 +489,36 @@ define(['jquery', 'underscore', 'backbone', 'fcom.pushclient', 'exports','fcom.a
             channel_adminuser.signals[msg.signal](msg);
         }
     }
+    var users = new ChatUserList.Collections.Users([]);  
+    var userView = new ChatUserList.Views.Users({collection: users});
+
+    var statusModel = new ChatUserList.Models.User({username:username,status:'online'});
+    var statusView=new ChatUserList.Views.Status({model:statusModel});
+    statusView.render();
+
     channel_adminuser.signals = {
         status: function(msg) {
-
-            var users = new ChatUserList.Collections.Users(msg.users);  
-            var userView = new ChatUserList.Views.Users({collection: users});
-            userView.render();            
             _.each(msg.users, function(user) {
-                if (user.username === username) {
+                
+                if(user.username===username)
+                {                    
+                    statusModel.set('status',user.status);
                     return;
+                }                
+                var temps=users.where({username:user.username});
+                if(temps.length>0)
+                {                    
+                    temps[0].set("status",user.status);
                 }
-                if (!msg.init) {
-                    $.bootstrapGrowl(user.username+' is '+user.status, { type:'success', align:'center', width:'auto' });
+                else
+                    users.add(user);
+
+                if(!msg.init){
+                    $.bootstrapGrowl(user.username+' is '+user.status, { type:'success', align:'center', width:'auto'});
                 }
-            })
+
+
+            });
         }
     }
 
@@ -288,8 +539,7 @@ define(['jquery', 'underscore', 'backbone', 'fcom.pushclient', 'exports','fcom.a
         },
         say: function(msg) {
 
-            show_window({channel:msg.channel});
-            msg.username=curUser;
+            show_window({channel:msg.channel});                        
             add_history(msg);
         },
         join: function(msg) {
