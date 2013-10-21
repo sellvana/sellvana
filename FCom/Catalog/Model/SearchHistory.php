a41e8fb589c9e6057e76935fbd6aa91e3e64c923
Boris Gurvich <unirgy@gmail.com>
2013-10-20 22:39:14 -0400

    Implemented SearchHistory and SearchAlias Fixed cart block thumbnails

---
+++ FCom/Catalog/Model/SearchHistory.php
@@ -0,0 +1,30 @@
+<?php
+
+class FCom_Catalog_Model_SearchHistory extends FCom_Core_Model_Abstract
+{
+    static protected $_table = 'fcom_search_history';
+    static protected $_origClass = __CLASS__;
+
+    static public function addSearchHit($query, $numProductsFound = null)
+    {
+        $sData =& BSession::i()->dataToUpdate();
+        if (!empty($sData['search_history'][$query])) {
+            return null;
+        }
+        $sData['search_history'][$query] = $query;
+        //TODO: add 'W'ord functionality
+        $data = array('term_type' => 'F', 'query' => $query);
+        $record = static::load($data);
+        if ($record) {
+            $record->add('num_searches');
+        } else {
+            $record = static::create($data);
+            $record->set(array('num_searches' => 1, 'first_at' => BDb::now()));
+        }
+        if (!is_null($numProductsFound)) {
+            $record->set('num_products_found_last', $numProductsFound);
+        }
+        $record->set('last_at', BDb::now())->save();
+        return $record;
+    }
+}
