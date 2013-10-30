dacf5014fdaf76fa926a5bead235909422fb70c0
Boris Gurvich <unirgy@gmail.com>
2013-10-30 13:27:00 -0400

    Working on dashboard widgets Updated grid filters html

--- FCom/Admin/View/Dashboard.php
+++ FCom/Admin/View/Dashboard.php
@@ -6,13 +6,20 @@ class FCom_Admin_View_Dashboard extends FCom_Admin_View_Abstract
     {
         $widgets = (array)$this->get('widgets');
         $widget['key'] = $widgetKey;
+        if (empty($widget['cols'])) {
+            $widget['cols'] = 6;
+        }
         $widgets[$widgetKey] = $widget;
+        $this->set('widgets', $widgets);
         return $this;
     }
 
     public function getWidgets()
     {
+        BEvents::i()->fire(__METHOD__, array('view' => $this));
+
         $widgets = (array)$this->get('widgets');
+
         $pers = FCom_Admin_Model_User::i()->personalize();
         if (!empty($pers['dashboard']['widgets'])) {
             foreach ($pers['dashboard']['widgets'] as $wKey => $wState) {
