8b08953bcf7066de3e6140c98835a6cffdbf322b
Boris Gurvich <unirgy@gmail.com>
2013-10-30 13:27:00 -0400

    Working on dashboard widgets Updated grid filters html

--- FCom/Admin/Main.php
+++ FCom/Admin/Main.php
@@ -14,25 +14,6 @@ class FCom_Admin_Main extends BClass
         ));
     }
 
-    public function onSettingsPost($args)
-    {
-        if (!empty($args['post']['config']['db'])) {
-            $db =& $args['post']['config']['db'];
-            if (empty($db['password']) || $db['password']==='*****') {
-                unset($db['password']);
-            }
-        }
-
-        $ip = BRequest::i()->ip();
-        foreach (array('Frontend','Admin') as $area) {
-            if (!empty($args['post']['config']['mode_by_ip']['FCom_'.$area])) {
-                $modes =& $args['post']['config']['mode_by_ip']['FCom_'.$area];
-                $modes = str_replace('@', $ip, $modes);
-                unset($modes);
-            }
-        }
-    }
-
     public static function href($url='')
     {
         return BApp::href($url, 1, 2);
