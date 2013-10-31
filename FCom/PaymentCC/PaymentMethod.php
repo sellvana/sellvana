51aa9148b67babdc16bc8965f3bb430e2f8cfbe7
Petar Dzhambazov <jamby77@gmail.com>
2013-11-01 00:57:24 +0200

    reformatted some tabs, added cc form

--- FCom/PaymentCC/PaymentMethod.php
+++ FCom/PaymentCC/PaymentMethod.php
@@ -1,6 +1,6 @@
 <?php
 
-class FCom_PaymentCC_PaymentMethod extends BClass
+class FCom_PaymentCC_PaymentMethod extends FCom_Sales_Method_Payment_Abstract
 {
     public function getName()
     {
@@ -10,5 +10,10 @@ class FCom_PaymentCC_PaymentMethod extends BClass
     public function payOnCheckout()
     {
         die("Credit Card payment not imlemented yet");
+    }
+
+    public function getCheckoutFormView()
+    {
+        return BLayout::i()->view('credit_card/form');
     }
 }
