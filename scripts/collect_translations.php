f2e1f8c20365a11cf6b91ae86406ca51bdb96101
Petar Dzhambazov <jamby77@gmail.com>
2013-11-06 02:13:50 +0200

    collect_translations update for twig files

--- scripts/collect_translations.php
+++ scripts/collect_translations.php
@@ -1,26 +1,27 @@
 <?php
+ini_set("display_errors", 1);
+error_reporting(-1);
+require_once __DIR__ . '/../tests/index.php';
 
-require_once __DIR_.'/../tests/index.php';
-
-$modules = BModuleRegistry::getAllModules();
+$modules = BModuleRegistry::i()->getAllModules();
 foreach ($modules as $modName => $mod) {
     //only for FCom modules
     if (false === strpos($modName, "FCom")) {
         continue;
     }
-    $viewDir = $mod->root_dir.'/Frontend/views';
+    $viewDir = $mod->root_dir . '/Frontend/views';
     if (!file_exists($viewDir)) {
         continue;
     }
-    $targetFile = $mod->root_dir.'/i18n';
+    $targetFile = $mod->root_dir . '/i18n';
     if (!file_exists($targetFile)) {
         mkdir($targetFile);
     }
-    $targetFile .='/de.php';
+    $targetFile .= '/de.php';
     if (!file_exists($targetFile)) {
         touch($targetFile);
     }
-    echo $targetFile."\n";
+    echo $targetFile . "\n";
     chmod($targetFile, 0777);
     BLocale::collectTranslations($viewDir, $targetFile);
 }
