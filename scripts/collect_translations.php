10cf7ad11201b4ffd056d52ffa986d18bb62454d
Petar Dzhambazov <jamby77@gmail.com>
2013-11-12 23:17:37 +0200

    some updated translations some updated logic for translations added bootstrap to multi language to add ?lang=<> to session

--- scripts/collect_translations.php
+++ scripts/collect_translations.php
@@ -3,6 +3,7 @@ ini_set("display_errors", 1);
 error_reporting(-1);
 require_once __DIR__ . '/../tests/index.php';
 
+echo "Starting" . PHP_EOL;
 $modules = BModuleRegistry::i()->getAllModules();
 foreach ($modules as $modName => $mod) {
     //only for FCom modules
@@ -11,11 +12,13 @@ foreach ($modules as $modName => $mod) {
     }
     $viewDir = $mod->root_dir . '/Frontend/views';
     if (!file_exists($viewDir)) {
+        echo $modName . " has no front views." . PHP_EOL;
         continue;
     }
     $targetFile = $mod->root_dir . '/i18n';
     if (!file_exists($targetFile)) {
-        mkdir($targetFile);
+//        mkdir($targetFile);
+        BUtil::ensureDir($targetFile);
     }
     $targetFile .= '/de.php';
     if (!file_exists($targetFile)) {
@@ -25,3 +28,4 @@ foreach ($modules as $modName => $mod) {
     chmod($targetFile, 0777);
     BLocale::collectTranslations($viewDir, $targetFile);
 }
+echo "Done" . PHP_EOL;
