557ac8c94c8c0e0956d9f70846e65a10a981b027
pdtkame <pdtkame@gmail.com>
2013-11-21 16:31:05 +0700

    update admin grid

--- FCom/CatalogIndex/Admin/Controller/Fields.php
+++ FCom/CatalogIndex/Admin/Controller/Fields.php
@@ -13,24 +13,48 @@ class FCom_CatalogIndex_Admin_Controller_Fields extends FCom_Admin_Controller_Ab
     {
 	    $fieldHlp = FCom_CatalogIndex_Model_Field::i();
         $config = parent::gridConfig();
-        $config['columns'] += array(
-            'id'           => array('index' => 'idxf.id'),
-            'field_name'   => array('label' => 'Name', 'index' => 'idxf.field_name', 'href' => BApp::href('catalogindex/fields/form/?id=:id')),
-            'field_label'  => array('label' => 'Label', 'index' => 'idxf.field_label'),
-            'field_type'   => array('label' => 'Type', 'index' => 'idxf.field_type', 'options'=>$fieldHlp->fieldOptions('field_type'), 'width'=>80),
-            'filter_type'  => array('label' => 'Filter', 'index' => 'idxf.filter_type', 'options'=>$fieldHlp->fieldOptions('filter_type'), 'width'=>80),
-            'filter_multivalue'  => array('label' => 'MultiValue', 'index' => 'idxf.filter_multivalue', 'options'=>$fieldHlp->fieldOptions('filter_multivalue'), 'width'=>80),
-            'filter_counts'  => array('label' => 'Calc Counts', 'index' => 'idxf.filter_counts', 'options'=>$fieldHlp->fieldOptions('filter_counts'), 'width'=>80),
-            'filter_show_empty'  => array('label' => 'Show Empty', 'index' => 'idxf.filter_show_empty', 'options'=>$fieldHlp->fieldOptions('filter_show_empty'), 'width'=>80),
-            'filter_order'  => array('label' => 'Filter Order', 'index' => 'idxf.filter_order', 'width'=>80),
-            'filter_custom_view' => array('label' => 'Filter Custom View', 'index' => 'idxf.filter_custom_view', 'width'=>80, 'hidden'=>true),
-            'search_type'  => array('label' => 'Search', 'index' => 'idxf.search_type', 'options'=>$fieldHlp->fieldOptions('search_type'), 'width'=>80),
-            'sort_type'  => array('label' => 'Sort', 'index' => 'idxf.sort_type', 'options'=>$fieldHlp->fieldOptions('sort_type'), 'width'=>80),
-            'sort_label'  => array('label' => 'Sort Label', 'index' => 'idxf.sort_label', 'width'=>80),
-            'sort_order'  => array('label' => 'Sort Order', 'index' => 'idxf.sort_order', 'width'=>80),
-            'source_type'  => array('label' => 'Source', 'index' => 'idxf.source_type', 'options'=>$fieldHlp->fieldOptions('source_type'), 'width'=>80),
-            'source_callback' => array('label' => 'Source Callback', 'index' => 'idxf.source_callback', 'width'=>80, 'hidden'=>true),
-            '_actions' => array('label' => 'Actions', 'sortable' => false),
+        $config['columns'] = array(
+            array('cell' => 'select-row', 'headerCell' => 'select-all', 'width' => 40),
+            array('name' => 'id', 'label' => 'ID', 'index' => 'idxf.id'),
+            array('name' => 'field_name', 'label' => 'Name', 'index' => 'idxf.field_name', 'href' => BApp::href('catalogindex/fields/form/?id=:id')),
+            array('name' => 'field_label', 'label' => 'Label', 'index' => 'idxf.field_label'),
+            array('name' => 'field_type', 'label' => 'Type', 'index' => 'idxf.field_type', 'options'=>$fieldHlp->fieldOptions('field_type'), 'width'=>80),
+            array('name' => 'filter_type', 'label' => 'Filter', 'index' => 'idxf.filter_type', 'options'=>$fieldHlp->fieldOptions('filter_type'), 'width'=>80),
+            array('name' => 'filter_multivalue', 'label' => 'MultiValue', 'index' => 'idxf.filter_multivalue', 'editable' => true, 'mass-editable' => true,
+                  'options' => $fieldHlp->fieldOptions('filter_multivalue'), 'width' => 80, 'editor' => 'select'),
+            array('name' => 'filter_counts', 'label' => 'Calc Counts', 'index' => 'idxf.filter_counts',
+                  'options'=>$fieldHlp->fieldOptions('filter_counts'), 'width'=>80),
+            array('name' => 'filter_show_empty', 'label' => 'Show Empty', 'index' => 'idxf.filter_show_empty',
+                  'options'=>$fieldHlp->fieldOptions('filter_show_empty'), 'width'=>80),
+            array('name' => 'filter_order', 'label' => 'Filter Order', 'index' => 'idxf.filter_order'),
+            array('name' => 'filter_custom_view', 'label' => 'Filter Custom View', 'index' => 'idxf.filter_custom_view', 'width'=>80, 'hidden'=>true),
+            array('name' => 'search_type', 'label' => 'Search', 'index' => 'idxf.search_type',
+                  'options'=>$fieldHlp->fieldOptions('search_type'), 'width'=>80),
+            array('name' => 'sort_type', 'label' => 'Sort', 'index' => 'idxf.sort_type',
+                  'options'=>$fieldHlp->fieldOptions('sort_type'), 'width'=>80),
+            array('name' => 'sort_label', 'label' => 'Sort Label', 'index' => 'idxf.sort_label', 'width'=>80),
+            array('name' => 'sort_order', 'label' => 'Sort Order', 'index' => 'idxf.sort_order', 'width'=>80),
+            array('name' => 'source_type', 'label' => 'Source', 'index' => 'idxf.source_type', 'options'=>$fieldHlp->fieldOptions('source_type'), 'width'=>80),
+            array('name' => 'source_callback', 'label' => 'Source Callback', 'index' => 'idxf.source_callback', 'width'=>80, 'hidden'=>true),
+            array('name' => '_actions', 'label' => 'Actions', 'sortable' => false,
+                  'data' => array('edit' => array('href' => BApp::href($this->_formViewName.'?id='), 'col' => 'id'), 'delete' => true)
+            ),
+        );
+        $config['actions'] = array(
+            'edit' => true,
+            'delete' => true
+        );
+        $config['filters'] = array(
+            array('field' => 'field_name', 'type' => 'text'),
+            array('field' => 'field_label', 'type' => 'text'),
+            array('field' => 'field_type', 'type' => 'select'),
+            array('field' => 'filter_type', 'type' => 'select'),
+            array('field' => 'filter_multivalue', 'type' => 'select'),
+            array('field' => 'filter_counts', 'type' => 'select'),
+            array('field' => 'filter_show_empty', 'type' => 'select'),
+            array('field' => 'search_type', 'type' => 'select'),
+            array('field' => 'sort_type', 'type' => 'select'),
+            array('field' => 'source_type', 'type' => 'select'),
         );
         return $config;
     }
