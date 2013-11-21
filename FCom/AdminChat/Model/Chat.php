8cd26ae3e46a745d3e27e98672c12e76ba75a489
Boris Gurvich <boris@fulleron.com>
2013-11-21 01:35:54 -0800

    Changed format of BModel::set() $flag argument to string Fixed quickview in frontend category/search pages Fixed manifest cache in PRODUCTION mode Added empty $where argument in BModel::update_many() Added Force Reindex button  in Index Fields

--- FCom/AdminChat/Model/Chat.php
+++ FCom/AdminChat/Model/Chat.php
@@ -133,8 +133,8 @@ class FCom_AdminChat_Model_Chat extends FCom_Core_Model_Abstract
     {
         if (!parent::onBeforeSave()) return false;
 
-        $this->set('status', 'active', null);
-        $this->set('create_at', BDb::now(), null);
+        $this->set('status', 'active', 'IFNULL');
+        $this->set('create_at', BDb::now(), 'IFNULL');
         $this->set('update_at', BDb::now());
 
         return true;
--- FCom/AdminChat/Model/UserStatus.php
+++ FCom/AdminChat/Model/UserStatus.php
@@ -63,7 +63,7 @@ class FCom_AdminChat_Model_UserStatus extends FCom_Core_Model_Abstract
     {
         if (!parent::onBeforeSave()) return false;
 
-        $this->set('create_at', BDb::now(), null);
+        $this->set('create_at', BDb::now(), 'IFNULL');
         $this->set('update_at', BDb::now());
 
         return true;
