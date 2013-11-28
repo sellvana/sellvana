70e77d23854799e2877014c87d335f93b0183af2
pdtkame <pdtkame@gmail.com>
2013-11-28 11:22:07 +0700

    update process in role/group form

--- FCom/CustomerGroups/Model/Group.php
+++ FCom/CustomerGroups/Model/Group.php
@@ -4,8 +4,7 @@
  * @project fulleron
  */
 
-class FCom_CustomerGroups_Model_Group
-    extends FCom_Core_Model_Abstract
+class FCom_CustomerGroups_Model_Group extends FCom_Core_Model_Abstract
 {
     protected static $_table = "fcom_customer_groups";
     protected static $_origClass = __CLASS__;
