5d4bc2301c42c9c1d3abd6153f4a10d182f133ad
Boris Gurvich <boris@fulleron.com>
2013-12-01 19:35:55 -0800

    Added FCom_Admin_Model_Aggregate Fixed order of admin menu items Added Local SKU and Base Price required Fixed Admin > Settings error

---
+++ FCom/Admin/Model/Aggregate.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @param
+ */
+class FCom_Admin_Model_Aggregate extends FCom_Core_Model_Abstract
+{
+    protected static $_table = 'fcom_aggregate';
+    protected static $_origClass = __CLASS__;
+
+}
