1711b667a1bb9d8b6c00b3988a70b1a6e015a6da
pdtkame <pdtkame@gmail.com>
2013-12-03 15:11:59 +0700

    use modal for emails form

--- FCom/Email/Admin/Controller/Subscriptions.php
+++ FCom/Email/Admin/Controller/Subscriptions.php
@@ -15,32 +15,17 @@ class FCom_Email_Admin_Controller_Subscriptions extends FCom_Admin_Controller_Ab
         $config['columns'] = array(
             array('cell' => 'select-row', 'headerCell' => 'select-all', 'width' => 40),
             array('name' => 'id', 'label' => 'ID', 'index' => 'e.id'),
-            array('name' => 'email', 'label' => 'Email', 'index' => 'e.email'),
-            array('name'          => 'unsub_all',
-                  'label'         => 'Un-subscribe all',
-                  'index'         => 'e.unsub_all',
-                  'editable'      => true,
-                  'mass-editable' => true,
-                  'options'       => array('1' => 'Yes', '0' => 'No'),
-                  'editor'        => 'select'
-            ),
-            array('name'          => 'sub_newsletter',
-                  'label'         => 'Subscribe newsletter',
-                  'index'         => 'e.sub_newsletter',
-                  'editable'      => true,
-                  'mass-editable' => true,
-                  'options'       => array('1' => 'Yes', '0' => 'No'),
-                  'editor'        => 'select'
-            ),
+            array('name' => 'email', 'label' => 'Email', 'index' => 'e.email', 'addable' => true, 'editable' => true,
+                  'validation' => array('required' => true, 'unique' => BApp::href('subscriptions/unique'))),
+            array('name' => 'unsub_all', 'label' => 'Un-subscribe all', 'index' => 'e.unsub_all', 'addable' => true, 'editable' => true,
+                  'mass-editable' => true, 'options' => array('1' => 'Yes', '0' => 'No'), 'editor' => 'select'),
+            array('name' => 'sub_newsletter', 'label' => 'Subscribe newsletter', 'index' => 'e.sub_newsletter', 'addable' => true,
+                  'editable' => true, 'mass-editable' => true, 'options' => array('1' => 'Yes', '0' => 'No'), 'editor' => 'select'),
             array('name' => 'create_at', 'label' => 'Created', 'index' => 'e.create_at'),
-            array(
-                'name'     => '_actions',
-                'label'    => 'Actions',
-                'sortable' => false,
-                'data'     => array('edit' => array('href' => BApp::href('subscriptions/form?id='), 'col' => 'id'), 'delete' => true)
-            ),
+            array('name' => '_actions', 'label' => 'Actions', 'sortable' => false, 'data' => array('edit' => true, 'delete' => true)),
         );
         $config['actions'] = array(
+            'new' => array('caption' => 'New Email Subscription', 'modal' => true),
             'export' => true,
             'edit'   => true,
             'delete' => true
@@ -50,5 +35,19 @@ class FCom_Email_Admin_Controller_Subscriptions extends FCom_Admin_Controller_Ab
             array('field' => 'sub_newsletter', 'type' => 'select'),
         );
         return $config;
+    }
+
+    public function gridViewBefore($args)
+    {
+        parent::gridViewBefore($args);
+        $this->view('admin/grid')->set(array( 'actions' => array( 'new' => '')));
+    }
+
+    public function action_unique__POST()
+    {
+        $post = BRequest::i()->post();
+        $data = each($post);
+        $rows = BDb::many_as_array(FCom_Email_Model_Pref::i()->orm()->where($data['key'], $data['value'])->find_many());
+        BResponse::i()->json(array( 'unique' => empty($rows), 'id' => (empty($rows) ? -1 : $rows[0]['id'])));
     }
 }
