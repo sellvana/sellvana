7f0048b7063f0f4e6ca71cd58d0bfbc806e66415
Boris Gurvich <boris@fulleron.com>
2014-02-03 10:13:37 +0000

    fixed blog layouts; fixed login redirect_to; added hash to marketclient setup url

--- FCom/Frontend/Controller/Abstract.php
+++ FCom/Frontend/Controller/Abstract.php
@@ -6,11 +6,17 @@ class FCom_Frontend_Controller_Abstract extends FCom_Core_Controller_Abstract
     public function action_unauthenticated()
     {
         $r = BRequest::i();
+
+        $redirect = $r->get('redirect_to');
+        if ($redirect==='CURRENT') {
+            $redirect = BRequest::i()->referrer();
+        }
+
         if ($r->xhr()) {
-            BSession::i()->data('login_orig_url', $r->referrer());
+            BSession::i()->data('login_orig_url', $redirect ? $redirect : $r->referrer());
             BResponse::i()->json(array('error'=>'login'));
         } else {
-            BSession::i()->data('login_orig_url', $r->currentUrl());
+            BSession::i()->data('login_orig_url', $redirect ? $redirect : $r->currentUrl());
             $this->layout('/customer/login');
             BResponse::i()->status(401, 'Unauthorized'); // HTTP sic
         }
@@ -19,11 +25,17 @@ class FCom_Frontend_Controller_Abstract extends FCom_Core_Controller_Abstract
     public function action_unauthorized()
     {
         $r = BRequest::i();
+
+        $redirect = $r->get('redirect_to');
+        if ($redirect==='CURRENT') {
+            $redirect = BRequest::i()->referrer();
+        }
+
         if ($r->xhr()) {
-            BSession::i()->data('login_orig_url', $r->referrer());
+            BSession::i()->data('login_orig_url', $redirect ? $redirect : $r->referrer());
             BResponse::i()->json(array('error'=>'denied'));
         } else {
-            BSession::i()->data('login_orig_url', $r->currentUrl());
+            BSession::i()->data('login_orig_url', $redirect ? $redirect : $r->currentUrl());
             $this->layout('/denied');
             BResponse::i()->status(403, 'Forbidden');
         }
