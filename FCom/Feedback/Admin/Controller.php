1a4a2693b52f196ddb1856be611860e847b02e2f
Boris Gurvich <boris@fulleron.com>
2014-02-03 03:28:27 -0800

    Added FCom_Feedback module

---
+++ FCom/Feedback/Admin/Controller.php
@@ -0,0 +1,6 @@
+<?php
+
+class FCom_Feedback_Admin_Controller extends FCom_Admin_Controller_Abstract_GridForm
+{
+
+}
---
+++ FCom/Feedback/Controller.php
@@ -0,0 +1,35 @@
+<?php
+
+class FCom_Feedback_Controller extends FCom_Core_Controller_Abstract
+{
+    public function action_index__POST()
+    {
+        $r = BRequest::i();
+        $result = array();
+        try {
+            $data = BUtil::arrayMask($r->post('feedback'), 'name,email,comments');
+            $data['url'] = $r->referrer();
+            if (BConfig::i()->get('modules/FCom_Feedback/send_mod_versions')) {
+                foreach (BModuleRegistry::i()->getAllModules() as $modName => $mod) {
+                    if ($mod->run_level === 'LOADED') {
+                        $data['mod_versions'][$modName] = array(
+                            'version' => $mod->version,
+                            'channel' => $mod->channel,
+                        );
+                    }
+                }
+            }
+
+            $response = BUtil::remoteHttp('POST', 'https://www.sellvana.com/api/v1/feedback', BUtil::toJson($data));
+            $result = BUtil::fromJson($response);
+        } catch (Exception $e) {
+            $result['msg'] = $e->getMessage();
+            $result['error'] = true;
+        }
+        if ($r->xhr()) {
+            BResponse::i()->json($result);
+        } else {
+            BResponse::i()->redirect($r->referrer());
+        }
+    }
+}
