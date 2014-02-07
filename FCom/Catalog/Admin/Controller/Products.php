6141db70ff667a962e18fa6482f78ae498c53728
Tran Pham <pdtran@ceres.com.vn>
2014-02-07 15:05:09 -0800

    #SC-318: Issue when delete Product

--- FCom/Catalog/Admin/Controller/Products.php
+++ FCom/Catalog/Admin/Controller/Products.php
@@ -395,10 +395,12 @@ class FCom_Catalog_Admin_Controller_Products extends FCom_Admin_Controller_Abstr
         $this->processCategoriesPost($model);
         $this->processLinkedProductsPost($model, $data);
         $this->processMediaPost($model, $data);
-        $this->processCustomFieldPost($model, $data);
-        $this->processVariantPost($model, $data);
-        $this->processSystemLangFieldsPost($model, $data);
-        $this->processFrontendPost($model, $data);
+        if (!isset($data['do']) || $data['do'] !== 'DELETE') {
+            $this->processCustomFieldPost($model, $data);
+            $this->processVariantPost($model, $data);
+            $this->processSystemLangFieldsPost($model, $data);
+            $this->processFrontendPost($model, $data);
+        }
     }
 
     public function processCategoriesPost($model)
@@ -467,6 +469,11 @@ class FCom_Catalog_Admin_Controller_Products extends FCom_Admin_Controller_Abstr
                     }
                 }
             }
+        }
+        if (isset($data['do']) && $data['do'] === 'DELETE') {
+            $hlp->delete_many(array(
+                'product_id' => $model->id,
+            ));
         }
 //exit;
         return $this;
