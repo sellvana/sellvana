e9619be02e45fb57611ce4f9fc3e0b33b550b6de
Boris Gurvich <boris@fulleron.com>
2014-02-11 00:02:41 -0800

    Implemented Add Dashboard Widget functionality

--- FCom/Admin/Admin.php
+++ FCom/Admin/Admin.php
@@ -82,23 +82,27 @@ FCom.Admin.personalize_href = "'.BApp::href('my_account/personalize').'";
             'title' => 'Recent Orders',
             'icon' => 'inbox',
             'view' => 'dashboard/orders-list',
+            'async' => true,
         ));
         $view->addWidget('customers-list', array(
             'title' => 'Recent Customers',
             'icon' => 'group',
             'view' => 'dashboard/customers-list',
+            'async' => true,
         ));
         $view->addWidget('orders-totals', array(
             'title' => 'Order Totals',
             'icon' => 'inbox',
             'view' => 'dashboard/orders-totals',
             'cols' => 4,
+            'async' => true,
         ));
         $view->addWidget('visitors-totals', array(
             'title' => 'Visitors',
             'icon' => 'group',
             'view' => 'dashboard/visitors-totals',
             'cols' => 2,
+            'async' => true,
         ));
     }
 }
