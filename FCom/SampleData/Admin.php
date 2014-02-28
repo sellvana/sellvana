7078af39b7cec73e4fbb82e7a40250a06c99c9c9
Petar Dzhambazov <jamby77@gmail.com>
2014-03-01 01:33:15 +0200

    some more added, need to change products.csv column names

--- FCom/SampleData/Admin.php
+++ FCom/SampleData/Admin.php
@@ -8,7 +8,12 @@ class FCom_SampleData_Admin extends BClass
 {
     public static function bootstrap()
     {
-        if(BConfig::i()->get('modules/FCom_SampleData/load')){
+        FCom_Admin_Model_Role::i()->createPermission(
+                             array(
+                                 'sample_data' => 'Install Sample Data'
+                             )
+        );
+        if ( BConfig::i()->get( 'modules/FCom_SampleData/load' ) ) {
             FCom_SampleData_Model_Loader::loadProducts();
         }
     }
--- FCom/SampleData/Model/Loader.php
+++ FCom/SampleData/Model/Loader.php
@@ -7,25 +7,40 @@
 class FCom_SampleData_Model_Loader extends BClass
 {
     protected static $defaultProductDataFile = 'products.csv';
-    protected static $defaultDataPath = '../data';
+    protected static $defaultDataPath = 'data';
 
     public static function loadProducts()
     {
-        $file = BConfig::i()->get('modules/FCom_SampleData/sample_file');
-        if(!$file){
+        $file     = BConfig::i()->get( 'modules/FCom_SampleData/sample_file' );
+        $module   = BModuleRegistry::i()->currentModule();
+        $basePath = '';
+        if ( $module ) {
+            $basePath = $module->root_dir;
+        }
+        if ( !$file ) {
             $file = static::$defaultProductDataFile;
         }
 
-        $path = BConfig::i()->get('modules/FCom_SampleData/sample_path');
+        $path = BConfig::i()->get( 'modules/FCom_SampleData/sample_path' );
 
-        if(!$path){
-            $path = static::$defaultDataPath;
+        if ( !$path ) {
+            $path = $basePath . DIRECTORY_SEPARATOR . static::$defaultDataPath;
         }
 
         $fileName = rtrim( $path, '/' ) . DIRECTORY_SEPARATOR . ltrim( $file, '/' );
-        $fileName = str_replace('\\', '/', realpath( $fileName ));
+        $fileName = str_replace( '\\', '/', realpath( $fileName ) );
+        $fr       = fopen( $fileName, 'r' );
+        $headings = fgetcsv( $fr );
 
-        die($fileName);
+        $rows = array();
+
+        while ( $line = fgetcsv( $fr ) ) {
+            $row = array_combine( $headings, $line );
+            if ( $row ) {
+                $rows[ ] = $row;
+            }
+        }
+        FCom_Catalog_Model_Product::i()->import( $rows );
         // todo
     }
 }
