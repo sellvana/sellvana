e6cae3f8a544b453ff67591f2aee0edf236751d1
Tran Pham <pdtran@ceres.com.vn>
2014-03-03 16:02:03 +0700

    #SC-353: init

---
+++ FCom/Core/js/fcom/fcom.ipmode.js
@@ -0,0 +1,20 @@
+define(['backbone',  'jquery',  'select2'], function(Backbone, $) {
+    var ipMode = {};
+    ipMode.Views = Backbone.View.extend({
+        template: _.template($('#ip-mode-template').html()),
+        events: {
+            'click .remove-ip-mode': 'remove'
+        },
+        render: function (mode) {
+            this.setElement(this.template());
+            this.$el.find('select').select2().select2('val', mode);
+            return this;
+        },
+        remove: function () {
+            console.log('nguyen')
+            this.$el.remove();
+        }
+
+    });
+    return ipMode;
+})
