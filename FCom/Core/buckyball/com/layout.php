8c30a6b101f1a0494c9afefad41e849d2e409029
Boris Gurvich <boris@fulleron.com>
2014-04-03 20:51:00 +0000

    minor fix for LAYOUT.view.do debug log

--- FCom/Core/buckyball/com/layout.php
+++ FCom/Core/buckyball/com/layout.php
@@ -903,7 +903,7 @@ class BLayout extends BClass
             foreach ($d['do'] as $args) {
                 $method = array_shift($args);
                 BDebug::debug('LAYOUT.view.do ' . $method .
-                    (!empty($args[0]) ? (' ('.$args[0].(!empty($args[1]) ? (', '.json_encode($args[1])) : '').')') : ''));
+                    (!empty($args[0]) ? (' ('.print_r($args[0],1).(!empty($args[1]) ? (', '.json_encode($args[1])) : '').')') : ''));
                 call_user_func_array(array($view, $method), $args);
             }
         }
