ce0310627de9b8793b0baed7a1cb68a657088ebc
Boris Gurvich <boris@sellvana.com>
2014-04-04 17:29:30 -0700

    Refactored BApp::frontendHref(), BApp::adminHref()

--- FCom/Core/buckyball/com/core.php
+++ FCom/Core/buckyball/com/core.php
@@ -332,6 +332,39 @@ class BApp extends BClass
                . BRouting::processHref( $url );
     }
 
+    public static function adminHref($url = '')
+    {
+        static $baseAdminHref;
+        if (!$baseAdminHref) {
+            $conf = BConfig::i();
+            $r = BRequest::i();
+            $adminHref = $conf->get('web/admin_href');
+            if (!$adminHref) {
+                $adminHref = rtrim(BConfig::i()->get('web/base_store'), '/') . '/admin/index.php';
+                $conf->set('web/admin_href', $adminHref);
+            }
+            if (!BUtil::isUrlFull($adminHref)) {
+                $adminHref = $r->scheme() . '://' . $r->httpHost() . $adminHref;
+            }
+            $baseAdminHref = rtrim($adminHref, '/') . '/';
+        }
+        return $baseAdminHref . ltrim($url, '/');
+    }
+
+    public static function frontendHref($url = '')
+    {
+        static $baseStoreHref;
+        if (!$baseStoreHref) {
+            $r = BRequest::i();
+            $storeHref = BConfig::i()->get('web/base_store');
+            if (!BUtil::isUrlFull($storeHref)) {
+                $storeHref = $r->scheme() . '://' . $r->httpHost() . $storeHref;
+            }
+            $baseStoreHref = rtrim($storeHref, '/') . '/';
+        }
+        return $baseStoreHref . ltrim($url, '/');
+    }
+
     /**
      * Shortcut to generate URL with base src (js, css, images, etc)
      *
