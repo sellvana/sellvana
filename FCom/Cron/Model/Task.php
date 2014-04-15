dc969e9e0214fd0c3b085677345ff98e5154191c
Petar Dzhambazov <jamby77@gmail.com>
2014-04-16 00:06:32 +0300

    adding ie profiles 1

--- FCom/Cron/Model/Task.php
+++ FCom/Cron/Model/Task.php
@@ -4,4 +4,7 @@ class FCom_Cron_Model_Task extends FCom_Core_Model_Abstract
 {
     protected static $_origClass = __CLASS__;
     protected static $_table = 'fcom_cron_task';
+    protected static $_importExportProfile = array(
+        'unique_key' => array( 'handle', ),
+    );
 }
