f4568a347644db942680b9ab7c6ce4bc3d53129b
Tran Pham <pdtran@ceres.com.vn>
2014-05-21 17:58:42 +0700

    #SC-372: Implement Stock System Settings

--- FCom/Stock/Model/Sku.php
+++ FCom/Stock/Model/Sku.php
@@ -4,4 +4,11 @@ class FCom_Stock_Model_Sku extends FCom_Core_Model_Abstract
 {
     static protected $_table = 'fcom_stock_sku';
     static protected $_origClass = __CLASS__;
+    public static function backOrders()
+    {
+        return [
+            "NOT_BACK_ORDERS"         => BLocale::i()->_("No Back Orders"),
+            "ALLOW_QUANTITY_BELOW" => BLocale::i()->_("Allow Quantity Below 0")
+        ];
+    }
 }
