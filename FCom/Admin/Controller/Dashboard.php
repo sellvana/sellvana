b863ae6a3dd451110bbf6f639483a0b13422f870
Tran Pham <pdtran@ceres.com.vn>
2014-06-05 15:25:31 +0700

    #HK-13428: add validate when filter on dashboard

--- FCom/Admin/Controller/Dashboard.php
+++ FCom/Admin/Controller/Dashboard.php
@@ -57,44 +57,79 @@ class FCom_Admin_Controller_Dashboard extends FCom_Admin_Controller_Abstract
     public function action_data__POST()
     {
         $p = $this->BRequest->post();
-        $persData = $this->FCom_Admin_Model_User->personalize();
-        $persData['dashboard']['filter'] = $p;
-        if ($p['range'] == 'range') {
-            switch ($p['date']) {
-                case 'last-month':
-                    $p['min'] = date("Y-m-1", strtotime("last month"));
-                    $p['max'] = date("Y-m-t", strtotime("last month"));
-                    break;
-                case 'last-week':
-                    $p['min'] = date("Y-m-d", strtotime("last week"));
-                    $p['max'] = date("Y-m-d", strtotime("last week + 7 days"));
-                    break;
-                case 'today':
-                    $p['date'] = date("Y-m-d");
-                    break;
-                case 'all':
-                    break;
-                default:
-                    $tmp = explode('~', $p['date']);
-                    $p['min'] = $tmp[0];
-                    $p['max'] = $tmp[1];
-                    break;
-            }
-        }
-        $widgets = $this->FCom_Admin_View_Dashboard->getWidgets();
-        $result = [];
-        $this->FCom_Admin_Model_User->personalize($persData);
-        foreach ($widgets as $key => $widget) {
-            if (isset($widget['async']) && $widget['async'] == true
-                && isset($widget['filter']) && $widget['filter'] == true
-            ) {
-                if (!isset($widget['state']['closed']) || $widget['state']['closed']  == false) {
-                    $widget['async'] = false;
-                    $html = $this->view($widget['view'])->set('filter', $p)->render();
-                    $result[] = ['key' => $key, 'html' => $html];
+        if (!$this->validateDateTime()) {
+            $persData = $this->FCom_Admin_Model_User->personalize();
+            $persData['dashboard']['filter'] = $p;
+            if ($p['range'] == 'range') {
+                switch ($p['date']) {
+                    case 'last-month':
+                        $p['min'] = date("Y-m-1", strtotime("last month"));
+                        $p['max'] = date("Y-m-t", strtotime("last month"));
+                        break;
+                    case 'last-week':
+                        $p['min'] = date("Y-m-d", strtotime("last week"));
+                        $p['max'] = date("Y-m-d", strtotime("last week + 7 days"));
+                        break;
+                    case 'today':
+                        $p['date'] = date("Y-m-d");
+                        break;
+                    case 'all':
+                        break;
+                    default:
+                        $tmp = explode('~', $p['date']);
+                        $p['min'] = $tmp[0];
+                        $p['max'] = $tmp[1];
+                        break;
                 }
             }
+            $widgets = $this->FCom_Admin_View_Dashboard->getWidgets();
+            $result = [];
+            $this->FCom_Admin_Model_User->personalize($persData);
+            foreach ($widgets as $key => $widget) {
+                if (isset($widget['async']) && $widget['async'] == true
+                    && isset($widget['filter']) && $widget['filter'] == true
+                ) {
+                    if (!isset($widget['state']['closed']) || $widget['state']['closed']  == false) {
+                        $widget['async'] = false;
+                        $html = $this->view($widget['view'])->set('filter', $p)->render();
+                        $result[] = ['key' => $key, 'html' => $html];
+                    }
+                }
+            }
+        } else {
+            $result = ['error' => true];
         }
+
         $this->BResponse->json($result);
+    }
+
+    public function validateDateTime()
+    {
+        $p = $this->BRequest->post();
+        $error = false;
+        if (!in_array($p['range'], ['range', 'not_range'])) {
+            $error = true;
+        }
+        switch ($p['date']) {
+            case 'last-month':case 'last-week':case 'today': case 'all':
+                if (empty($p['is_btn_filter'])) {
+                    $error = true;
+                }
+            break;
+            default:
+                $tmp = explode('~', $p['date']);
+                if (($p['range'] == 'range' && count($tmp) < 2) || ($p['range'] == 'not_range' && count($tmp) > 1)) {
+                    $error = true;
+                }
+                if (!$error) {
+                    foreach ($tmp as $value) {
+                        if (!preg_match('/^\d{4}[\/\-]\d{1,2}[\/\-]\d{1,2}$/', $value)) {
+                            $error = true;
+                        }
+                    }
+                }
+                break;
+        }
+        return $error;
     }
 }
