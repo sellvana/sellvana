12f5c91f73d89b06ff623bcc6596d507214d55e6
Petar Dzhambazov <jamby77@gmail.com>
2014-06-12 10:11:33 +0300

    $this cannot be used here

--- FCom/PushServer/index.php
+++ FCom/PushServer/index.php
@@ -2,12 +2,11 @@
 
 $rootDir = dirname(dirname(__DIR__));
 require_once $rootDir . '/FCom/Core/Main.php';
-
-$webRoot = $this->BRequest->webRoot(2);
-$this->BConfig
+$webRoot = BRequest::i()->webRoot(2);
+BConfig::i()
     ->set('fs/root_dir', $rootDir)
     ->set('web/base_href', $webRoot)
 ;
 
-$this->BDebug->mode('DEVELOPMENT');
-$this->FCom_Core_Main->run('FCom_PushServer');
+BDebug::i()->mode('DEVELOPMENT');
+FCom_Core_Main::i()->run('FCom_PushServer');
