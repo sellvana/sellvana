a543463a825deb2f55eee11b58081cb033352914
pdtkame <pdtkame@gmail.com>
2014-06-30 15:13:53 +0700

    tadd phpdoc for all class in core

--- FCom/Core/buckyball/com/db.php
+++ FCom/Core/buckyball/com/db.php
@@ -1801,15 +1801,57 @@ class BORM extends ORMWrapper
  * ORM model base class
  * @property static string $_table
  * @property static array $_fieldOptions
+ *
+ * DI
+ * core
  * @property BApp $BApp
+ * @property BException $BException
  * @property BConfig $BConfig
+ * @property BClassRegistry $BClassRegistry
+ * @property BClassAutoload $BClassAutoload
  * @property BEvents $BEvents
  * @property BSession $BSession
+ *
+ * controller
  * @property BRequest $BRequest
  * @property BResponse $BResponse
+ * @property BRouting $BRouting
+ *
+ * layout
  * @property BLayout $BLayout
+ * @property BView $BView
+ * @property BViewEmpty $BViewEmpty
+ * @property BViewHead $BViewHead
+ * @property BViewList $BViewList
+ *
+ * db
+ * @property BDb $BDb
+ *
+ * locale
+ * @property BLocale $BLocale
+ *
+ * module
+ * @property BModuleRegistry $BModuleRegistry
+ * @property BModule $BModule
+ * @property BMigrate $BMigrate
+ * @property BDbModule $BDbModule
+ *
+ * cache
+ * @property BCache $BCache
+ *
+ * misc
  * @property BUtil $BUtil
+ * @property BHTML $BHTML
+ * @property BEmail $BEmail
+ * @property BValue $BValue
+ * @property BData $BData
  * @property BDebug $BDebug
+ * @property BLoginThrottle $BLoginThrottle
+ * @property BYAML $BYAML
+ * @property BValidate $BValidate
+ * @property BValidateViewHelper $BValidateViewHelper
+ * @property Bcrypt $Bcrypt
+ * @property BRSA $BRSA
  */
 class BModel extends Model
 {
@@ -2130,7 +2172,8 @@ class BModel extends Model
     * Create a new instance of the model
     *
     * @param null|array $data
-    * @return BModel
+    * @param boolean $new is new record
+    * @return $this
     */
     public static function create($data = null, $new = true)
     {
