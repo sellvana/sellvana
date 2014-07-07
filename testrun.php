329706e7cc3a1b1e83f1ac9ca6afc5b0c47cadc6
Petar Dzhambazov <jamby77@gmail.com>
2014-07-07 23:58:06 +0300

    some more tweaks bdebug disabled for tests, not to polute the results

--- testrun.php
+++ testrun.php
@@ -7,6 +7,8 @@ require_once 'FCom/Core/Main.php';
 BConfig::i()->set('fs/root_dir', __DIR__);
 FCom_Core_Main::i()->init('FCom_Test');
 BModuleRegistry::i()->bootstrap();
+
+BDebug::i()->mode(BDebug::MODE_DISABLED);
 /** @var FCom_Test_Admin_Controller_Tests $runner */
 $runner = FCom_Test_Admin_Controller_Tests::i();
 $tests = $runner->collectTests();
