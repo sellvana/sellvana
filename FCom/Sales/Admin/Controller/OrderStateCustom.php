a32f0769d77e2b824bbaa8e7a6c1b1d4dab0823f
bane <programer.master@gmail.com>
2014-12-16 00:49:07 +0100

    order custom state add/edit dialog

--- FCom/Sales/Admin/Controller/OrderStateCustom.php
+++ FCom/Sales/Admin/Controller/OrderStateCustom.php
@@ -19,17 +19,18 @@ class FCom_Sales_Admin_Controller_OrderStateCustom extends FCom_Admin_Controller
                 'id'     => 'state-custom',
                 'caption' => 'State Custom',
                 'data_url' => $this->BApp->href('/orderstatecustom/grid_data'),
+                'edit_url' => $this->BApp->href('/orderstatecustom/grid_data'),
                 'orm' => $orm,
                 'columns' => [
                     ['type' => 'row_select'],
-                    ['name' => 'id', 'index' => 'oscs.id', 'label' => 'ID', 'width' => 70],
-                    ['name' => 'entity_type', 'index' => 'sc.entity_type', 'label' => 'Entity Type','width' => 15, 'editable' => true,
+                    ['name' => 'id', 'index' => 'oscs.id', 'label' => 'ID', 'width' => 40],
+                    ['name' => 'entity_type', 'index' => 'sc.entity_type', 'label' => 'Entity Type','width' => 85, 'addable'=>true,'editable' => true,
                         'validation' => ['required' => true]],
-                    ['name' => 'state_code', 'index' => 'sc.state_code', 'label' => 'Code', 'width' => 20, 'editable' => true,
+                    ['name' => 'state_code', 'index' => 'sc.state_code', 'label' => 'Code', 'width' =>  150, 'addable'=>true, 'editable' => true,
                         'validation' => ['required' => true, 'unique' => $this->BApp->href('orderstatecustom/unique')]],
-                    ['name' => 'state_label', 'index' => 'sc.state_label', 'label' => 'Label' ,'width' => 50, 'editable' => true,    
+                    ['name' => 'state_label', 'index' => 'sc.state_label', 'label' => 'Label' ,'width' => 150, 'addable'=>true, 'editable' => true,    
                         'validation' => ['required' => true,'unique' => $this->BApp->href('orderstatecustom/unique')]],
-                    ['name' => 'concrete_class', 'index' => 'sc.concrete_class', 'label' => 'Concrecte Class', 'width' => 100, 'editable' => true,    
+                    ['name' => 'concrete_class', 'index' => 'sc.concrete_class', 'label' => 'Concrecte Class', 'width' => 150, 'addable'=>true, 'editable' => true,    
                         'validation' => ['required' => true]],
                     ['type' => 'btn_group', 'buttons' => [['name' => 'edit'], ['name' => 'delete']]]
                 ],
@@ -64,6 +65,11 @@ class FCom_Sales_Admin_Controller_OrderStateCustom extends FCom_Admin_Controller
             ['c' => $data['state']['c']],
             $this->BDb->many_as_array($data['rows']),
         ]);
+    }
+    
+    public function action_grid_data__POST()
+    {
+        $this->_processGridDataPost('FCom_Sales_Model_StateCustom');
     }
 
     /**
