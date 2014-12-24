d8415419d38422d34b0762e43d26b4ee06d8ceb7
Tran Pham <pdtkame@gmail.com>
2014-12-24 12:25:42 +0700

    add component for each type of filter

--- FCom/LibJsCommon/jsx/fcom.gridFilter.jsx
+++ FCom/LibJsCommon/jsx/fcom.gridFilter.jsx
@@ -30,7 +30,7 @@ define(['underscore', 'react'], function (_, React) {
                         that.filter(e);
                     }
                 }).on('click', '.f-grid-filter button.filter-text-sub', function (e) {
-                    $(this).parents('div.dropdown').addClass('open');
+                    that.keepShowDropDown(this);
                 }).on('click', 'ul.filter-sub a.filter_op', function (e) {
                     var operator = $(e.target);
                     var text = operator.html();
@@ -44,9 +44,21 @@ define(['underscore', 'react'], function (_, React) {
                 })
             ;
         },
+        /**
+         * keep parents dropdown still be shown
+         * @param ele
+         */
+        keepShowDropDown: function(ele) {
+            $(ele).parents('div.dropdown').addClass('open');
+        },
         handleChange: function(event) {
             this.props.changeFilter(event.target.value);
         },
+        /**
+         * get display name from columns metadata
+         * @param field
+         * @returns {*}
+         */
         getFieldName: function(field) {
             var row = _.findWhere(this.props.getConfig('columns'), {name: field});
             return row ? row.label : field;
@@ -62,7 +74,12 @@ define(['underscore', 'react'], function (_, React) {
             });
 
             this.setState({filters: filters});
+            this.keepShowDropDown(event.target);
         },
+        /**
+         * filter data
+         * @param event
+         */
         filter: function (event) {
             console.log('doing filter');
         },
@@ -72,8 +89,7 @@ define(['underscore', 'react'], function (_, React) {
             var filters = this.state.filters;
 
             //quick search
-            var quickSearchId = id + '-quick-search';
-            var quickSearch = <input type="text" className="f-grid-quick-search form-control" placeholder={this.props.placeholderText} id={quickSearchId} />;
+            var quickSearch = <input type="text" className="f-grid-quick-search form-control" placeholder={this.props.placeholderText} onChange={this.handleChange} id={id + '-quick-search'} />;
 
             var filterSettingNodes = filters.map(function(f, index) {
                 return (
@@ -90,67 +106,21 @@ define(['underscore', 'react'], function (_, React) {
             });
 
             var filterSettings = (
-                <span className="FCom_CustomerGroups_Admin_Controller_CustomerGroups dropdown">
+                <div className={id + ' dropdown'} style={{"display" : "inline-block"}}>
                     <a data-toggle="dropdown" className="btn dropdown-toggle showhide_columns">
                         Filters <b className="caret"></b>
                     </a>
-                    <ul className="FCom_CustomerGroups_Admin_Controller_CustomerGroups dd-list dropdown-menu filters ui-sortable">
+                    <ul className={id + " dd-list dropdown-menu filters ui-sortable"}>
                         {filterSettingNodes}
                     </ul>
-                </span>
+                </div>
             );
 
-            var operators = <ul className="dropdown-menu filter-sub">
-                <li>
-                    <a className="filter_op" data-id="contains" href="#">contains</a>
-                </li>
-                <li>
-                    <a className="filter_op" data-id="not" href="#">does not contain</a>
-                </li>
-                <li>
-                    <a className="filter_op" data-id="equal" href="#">is equal to</a>
-                </li>
-                <li>
-                    <a className="filter_op" data-id="start" href="#">start with</a>
-                </li>
-                <li>
-                    <a className="filter_op" data-id="end" href="#">end with</a>
-                </li>
-            </ul>;
-
-            var filterList = filters.map(function(f, index) {
+            var filterNodes = filters.map(function(f, index) {
                 if (!f.show) {
                     return false;
                 }
-                return (
-                    <div className="btn-group dropdown f-grid-filter">
-                        <button className="btn dropdown-toggle filter-text-main" data-toggle="dropdown">
-                            <span className="f-grid-filter-field">{that.getFieldName(f.field)}</span>:
-                            <span className="f-grid-filter-value"> All </span>
-                            <span className="caret"></span>
-                        </button>
-                        <ul className="dropdown-menu filter-box">
-                            <li>
-                                <div className="input-group">
-                                    <div className="input-group-btn dropdown">
-                                        <button className="btn btn-default dropdown-toggle filter-text-sub" data-toggle="dropdown">
-                                            Contains
-                                            <span className="caret"></span>
-                                        </button>
-                                        {operators}
-                                    </div>
-                                    <input type="text" className="form-control" value="" />
-                                    <div className="input-group-btn">
-                                        <button type="button" className="btn btn-primary update" onClick={this.filter}>
-                                            Update
-                                        </button>
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
-                        <abbr className="select2-search-choice-close"></abbr>
-                    </div>
-                );
+                return (<FComFilterNodeContainer filter={f} getFieldName={that.getFieldName} setFilter={that.filter} />);
             });
 
             return (
@@ -159,17 +129,121 @@ define(['underscore', 'react'], function (_, React) {
                         {quickSearch}
                         {filterSettings}
                     </div>
-                    <span className="FCom_CustomerGroups_Admin_Controller_CustomerGroups f-filter-btns">
-                        {filterList}
+                    <span className={id + " f-filter-btns"}>
+                        {filterNodes}
                     </span>
                 </div>
             );
         }
     });
 
-    var FComFilterOperations = React.createClass({
+    var FComFilterNodeContainer = React.createClass({
+        getDefaultProps: function() {
+            return {
+                'filter': {}
+            };
+        },
         render: function() {
-            return (<div></div>);
+            if (typeof this.props.filter === 'undefined') {
+                return false;
+            }
+
+            var filter = this.props.filter;
+            var node = null;
+
+            switch (filter.type) {
+                case 'text':
+                    node = <FComFilterText {...this.props} />;
+                    break;
+                case 'date-range':
+                    node = <FComFilterDateRange {...this.props} />;
+                    break;
+                case 'number-range':
+                    node = <FComFilterNumberRange {...this.props} />;
+                    break;
+                case 'multiselect':
+                    node = <FComFilterMultiSelect {...this.props} />;
+                    break;
+                case 'select':
+                    node = <FComFilterSelect {...this.props} />;
+                    break;
+            }
+
+            return node;
+        }
+    });
+
+    var FComFilterText = React.createClass({
+        render: function() {
+            return (
+                <div className="btn-group dropdown f-grid-filter">
+                    <button className="btn dropdown-toggle filter-text-main" data-toggle="dropdown">
+                        <span className="f-grid-filter-field">{this.props.getFieldName(this.props.filter.field)}</span>:
+                        <span className="f-grid-filter-value"> All </span>
+                        <span className="caret"></span>
+                    </button>
+                    <ul className="dropdown-menu filter-box">
+                        <li>
+                            <div className="input-group">
+                                <div className="input-group-btn dropdown">
+                                    <button className="btn btn-default dropdown-toggle filter-text-sub" data-toggle="dropdown">
+                                        Contains
+                                        <span className="caret"></span>
+                                    </button>
+                                    <ul className="dropdown-menu filter-sub">
+                                        <li>
+                                            <a className="filter_op" data-id="contains" href="#">contains</a>
+                                        </li>
+                                        <li>
+                                            <a className="filter_op" data-id="not" href="#">does not contain</a>
+                                        </li>
+                                        <li>
+                                            <a className="filter_op" data-id="equal" href="#">is equal to</a>
+                                        </li>
+                                        <li>
+                                            <a className="filter_op" data-id="start" href="#">start with</a>
+                                        </li>
+                                        <li>
+                                            <a className="filter_op" data-id="end" href="#">end with</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <input type="text" className="form-control" value="" />
+                                <div className="input-group-btn">
+                                    <button type="button" className="btn btn-primary update" onClick={this.props.setFilter}>
+                                        Update
+                                    </button>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                    <abbr className="select2-search-choice-close"></abbr>
+                </div>
+            );
+        }
+    });
+
+    var FComFilterDateRange = React.createClass({
+        render: function() {
+            return null;
+        }
+    });
+
+    var FComFilterNumberRange = React.createClass({
+        render: function() {
+            return null;
+        }
+    });
+
+    var FComFilterMultiSelect = React.createClass({
+        render: function() {
+            return null;
+        }
+    });
+
+    var FComFilterSelect = React.createClass({
+        render: function() {
+            return null;
         }
     });
 
