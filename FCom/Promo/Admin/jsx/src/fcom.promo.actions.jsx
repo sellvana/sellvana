1333f896980fe057f0a22671a8fed858f99d475b
Petar Dzhambazov <jamby77@gmail.com>
2015-02-10 01:50:44 +0200

    fetching shipping methods and category names on form load

--- FCom/Promo/Admin/jsx/src/fcom.promo.actions.jsx
+++ FCom/Promo/Admin/jsx/src/fcom.promo.actions.jsx
@@ -1055,7 +1055,22 @@ define(['react', 'jquery', 'jsx!fcom.components', 'jsx!fcom.promo.common', 'fcom
                 multiple: true,
                 query: self.select2query,
                 dropdownAutoWidth: true,
-                initSelection: self.initSelection
+                initSelection: function (el, callback) {
+                    //var data = [];
+                    var val = el.val();
+
+                    $.get(self.url, {methods: val}).done(function (result) {
+                        //console.log(result);
+                        callback(result.items);
+                    });
+
+                    //var val = el.val().split(",");
+                    //for (var i in val) {
+                    //    var val2 = val[i];
+                    //    data.push({id: val2, text: val2});
+                    //}
+                    //callback(data);
+                }
             }).on('change', this.onChange);
         },
         onChange: function () {
