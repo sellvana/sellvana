bb58064b9e92abfe172ca57e9f6ca0b76bef8a72
Tran Pham <pdtkame@gmail.com>
2015-02-27 11:26:31 +0700

    convert and use js instead jsx

--- FCom/Core/jsx/build/fcom.griddle.js
+++ FCom/Core/jsx/build/fcom.griddle.js
@@ -1,6 +1,6 @@
 /** @jsx React.DOM */
 
-define(['underscore', 'react', 'jquery', 'jsx!griddle.fcomGridBody', 'jsx!griddle.fcomGridFilter', 'fcom.components', 'jsx!griddle', 'backbone', 'bootstrap'],
+define(['underscore', 'react', 'jquery', 'griddle.fcomGridBody', 'griddle.fcomGridFilter', 'fcom.components', 'griddle.custom', 'backbone', 'bootstrap'],
 function (_, React, $, FComGridBody, FComFilter, Components, Griddle, Backbone) {
 
     var dataUrl,
