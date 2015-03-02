230da88b84d54fec7ac0c0ea1c24d709000e0d0b
Tran Pham <pdtkame@gmail.com>
2015-03-02 11:15:20 +0700

    griddle: use pre-template to test import items from grid to grid in product form

--- core/Sellvana/Catalog/Admin/Controller/Products.php
+++ core/Sellvana/Catalog/Admin/Controller/Products.php
@@ -350,6 +350,29 @@ class Sellvana_Catalog_Admin_Controller_Products extends FCom_Admin_Controller_A
     }
 
     /**
+     * //todo: remove after finish griddle
+     * temporary function to build griddle, will be removed after test
+     * @param $model
+     * @return array
+     */
+    public function productImagesGridConfigForGriddle($model)
+    {
+        $config = $this->productImagesGridConfig($model);
+        unset($config['config']['actions']['add']);
+        $config['config']['actions'] += [
+            'add-images' => [
+                'caption'  => 'Add images',
+                'type'     => 'button',
+                'id'       => 'add-image-from-grid',
+                'class'    => 'btn-primary',
+                'callback' => 'showModalToAddImage'
+            ]
+        ];
+
+        return $config;
+    }
+
+    /**
      * modal grid on category/product tab
      * @param $model Sellvana_Catalog_Model_Product
      * @return array
