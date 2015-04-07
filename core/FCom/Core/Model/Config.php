1bc19f709a21ef51d38df5b4fd0ceae27c782bcb
Petar Dzhambazov <jamby77@gmail.com>
2015-04-07 22:11:16 +0300

    Core models updated

--- core/FCom/Core/Model/Config.php
+++ core/FCom/Core/Model/Config.php
@@ -4,6 +4,9 @@ class FCom_Core_Model_Config extends FCom_Core_Model_Abstract
 {
     protected static $_table = 'fcom_core_config';
     protected $_instance_id_column = 'path';
+    protected static $_importExportProfile = [
+        'unique_key' => ['path'],
+    ];
 
     public function fetch($path)
     {
