d1bd50b2bfdaea4d43f2b1f49652c91681d8f3d2
Tran Pham <pdtkame@gmail.com>
2015-04-22 22:40:01 +0700

    working Product form -> Variants tab: variant fields

--- core/Sellvana/CustomField/Admin/Controller/Products.php
+++ core/Sellvana/CustomField/Admin/Controller/Products.php
@@ -100,7 +100,10 @@ class Sellvana_CustomField_Admin_Controller_Products extends FCom_Admin_Controll
                 'actions' => [
                    'delete' => ['caption' => 'Remove']
                 ],
-                'grid_before_create' => 'variantFieldGridRegister'
+                'grid_before_create' => 'variantFieldGridRegister',
+                'callbacks' => [
+                    'componentDidMount' => 'variantFieldGriddleRegister'
+                ]
             ]
         ];
 
