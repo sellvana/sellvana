b4bc1b812004ebbaaf5d41e75d74f6e4a7e74460
Boris Gurvich <boris@fulleron.com>
2015-04-30 17:43:42 +0000

    Fixed frontend Qty inputs as type=number Fixed frontend product compare page Fixed using link to get to product compare page as oppose to form POST Fixed not showing "You Save %" when MAP is triggered

--- core/Sellvana/Catalog/Model/Product.php
+++ core/Sellvana/Catalog/Model/Product.php
@@ -1346,7 +1346,7 @@ class Sellvana_Catalog_Model_Product extends FCom_Core_Model_Abstract
             'final_price' => &$finalPrice,
         ]);
 
-        if ($finalPrice !== null && $finalPrice < $basePrice) {
+        if ($finalPrice !== null && $finalPrice < $basePrice && (!$mapPriceModel || $finalPrice > $mapPrice)) {
             if (!empty($msrpPrice)) {
                 $basePrice = max($basePrice, $msrpPrice);
             }
