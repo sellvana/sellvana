c12fbea453ead87e7c3ac7bd4fdd8e7ee9f622c3
Tran Pham <pdtkame@gmail.com>
2015-05-29 11:27:58 +0700

    Griddle grid: fix Search within results doesn't work in grids with remote data

--- core/FCom/LibJsCommon/jsx/griddle/build/griddle.custom.js
+++ core/FCom/LibJsCommon/jsx/griddle/build/griddle.custom.js
@@ -639,11 +639,8 @@ var Griddle = React.createClass({displayName: "Griddle",
         var filteredResults = originalResults;
 
         _.each(filters, function(filter, key) { //key is field name
-
             if (filter.submit == true && typeof filter.val != 'undefined') {
                 var filterVal = filter.val.toLowerCase();
-
-                console.log('filter:', filter,'key:', key);
 
                 switch (filter.type) {
                     case 'text':
@@ -824,9 +821,10 @@ var Griddle = React.createClass({displayName: "Griddle",
                 };
 
             var results = this.state.results;
-            //console.log('state.filter', this.state.filter, ' within total:',results.length, 'total');
-            if (this.state.filter != '' && ! this.hasExternalResults()) { //if have filter, need to filter data then search in results
+            //console.log('state.filter', this.state.filter);
+            if (this.state.filter != '' && ! this.hasExternalResults() ) { //if have filter, need to filter data then search in results
                 results = this.filterLocalData(null, JSON.parse(this.state.filter));
+                //console.log('results before search', results);
             }
 
             state.filteredResults = _.filter(results,
