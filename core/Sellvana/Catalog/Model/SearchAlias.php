f84a3d12d13bd5a2fc657eccb688a1a32833204f
Boris Gurvich <boris@gurvich.me>
2015-07-05 21:33:24 -0700

    Added stubs for CustomerSegments and GiftCards modules Added multisite user roles Added target_url to search aliases for redirect on search

--- core/Sellvana/Catalog/Model/SearchAlias.php
+++ core/Sellvana/Catalog/Model/SearchAlias.php
@@ -4,9 +4,20 @@ class Sellvana_Catalog_Model_SearchAlias extends FCom_Core_Model_Abstract
 {
     static protected $_table = 'fcom_search_alias';
     static protected $_origClass = __CLASS__;
-    protected static $_importExportProfile = ['skip' => ['id', 'create_at', 'update_at'],];
 
-    public function processSearchQuery($query)
+    const TYPE_FULL = 'F', TYPE_WORD = 'W';
+
+    protected static $_importExportProfile = ['skip' => ['id', 'create_at', 'update_at']];
+
+    protected static $_fieldOptions = [
+        'alias_type' => [
+            self::TYPE_FULL => 'Full',
+            self::TYPE_WORD => 'Word',
+        ],
+    ];
+
+
+    public function fetchSearchAlias($query)
     {
         $sData =& $this->BSession->dataToUpdate();
         if (!empty($sData['search_alias'][$query])) {
@@ -17,10 +28,10 @@ class Sellvana_Catalog_Model_SearchAlias extends FCom_Core_Model_Abstract
         $record = $this->loadWhere($data);
         if (!$record) {
             $sData['search_alias'][$query] = $query;
-            return $query;
+            return [];
         }
         $record->add('num_hits')->save();
         $sData['search_alias'][$query] = $record->get('target_term');
-        return $record->get('target_term');
+        return $record;
     }
 }
