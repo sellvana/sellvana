4417373de89366f453c6b1ebb0b1ac45452d16fc
Petar Dzhambazov <jamby77@gmail.com>
2015-10-13 00:44:52 +0300

    dropdown options pulled in php

--- core/Sellvana/Rewards/Model/Rule.php
+++ core/Sellvana/Rewards/Model/Rule.php
@@ -6,13 +6,20 @@
  */
 class Sellvana_Rewards_Model_Rule extends FCom_Core_Model_Abstract
 {
-    protected static $_table     = 'fcom_rewards_rule';
-    protected static $_origClass = __CLASS__;
+    protected static $_table        = 'fcom_rewards_rule';
+    protected static $_origClass    = __CLASS__;
     protected static $_fieldOptions = [
         'conditions_operator' => [
             "always" => "Apply Always",
             "all"    => "All Conditions Have to Match",
             "any"    => "Any Condition Has to Match",
+        ],
+        'conditions_options'  => [
+            "sku"         => "Products",
+            "category"    => "Categories",
+            "combination" => "Attribute Combination",
+            "total"       => "Cart Total",
+            "shipping"    => "Shipping Destination",
         ]
     ];
 }
