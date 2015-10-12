3f0e7f5077764308bf26e8ff69fd0251033d973d
Petar Dzhambazov <jamby77@gmail.com>
2015-10-13 00:03:31 +0300

    added conditions

--- core/Sellvana/Rewards/Model/Rule.php
+++ core/Sellvana/Rewards/Model/Rule.php
@@ -8,4 +8,11 @@ class Sellvana_Rewards_Model_Rule extends FCom_Core_Model_Abstract
 {
     protected static $_table     = 'fcom_rewards_rule';
     protected static $_origClass = __CLASS__;
+    protected static $_fieldOptions = [
+        'conditions_operator' => [
+            "always" => "Apply Always",
+            "all"    => "All Conditions Have to Match",
+            "any"    => "Any Condition Has to Match",
+        ]
+    ];
 }
