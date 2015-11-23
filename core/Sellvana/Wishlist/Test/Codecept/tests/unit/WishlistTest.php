e9aad66ee0b025c8af3207d850b6cebdfc9a3e70
Tran Pham <pdtkame@gmail.com>
2015-11-23 13:51:46 +0700

    Fixed typo on wishlist test

--- core/Sellvana/Wishlist/Test/Codecept/tests/unit/WishlistTest.php
+++ core/Sellvana/Wishlist/Test/Codecept/tests/unit/WishlistTest.php
@@ -1,12 +1,10 @@
 <?php
-namespace core\Sellvana\Wishlist;
-
-use Codeception\Util\Stub;
+namespace Sellvana\Wishlist;
 
 class WishlistTest extends \Codeception\TestCase\Test
 {
     /**
-     * @var \Wishlist\UnitTester
+     * @var \Sellvana\Wishlist\UnitTester
      */
     protected $tester;
 
@@ -33,11 +31,14 @@ class WishlistTest extends \Codeception\TestCase\Test
      * @license http://www.php.net/license/index.php#doc-lic
      * @license http://creativecommons.org/licenses/by/3.0/
      * @license CC-BY-3.0 <http://spdx.org/licenses/CC-BY-3.0>
+     *
+     * TODO: Bring it to BUtil class
      */
-    function xml2array ( $xmlObject, $out = array () )
+    function xml2array($xmlObject, $out = [])
     {
-        foreach ( (array) $xmlObject as $index => $node )
-            $out[$index] = ( is_object ( $node ) ) ? xml2array ( $node ) : $node;
+        foreach ((array)$xmlObject as $index => $node) {
+            $out[$index] = is_object($node) ? xml2array($node) : $node;
+        }
 
         return $out;
     }
@@ -55,17 +56,17 @@ class WishlistTest extends \Codeception\TestCase\Test
     public function testAddEntry()
     {
         $this->tester->seeNumRecords(2, 'fcom_wishlist');
-        $mWislist = \Sellvana_Wishlist_Model_Wishlist::i(true);
+        $mWishlist = \Sellvana_Wishlist_Model_Wishlist::i(true);
         $data = ['customer_id' => 3, 'title' => 'test'];
-        $mWislist->create($data)->save();
+        $mWishlist->create($data)->save();
         $this->tester->seeNumRecords(3, 'fcom_wishlist');
         $this->tester->seeInDatabase('fcom_wishlist', ['customer_id' => 3, 'title' => 'test']);
     }
 
     public function testAddItem()
     {
-        $mWislist = \Sellvana_Wishlist_Model_Wishlist::i(true);
-        $wishlist = $mWislist->load(1);
+        $mWishlist = \Sellvana_Wishlist_Model_Wishlist::i(true);
+        $wishlist = $mWishlist->load(1);
         $wishlist->addItem(4);
 
         $this->tester->seeNumRecords(4, 'fcom_wishlist_items');
@@ -75,8 +76,8 @@ class WishlistTest extends \Codeception\TestCase\Test
     public function testRemoveItem()
     {
         $this->tester->seeNumRecords(3, 'fcom_wishlist_items');
-        $mWislist = \Sellvana_Wishlist_Model_Wishlist::i(true);
-        $wishlist = $mWislist->load(1);
+        $mWishlist = \Sellvana_Wishlist_Model_Wishlist::i(true);
+        $wishlist = $mWishlist->load(1);
         $this->assertEquals(2, count($wishlist->items()), "Count items before remove");
         $wishlist->removeProduct(1);
         $this->assertEquals(1, count($wishlist->items()), "Count items after remove");
@@ -87,8 +88,8 @@ class WishlistTest extends \Codeception\TestCase\Test
     public function testClearWishlist()
     {
         $this->tester->seeNumRecords(2, 'fcom_wishlist');
-        $mWislist = \Sellvana_Wishlist_Model_Wishlist::i(true);
-        $wishlist = $mWislist->load(1);
+        $mWishlist = \Sellvana_Wishlist_Model_Wishlist::i(true);
+        $wishlist = $mWishlist->load(1);
         $this->assertEquals(2, count($wishlist->items()), "Items count is not correct");
 
         foreach ($wishlist->items() as $item) {
@@ -101,10 +102,10 @@ class WishlistTest extends \Codeception\TestCase\Test
     public function testSessionWishListReturnsValidModel()
     {
         \BSession::i()->set('customer_id', 3); // set session user
-        /** @var Sellvana_Wishlist_Model_Wishlist $mWislist */
-        $mWislist = \Sellvana_Wishlist_Model_Wishlist::i(true);
-        /** @var Sellvana_Wishlist_Model_Wishlist $sessionWhishlist */
-        $sessionWhishlist = $mWislist->sessionWishlist(true);
+        /** @var \Sellvana_Wishlist_Model_Wishlist $mWishlist */
+        $mWishlist = \Sellvana_Wishlist_Model_Wishlist::i(true);
+        /** @var \Sellvana_Wishlist_Model_Wishlist $sessionWhishlist */
+        $sessionWhishlist = $mWishlist->sessionWishlist(true);
 
         $this->assertNotEmpty($sessionWhishlist->id());
         \BSession::i()->set('customer_id', null); // set session user
