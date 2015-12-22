fb72ca687055f4fafd901e39343646c031868abc
Tran Pham <pdtkame@gmail.com>
2015-12-22 10:56:06 +0700

    Optimized multi-language component also fixed some minor bugs

--- core/FCom/Core/jsx/build/fcom.language.js
+++ core/FCom/Core/jsx/build/fcom.language.js
@@ -1,8 +1,9 @@
-/** @jsx React.DOM */
-
+/**
+ * @jsx React.DOM
+ *
+ * FCom Multi Languages Component
+ */
 define(['underscore', 'react', 'jquery', 'fcom.griddle', 'fcom.components', 'griddle.fcomSelect2', 'fcom.locale'], function (_, React, $, FComGriddleComponent, Components, FComSelect2, Locale) {
-
-    var LANGAPP = {};
 
     var LangFields = React.createClass({displayName: "LangFields",
         mixins: [FCom.Mixin],
@@ -10,8 +11,11 @@ define(['underscore', 'react', 'jquery', 'fcom.griddle', 'fcom.components', 'gri
 
         },
         removeLangField: function (e) {
-            var code = $(e.currentTarget).data('code');
-            $(LANGAPP).trigger('removeLangField', [code]);
+            this.props.removeField($(e.currentTarget).data('code'));
+        },
+        handleChange: function (e) {
+            $input = $(e.currentTarget);
+            this.props.setLangVal($input.data('code'), $input.val());
         },
         render: function () {
             var that = this;
@@ -24,10 +28,14 @@ define(['underscore', 'react', 'jquery', 'fcom.griddle', 'fcom.components', 'gri
                                     React.createElement("span", {className: "badge badge-default"}, lang.lang_code)
                                 ), 
                                 React.createElement("div", {className: "col-md-6"}, 
-                                    React.createElement("input", {type: "text", className: "form-control", "data-type": "lang", "data-code": lang.lang_code, "data-rule-required": "true", name: guid(), defaultValue: lang.value})
+                                    React.createElement("input", {type: "text", className: "form-control", "data-type": "lang", 
+                                           onBlur: that.handleChange, 
+                                           "data-code": lang.lang_code, "data-rule-required": "true", name: that.props.id, 
+                                           defaultValue: lang.value})
                                 ), 
                                 React.createElement("div", {className: "col-md-3"}, 
-                                    React.createElement("button", {type: "button", onClick: that.removeLangField, "data-code": lang.lang_code, className: "btn btn-default btn-sm field-remove"}, 
+                                    React.createElement("button", {type: "button", onClick: that.removeLangField, "data-code": lang.lang_code, 
+                                            className: "btn btn-default btn-sm field-remove"}, 
                                         React.createElement("i", {className: "icon-remove"})
                                     )
                                 )
@@ -73,66 +81,69 @@ define(['underscore', 'react', 'jquery', 'fcom.griddle', 'fcom.components', 'gri
             });
         },
         componentDidMount: function () {
-            var that = this;
-            var modalConfig = this.props.modalConfig;
-
-            $(LANGAPP).on('setSelection', function (e, selection) {
-                that.setSelection(selection);
-            });
-
-            $(LANGAPP).on('removeLangField', function (e, code) {
-                that.removeLangField(code);
-            });
-
-            if (this.props.select2Callback) {
-                window[this.props.select2Callback] = function (e, selection) {
-                    $(LANGAPP).trigger('setSelection', [selection]);
-                };
-            }
+            this.props.tmpAvailLangs = _.clone(this.state.availLangs);
+            this.props.tmpDefaultLangs = _.clone(this.state.defaultLangs);
         },
         componentWillUnmount: function () {
-            $(LANGAPP).off('setSelection');
-            $(LANGAPP).off('removeLangField');
+            //
         },
         componentDidUpdate: function (prevProps, prevState) {
             // Reset selection
             this.state.selection = null;
         },
         getModalConfig: function () {
-            var config = $.extend({}, {
+            return $.extend({}, {
                 title: Locale._('Multi Languages'),
                 confirm: Locale._('Save Change'),
                 cancel: Locale._('Close'),
                 show: false,
                 id: this.props.id + '-modal',
                 onLoad: null,
-                onConfirm: null,
-                onCancel: null,
+                onConfirm: this.confirmEditLangs,
+                onCancel: this.cancelEditLangs,
                 ref: this.props.id + '-modal'
             }, this.props.modalConfig);
+        },
+        confirmEditLangs: function (modal) {
+            var modalConfig = this.props.modalConfig;
 
-            if (config.onConfirm && typeof config.onConfirm === 'string') {
-                config.onConfirm = window[config.onConfirm];
+            this.props.tmpAvailLangs = _.clone(this.state.availLangs);
+            this.props.tmpDefaultLangs = _.clone(this.state.defaultLangs);
+
+            if (modalConfig.onSaved && typeof modalConfig.onSaved === 'string') {
+                window[modalConfig.onSaved](modal);
+            }
+        },
+        cancelEditLangs: function (modal) {
+            this.setState({
+                availLangs: this.props.tmpAvailLangs,
+                defaultLangs: this.props.tmpDefaultLangs
+            });
+
+            var modalConfig = this.props.modalConfig;
+            if (modalConfig.onCanceled && typeof modalConfig.onCanceled === 'string') {
+                window[modalConfig.onCanceled](modal);
             }
 
-            return config;
+            modal.close();
         },
         getModalNode: function () {
-            return $('#'+ this.props.id + '-modal');
+            return $('#' + this.props.id + '-modal');
         },
         getSelect2Config: function () {
             return $.extend({}, {
                 id: 'multilang-' + this.props.id,
                 name: 'multilang-' + this.props.id,
-                className: ''
+                className: '',
+                placeholder: Locale._("Select languages"),
+                multiple: false
             }, this.props.select2Config);
         },
-        handleSelect2Change: function (event, callback, selections) {
+        handleSelect2Change: function (event, callback, selection) {
             if (typeof window[callback] === 'function') {
-                return window[callback](event, selections);
+                window[callback](e, selection);
             }
-        },
-        setSelection: function(selection) {
+
             this.state.selection = selection.text;
         },
         getDefaultLangs: function () {
@@ -157,18 +168,23 @@ define(['underscore', 'react', 'jquery', 'fcom.griddle', 'fcom.components', 'gri
             }
 
             var availLangs = this.state.availLangs;
-            _(availLangs).each(function(lang, i) {
+            _(availLangs).each(function (lang, i) {
                 if (lang.lang_code == code) delete availLangs[i];
             });
 
             this.setState({
-                defaultLangs: _.sortBy(defaultLangs, 'id'),
-                availLangs: _.sortBy(availLangs, 'lang_code')
+                defaultLangs: defaultLangs,
+                availLangs: availLangs
             });
         },
-        AddLocaleField: function(e) {
+        AddLocaleField: function (e) {
             if (null === this.state.selection) {
-                $.bootstrapGrowl(Locale._("Please choose language."), { type: 'warning', align: 'center', width: 'auto', delay: 3000 });
+                $.bootstrapGrowl(Locale._("Please choose language."), {
+                    type: 'warning',
+                    align: 'center',
+                    width: 'auto',
+                    delay: 3000
+                });
                 return;
             }
 
@@ -181,6 +197,12 @@ define(['underscore', 'react', 'jquery', 'fcom.griddle', 'fcom.components', 'gri
 
             this.forceUpdate();
         },
+        setLangVal: function (code, value) {
+            var langs = this.state.availLangs;
+            _(langs).each(function (lang, i) {
+                if (lang.lang_code == code) lang.value = value;
+            });
+        },
         showModal: function () {
             this.getModalNode().modal('show');
         },
@@ -190,15 +212,18 @@ define(['underscore', 'react', 'jquery', 'fcom.griddle', 'fcom.components', 'gri
 
             if (this.state.availLangs.length) {
                 var langIds = _.pluck(this.state.availLangs, 'lang_code');
-                this.props.btnLangLabel = langIds.filter(function (item) { return item != undefined }).join(',');
+                this.props.btnLangLabel = langIds.filter(function (item) {
+                    return item != undefined
+                }).join(',');
             }
 
             return (
                 React.createElement("div", {key: this.props.id, className: "row multilang-field"}, 
                     React.createElement("div", {className: "col-md-2"}), 
                     React.createElement("div", {className: "col-md-5"}, 
-                        React.createElement("button", {type: "button", style: {marginTop: '5px', marginBottom: '10px'}, onClick: this.showModal, 
-                                className: "btn btn-xs multilang " + (this.props.btnLangLabel ? 'btn-info' : '')}, !this.props.btnLangLabel ? React.createElement("i", {className: "icon icon-globe"}) : '', " ", this.props.btnLangLabel || Locale._('Translate')
+                        React.createElement("button", {type: "button", style: {marginBottom: '10px'}, onClick: this.showModal, 
+                                className: "btn btn-xs multilang " + (this.props.btnLangLabel ? 'btn-info' : '')}, !this.props.btnLangLabel ?
+                            React.createElement("i", {className: "icon icon-globe"}) : '', " ", this.props.btnLangLabel || Locale._('Translate')
                         )
                     ), 
                     React.createElement(Components.Modal, React.__spread({},  this.props.modalConfig), 
@@ -207,20 +232,21 @@ define(['underscore', 'react', 'jquery', 'fcom.griddle', 'fcom.components', 'gri
                                 React.createElement("tbody", null, 
                                 React.createElement("tr", null, 
                                     React.createElement("td", null, React.createElement(FComSelect2, React.__spread({},  inlineProps, {options: defaultLangs, 
-                                                                      multiple: this.props.multiple || false, 
-                                                                      placeholder: this.props.placeholder || Locale._("Select languages"), 
                                                                       onChange: this.handleSelect2Change, 
-                                                                      defaultValue: [], 
-                                                                      callback: this.props.select2Callback}))), 
+                                                                      defaultValue: []}))
+                                    ), 
                                     React.createElement("td", null, 
-                                        React.createElement("button", {className: "btn btn-primary", onClick: this.AddLocaleField, type: "button"}, Locale._('Add Locale'))
+                                        React.createElement("button", {className: "btn btn-primary", onClick: this.AddLocaleField, 
+                                                type: "button"}, Locale._('Add Locale'))
                                     )
                                 )
                                 )
                             )
                         ), 
                         React.createElement("div", {className: this.props.id + '-container'}, 
-                            React.createElement(LangFields, {langs: this.state.availLangs})
+                            React.createElement(LangFields, {id: this.props.id, langs: this.state.availLangs, 
+                                        removeField: this.removeLangField, 
+                                        setLangVal: this.setLangVal})
                         )
                     )
                 )
