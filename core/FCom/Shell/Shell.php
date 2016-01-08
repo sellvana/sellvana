2f12b5fd92728209bdd1e417570d1905e5b04ab6
Tpymah <vadim.buchinsky@gmail.com>
2016-01-08 14:31:15 +0200

    Refactored shell colorizing added enable/disable colors, strip, stdin, stdout

--- core/FCom/Shell/Shell.php
+++ core/FCom/Shell/Shell.php
@@ -13,6 +13,9 @@ class FCom_Shell_Shell extends BClass
      */
     protected $_params = [];
 
+    /**
+     * @var array String foreground and background colors.
+     */
     static protected $_colorCodes = [
         'normal' => '0',
         'reset' => '0',
@@ -40,6 +43,9 @@ class FCom_Shell_Shell extends BClass
         'bg-white' => '47',
     ];
 
+    /**
+     * @var array String styling.
+     */
     static protected $_shortMods = [
         '.' => '0', // normal
         '/' => '0', // reset (normal)
@@ -49,23 +55,23 @@ class FCom_Shell_Shell extends BClass
         '!' => '7', // inverse front and back colors
     ];
 
+    /**
+     * Calculated colors regex
+     *
+     * @var string
+     */
     static protected $_colorsRegex;
 
+    /**
+     * Use colors
+     *
+     * @var bool
+     */
     static protected $_colorsEnabled;
 
     public function run()
     {
-        // colorize?
-        static::$_colorsEnabled = strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN';
-
-        // calculate colors regex
-        $mods = [];
-        foreach (static::$_shortMods as $m => $_) {
-            $mods[] = preg_quote($m, '#');
-        }
-        $modsRe = '[' . join('', $mods) . ']*';
-        $colorsRe = '(' . join('|', array_keys(static::$_colorCodes)) . ')';
-        static::$_colorsRegex = "#\\{({$modsRe})({$colorsRe}(;{$colorsRe})*)?({$modsRe})\\}#";
+        $this->initColors();
 
         // collect parameters and options from cli
         $this->_params = $GLOBALS['argv'];
@@ -160,6 +166,55 @@ class FCom_Shell_Shell extends BClass
     }
 
     /**
+     * Enable the use of coloring
+     *
+     * @return bool
+     */
+    public function enableColors()
+    {
+        static::$_colorsEnabled = strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN';
+
+        if (extension_loaded('posix') && !posix_isatty(STDOUT)) {
+            static::$_colorsEnabled = false;
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Disable the use of coloring
+     */
+    public function disableColors()
+    {
+        static::$_colorsEnabled = false;
+    }
+
+    /**
+     * Calculate colors regex
+     */
+    public function initColors()
+    {
+        if (null !== static::$_colorsRegex) {
+            return;
+        }
+
+        static::$_colorsEnabled = strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN';
+
+        if (extension_loaded('posix') && !posix_isatty(STDOUT)) {
+            static::$_colorsEnabled = false;
+        }
+
+        $mods = [];
+        foreach (static::$_shortMods as $m => $_) {
+            $mods[] = preg_quote($m, '#');
+        }
+        $modsRe = '[' . join('', $mods) . ']*';
+        $colorsRe = '(' . join('|', array_keys(static::$_colorCodes)) . ')';
+        static::$_colorsRegex = "#\\{({$modsRe})({$colorsRe}(;{$colorsRe})*)?({$modsRe})\\}#";
+    }
+
+    /**
      * Colorize a string for shell output
      *
      * Format examples:
@@ -173,6 +228,9 @@ class FCom_Shell_Shell extends BClass
      */
     public function colorize($string)
     {
+        if (null !== static::$_colorsRegex){
+            $this->initColors();
+        }
         return preg_replace_callback(static::$_colorsRegex, function($m) {
             if (!static::$_colorsEnabled) {
                 return '';
@@ -195,5 +253,45 @@ class FCom_Shell_Shell extends BClass
             }
             return "\033[" . join(';', $colors) . "m";
         }, $string);
+    }
+
+    /**
+     * Strip a string of ansi-control codes.
+     *
+     * @param string $string String to strip
+     * @return string
+     */
+    public function strip($string)
+    {
+        return preg_replace('/\033\[(\d+)(;\d+)*m/', '', $string);
+    }
+
+    /**
+     * Get from STDIN
+     *
+     * @param bool $raw If set to true, returns the raw string without trimming
+     * @return string
+     */
+    public function stdin($raw = false)
+    {
+        return $raw ? fgets(STDIN) : rtrim(fgets(STDIN), PHP_EOL);
+    }
+
+    /**
+     * Print to STDOUT.
+     *
+     * @param string $string
+     * @param bool $raw
+     * @param string $eol
+     * @return int
+     */
+    public function stdout($string, $raw = false, $eol = PHP_EOL)
+    {
+        $string .= $eol;
+        if ($raw) {
+            return fwrite(STDOUT, $string);
+        }  else {
+            return fwrite(STDOUT, $this->colorize($string));
+        }
     }
 }
