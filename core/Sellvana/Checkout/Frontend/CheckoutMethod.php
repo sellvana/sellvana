7e46824e58f24dd2b075c233ca61d8654988df1d
Boris Gurvich <boris@gurvich.me>
2016-01-23 11:21:05 -0800

    Fixed typo in previous commit

--- core/Sellvana/Checkout/Frontend/CheckoutMethod.php
+++ core/Sellvana/Checkout/Frontend/CheckoutMethod.php
@@ -14,7 +14,7 @@ class Sellvana_Checkout_Frontend_CheckoutMethod extends Sellvana_Sales_Method_Ch
             'href'  => $this->BApp->href('checkout'),
             'label' => 'Proceed to Checkout',
         ];
-        $cart = $this->Sellvana_Sales_Model_Cart->sessionCart()
+        $cart = $this->Sellvana_Sales_Model_Cart->sessionCart();
         if ($cart && $cart->hasUnavailableItems()) {
             $result['disabled'] = true;
             $result['label'] = 'Please remove unavailable items';
