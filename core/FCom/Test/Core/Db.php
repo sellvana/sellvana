4633de70ee7f8ae5958b3d6a9ca79ebc977aa490
Tran Pham <pdtkame@gmail.com>
2016-03-11 18:15:27 +0700

    Mobile detect test and fix codes

--- core/FCom/Test/Core/Db.php
+++ core/FCom/Test/Core/Db.php
@@ -3,11 +3,10 @@ namespace FCom\Test\Core;
 
 use Codeception\Exception\ModuleException;
 use Codeception\Exception\ModuleConfigException;
-use Codeception\Lib\Interfaces\Db as DbInterface;
 use Codeception\TestCase;
 use FCom_Test_Core_Sellvana as Driver;
 
-class Db extends \Codeception\Module implements DbInterface
+class Db extends \Codeception\Module\Db
 {
     /**
      * @api
@@ -24,10 +23,11 @@ class Db extends \Codeception\Module implements DbInterface
      * @var array
      */
     protected $config = [
-        'populate' => true,
-        'cleanup' => true,
+        'populate' => true, // Populate test db before each test
+        'cleanup' => true, // Cleanup test db after each test
         'reconnect' => false,
-        'dump' => null
+        'dump' => null, // Sql dump file for re-populate db
+        'auto_create_db' => false // Auto create test db while create dump if not available
     ];
 
     /**
@@ -49,12 +49,6 @@ class Db extends \Codeception\Module implements DbInterface
      * @var array
      */
     protected $requiredFields = [];
-
-    private function disconnect()
-    {
-        $this->dbh = null;
-        $this->driver = null;
-    }
 
     public function _before(TestCase $test)
     {
@@ -96,7 +90,7 @@ class Db extends \Codeception\Module implements DbInterface
 
                 $ld->make('*', $this->getDumpPath(), [
                         'raw' => true,
-                        'autocreate' => $this->config['auto_create_db'] ?: false
+                        'auto_create_db' => $this->config['auto_create_db'] ?: false
                     ]
                 );
             }
@@ -157,50 +151,10 @@ class Db extends \Codeception\Module implements DbInterface
         $this->dbh = $this->driver->getDbh();
     }
 
-    /**
-     *
-     *
-     * @throws ModuleConfigException
-     * @throws ModuleException
-     */
-    protected function cleanup()
+    private function disconnect()
     {
-        $dbh = $this->driver->getDbh();
-        if (!$dbh) {
-            throw new ModuleConfigException(
-                __CLASS__,
-                \BApp::i()->t('No connection to database. Remove this module from config if you don\'t need database repopulation')
-            );
-        }
-        try {
-            // don't clear database for empty dump
-            if (!count($this->sql)) {
-                return;
-            }
-            $this->driver->cleanup();
-        } catch (\Exception $e) {
-            throw new ModuleException(__CLASS__, $e->getMessage());
-        }
-    }
-
-    /**
-     * Load sql dump file for testing
-     *
-     * @throws ModuleException
-     */
-    protected function loadDump()
-    {
-        if (!$this->sql) {
-            return;
-        }
-        try {
-            $this->driver->load($this->sql);
-        } catch (\PDOException $e) {
-            throw new ModuleException(
-                __CLASS__,
-                $e->getMessage() . \BApp::i()->t("\nSQL query being executed: ") . $this->driver->sqlToRun
-            );
-        }
+        $this->dbh = null;
+        $this->driver = null;
     }
 
     /**
@@ -254,155 +208,5 @@ class Db extends \Codeception\Module implements DbInterface
         $this->config['dump'] = $dump;
 
         return $dump;
-    }
-
-    /**
-     * Store inserted rows to remove after testing
-     *
-     * @param $table
-     * @param array $row
-     * @param $id
-     */
-    private function addInsertedRow($table, array $row, $id)
-    {
-        $primaryKey = $this->driver->getPrimaryKey($table);
-        $primary = [];
-        if ($primaryKey) {
-            if ($id && count($primaryKey) === 1) {
-                $primary [$primaryKey[0]] = $id;
-            } else {
-                foreach ($primaryKey as $column) {
-                    if (isset($row[$column])) {
-                        $primary[$column] = $row[$column];
-                    } else {
-                        throw new \InvalidArgumentException(\BApp::i()->t("Primary key field ' . $column . ' is not set for table ") . $table);
-                    }
-                }
-            }
-        } else {
-            $primary = $row;
-        }
-
-        $this->insertedRows[] = [
-            'table' => $table,
-            'primary' => $primary,
-        ];
-    }
-
-    /**
-     * Remove records has been inserted before
-     */
-    protected function removeInserted()
-    {
-        foreach (array_reverse($this->insertedRows) as $row) {
-            try {
-                $this->driver->deleteQueryByCriteria($row['table'], $row['primary']);
-            } catch (\Exception $e) {
-                $this->debug(\BApp::i()->t("coudn't delete record ") . json_encode($row['primary']) . \BApp::i()->t(" from ") . $row['table']);
-            }
-        }
-        $this->insertedRows = [];
-    }
-
-    /**
-     * Check if record has been inserted
-     *
-     * @param $table
-     * @param array $criteria
-     */
-    public function seeInDatabase($table, $criteria = [])
-    {
-        $res = $this->countInDatabase($table, $criteria);
-        $this->assertGreaterThan(0, $res, \BApp::i()->t('No matching records found for criteria ') . json_encode($criteria) . \BApp::i()->t(' in table ') . $table);
-    }
-
-    /**
-     * Asserts that found number of records in database
-     *
-     * @param int    $expectedNumber      Expected number
-     * @param string $table    Table name
-     * @param array  $criteria Search criteria [Optional]
-     */
-    public function seeNumRecords($expectedNumber, $table, array $criteria = [])
-    {
-        $actualNumber = $this->countInDatabase($table, $criteria);
-        $this->assertEquals($expectedNumber, $actualNumber, \BApp::i()->t('The number of found rows (') . $actualNumber. \BApp::i()->t(') does not match expected number ') . $expectedNumber . \BApp::i()->t(' for criteria ') . json_encode($criteria) . \BApp::i()->t(' in table ') . $table);
-    }
-
-    /**
-     * Check record has not been inserted
-     *
-     * @param $table
-     * @param array $criteria
-     */
-    public function dontSeeInDatabase($table, $criteria = [])
-    {
-        $count = $this->countInDatabase($table, $criteria);
-        $this->assertLessThan(1, $count, \BApp::i()->t('Unexpectedly found matching records for criteria ') . json_encode($criteria) . \BApp::i()->t(' in table ') . $table);
-    }
-
-    /**
-     * Count rows in database
-     *
-     * @param string $table    Table name
-     * @param array  $criteria Search criteria [Optional]
-     *
-     * @return int
-     */
-    protected function countInDatabase($table, array $criteria = [])
-    {
-        return (int) $this->proceedSeeInDatabase($table, 'count(*)', $criteria);
-    }
-
-    protected function proceedSeeInDatabase($table, $column, $criteria)
-    {
-        $query = $this->driver->select($column, $table, $criteria);
-        $this->debugSection('Query', $query, json_encode($criteria));
-
-        $sth = $this->driver->executeQuery($query, array_values($criteria));
-
-        return $sth->fetchColumn();
-    }
-
-    public function grabFromDatabase($table, $column, $criteria = [])
-    {
-        return $this->proceedSeeInDatabase($table, $column, $criteria);
-    }
-
-    /**
-     * Inserts SQL record into database. This record will be erased after the test.
-     *
-     * @param string $table
-     * @param array $data
-     *
-     * @return integer $id
-     */
-    public function haveInDatabase($table, array $data)
-    {
-        $query = $this->driver->insert($table, $data);
-        $this->debugSection('Query', $query);
-
-        $this->driver->executeQuery($query, array_values($data));
-
-        try {
-            $lastInsertId = (int)$this->driver->lastInsertId($table);
-        } catch (\PDOException $e) {
-            // ignore errors due to uncommon DB structure,
-            // such as tables without _id_seq in PGSQL
-            $lastInsertId = 0;
-        }
-
-        $this->addInsertedRow($table, $data, $lastInsertId);
-
-        return $lastInsertId;
-    }
-
-    /**
-     * Manual make connection if reconnect is false
-     *
-     * @throws ModuleException
-     */
-    public function createConnection() {
-        $this->connect();
     }
 }
--- core/Sellvana/Sales/Test/tests/unit/OrderTest.php
+++ core/Sellvana/Sales/Test/tests/unit/OrderTest.php
@@ -11,7 +11,7 @@
 class OrderTest extends \Codeception\TestCase\Test
 {
     /**
-     * @var \Sellvana\Wishlist\UnitTester
+     * @var \Sellvana\Sales\UnitTester
      */
     protected $tester;
 
@@ -34,11 +34,67 @@ class OrderTest extends \Codeception\TestCase\Test
         } else die('__ERROR__');
     }
 
+    public function testOrderStateProcess()
+    {
+        $this->tester->seeNumRecords(3, 'fcom_sales_order');
+
+        /** @var Sellvana_Sales_Model_Order $order1 */
+        $order1 = Sellvana_Sales_Model_Order::i()->load(1);
+        /** @var Sellvana_Sales_Model_Cart_State_Overall $state */
+        $state = $order1->state()->overall()->setArchived();
+        $this->tester->assertEquals(Sellvana_Sales_Model_Cart_State_Overall::ARCHIVED, $state->getValue(), 'Order overall state is incorrect');
+        $this->tester->assertEquals(Sellvana_Sales_Model_Cart_State_Overall::ARCHIVED, $state->setDefaultState()->getValue(), 'Order state default is incorrect');
+
+        $this->tester->assertSame($order1, $state->getModel(), 'Load model fail.');
+
+        /** @var Sellvana_Sales_Model_Order $order2 */
+        $order2 = Sellvana_Sales_Model_Order::i()->load(2);
+        /** @var Sellvana_Sales_Model_Cart_State_Payment $state */
+        $state = $order2->state()->payment()->setFree();
+        $this->tester->assertEquals(Sellvana_Sales_Model_Order_State_Payment::FREE, $state->getValue(), 'Order payment state is incorrect');
+        $this->tester->assertEquals(Sellvana_Sales_Model_Order_State_Payment::FREE, $state->setDefaultState()->getValue(), 'Order state default is incorrect');
+
+        $this->tester->assertSame($order2, $state->getModel(), 'Load model fail.');
+
+        /** @var Sellvana_Sales_Model_Order $order3 */
+        $order3 = Sellvana_Sales_Model_Order::i()->load(3);
+        /** @var Sellvana_Sales_Model_Cart_State_Payment $state */
+        $state = $order3->state()->returns()->setProcessing();
+        $this->tester->assertEquals(Sellvana_Sales_Model_Order_State_Return::PROCESSING, $state->getValue(), 'Order payment state is incorrect');
+        $this->tester->assertEquals(Sellvana_Sales_Model_Order_State_Return::PROCESSING, $state->setDefaultState()->getValue(), 'Order state default is incorrect');
+
+        $this->tester->assertSame($order3, $state->getModel(), 'Load model fail.');
+
+        $this->tester->seeNumRecords(3, 'fcom_sales_order');
+    }
+
+    public function testGetCart()
+    {
+        $this->tester->seeNumRecords(3, 'fcom_sales_order');
+        /** @var Sellvana_Sales_Model_Order $order */
+        $order = Sellvana_Sales_Model_Order::i()->load(2);
+        $this->assertEquals(1, count($order->items()), "Before add failed");
+
+        $orderItem = ['order_id' => $order->id(), 'product_id' => 1, 'qty_ordered' => 1];
+        Sellvana_Sales_Model_Order_Item::i()->create($orderItem)->save();
+
+        $this->assertEquals(1, count($order->items()), "After add failed");
+
+        /** @var Sellvana_Sales_Model_Order_Item $testItem */
+        $testItem = Sellvana_Sales_Model_Order_Item::i();
+        $testItem->isItemExist($order->id(), 1);
+        $this->assertTrue(is_object($testItem), "Item exists failed");
+
+        $testItem = Sellvana_Sales_Model_Order_Item::i();
+        $testItem->isItemExist($order->id(), 111111);
+        $this->assertFalse(is_object($testItem), "Item not exists failed");
+    }
+
     public function testAddEntry()
     {
         $this->tester->seeNumRecords(2, 'fcom_sales_order');
 
-        $data = ['cart_id' => 3, 'customer_id' => 2];
+        $data = ['cart_id' => 3, 'customer_id' => 2, 'item_qty' => 5, 'subtotal' => 20];
         Sellvana_Sales_Model_Order::i()->create($data)->save();
 
         $this->tester->seeNumRecords(3, 'fcom_sales_order');
@@ -49,12 +105,12 @@ class OrderTest extends \Codeception\TestCase\Test
         $this->tester->seeNumRecords(2, 'fcom_sales_order');
         /** @var Sellvana_Sales_Model_Order $order */
         $order = Sellvana_Sales_Model_Order::i()->load(2);
-        $this->assertEquals(1, count($order->items()), "Before add failed");
+        $this->tester->assertEquals(1, count($order->items()), "Before add failed");
 
         $orderItem = ['order_id' => $order->id(), 'product_id' => 1, 'qty' => 1, 'total' => 10];
         Sellvana_Sales_Model_Order_Item::i()->create($orderItem)->save();
 
-        $this->assertEquals(2, count($order->items()), "After add failed");
+        $this->tester->assertEquals(2, count($order->items()), "After add failed");
     }
 
     public function testItemsExist()
@@ -63,24 +119,24 @@ class OrderTest extends \Codeception\TestCase\Test
 
         /** @var Sellvana_Sales_Model_Order $order */
         $order = Sellvana_Sales_Model_Order::i()->load(2);
-        $this->assertEquals(1, count($order->items()), "Before add failed");
+        $this->tester->assertEquals(1, count($order->items()), "Before add failed");
 
         $orderItem = ['order_id' => $order->id(), 'product_id' => 1, 'qty' => 1, 'total' => 10];
         Sellvana_Sales_Model_Order_Item::i()->create($orderItem)->save();
 
-        $this->assertEquals(2, count($order->items()), "After add failed");
+        $this->tester->assertEquals(2, count($order->items()), "After add failed");
 
         $testItem = Sellvana_Sales_Model_Order_Item::i()->isItemExist($order->id(), 1);
-        $this->assertTrue(is_object($testItem), "Item exists failed");
+        $this->tester->assertTrue(is_object($testItem), "Item exists failed");
 
         $testItem = Sellvana_Sales_Model_Order_Item::i()->isItemExist($order->id(), 111111);
-        $this->assertFalse(is_object($testItem), "Item not exists failed");
+        $this->tester->assertFalse(is_object($testItem), "Item not exists failed");
     }
 
     public function testAddPaymentMethod()
     {
         Sellvana_Sales_Main::i()->addPaymentMethod('paypal', 'Sellvana_PaymentPaypal_Frontend');
         $methods = Sellvana_Sales_Main::i()->getPaymentMethods();
-        $this->assertTrue(isset($methods['paypal']));
+        $this->tester->assertTrue(isset($methods['paypal']));
     }
 }
