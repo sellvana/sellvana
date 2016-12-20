87305ef55da0b8d2eb03b5b23d6e0c726f8c50ef
nadiavasylenko <nv@doitua.com>
2016-12-20 18:22:55 +0200

    order-edit-page - desktop version. In progress

--- core/Sellvana/Sales/AdminSPA/vue/page/sales/orders/form.js
+++ core/Sellvana/Sales/AdminSPA/vue/page/sales/orders/form.js
@@ -1,19 +1,49 @@
-define(['sv-app', 'sv-comp-form', 'text!sv-page-sales-orders-form-nav-tpl', 'text!sv-page-sales-orders-form-info-tpl'],
-    function (SvApp, SvCompForm, formNavTpl, formInfoTpl) {
+define(['sv-app', 'sv-comp-form', 'text!sv-page-sales-orders-form-nav-tpl', 'text!sv-page-sales-orders-form-info-tpl', 'text!sv-page-sales-orders-form-control-buttons-tpl', 'text!sv-page-sales-orders-form-general-info-tpl', 'text!sv-page-sales-orders-form-general-info-items-tpl', 'text!sv-page-sales-orders-form-general-info-main-info-tpl', 'text!sv-page-sales-orders-form-general-info-total-tpl'],
+	   function (SvApp, SvCompForm, formNavTpl, formInfoTpl, SvCompControlButtons, SvCompOrderGeneralInfo, SvCompOrderGeneralInfoItems, SvCompOrderGeneralInfoMain, SvCompOrderGeneralInfoTotal) {
 
     const FormNav = {
         template: formNavTpl
     };
 
-    const FormInfo = {
-        template: formInfoTpl
-    };
+	const FormInfo = {
+		template: formInfoTpl
+	};
+	
+	var ControlButtons = {
+		template: SvCompControlButtons
+	};
+	
+	var GeneralInfoItems = {
+		template: SvCompOrderGeneralInfoItems
+	};
+
+	var GeneralInfoMain = {
+		template: SvCompOrderGeneralInfoMain
+	};
+
+	var GeneralInfoTotal = {
+		template: SvCompOrderGeneralInfoTotal
+	};
+	
+	var GeneralInfo = {
+		template: SvCompOrderGeneralInfo,
+		components: {
+			'sv-page-sales-orders-form-general-info-items': GeneralInfoItems,
+			'sv-page-sales-orders-form-general-info-main-info': GeneralInfoMain,
+			'sv-page-sales-orders-form-general-info-total': GeneralInfoTotal,
+		}
+	};
+	
+	
 
     return {
         components: {
             'sv-comp-form': SvCompForm,
             'sv-page-sales-orders-form-nav': FormNav,
-            'sv-page-sales-orders-form-info': FormInfo
+            'sv-page-sales-orders-form-info': FormInfo,
+            'sv-page-sales-orders-form-control-buttons': ControlButtons,
+            'sv-page-sales-orders-form-general-info': GeneralInfo,
+			
         }
     };
 });
