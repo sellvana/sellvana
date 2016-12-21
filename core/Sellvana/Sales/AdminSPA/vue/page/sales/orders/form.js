f0ab4b89f7630412af11c6e576bba36eee9569ad
nadiavasylenko <nv@doitua.com>
2016-12-21 18:19:46 +0200

    order-comments - desktop version. In progress

--- core/Sellvana/Sales/AdminSPA/vue/page/sales/orders/form.js
+++ core/Sellvana/Sales/AdminSPA/vue/page/sales/orders/form.js
@@ -1,5 +1,17 @@
-define(['sv-app', 'sv-comp-form', 'text!sv-page-sales-orders-form-nav-tpl', 'text!sv-page-sales-orders-form-info-tpl', 'text!sv-page-sales-orders-form-control-buttons-tpl', 'text!sv-page-sales-orders-form-general-info-tpl', 'text!sv-page-sales-orders-form-general-info-items-tpl', 'text!sv-page-sales-orders-form-general-info-main-info-tpl', 'text!sv-page-sales-orders-form-general-info-total-tpl'],
-	   function (SvApp, SvCompForm, formNavTpl, formInfoTpl, SvCompControlButtons, SvCompOrderGeneralInfo, SvCompOrderGeneralInfoItems, SvCompOrderGeneralInfoMain, SvCompOrderGeneralInfoTotal) {
+define(['sv-app', 'sv-comp-form', 'text!sv-page-sales-orders-form-nav-tpl', 'text!sv-page-sales-orders-form-info-tpl', 'text!sv-page-sales-orders-form-control-buttons-tpl', 'text!sv-page-sales-orders-form-general-info-tpl', 'text!sv-page-sales-orders-form-general-info-items-tpl', 'text!sv-page-sales-orders-form-general-info-main-info-tpl', 'text!sv-page-sales-orders-form-general-info-total-tpl', 
+		'text!sv-page-sales-orders-form-comments-tpl', 
+		'text!sv-page-sales-orders-form-comments-sort-bar-tpl', 
+		'text!sv-page-sales-orders-form-comments-control-buttons-tpl', 
+		'text!sv-page-sales-orders-form-comments-comment-public-tpl', 
+		'text!sv-page-sales-orders-form-comments-comment-private-tpl', 
+		'text!sv-page-sales-orders-form-comments-comment-new-tpl'],
+	   function (SvApp, SvCompForm, formNavTpl, formInfoTpl, SvCompControlButtons, SvCompOrderGeneralInfo, SvCompOrderGeneralInfoItems, SvCompOrderGeneralInfoMain, SvCompOrderGeneralInfoTotal, 
+				  SvCompOrderComments, 
+				  SvCompOrderCommentSortBar, 
+				  SvCompOrderCommentControlButtons,
+				  SvCompOrderCommentPublic, 
+				  SvCompOrderCommentPrivate,
+				  SvCompOrderCommentNew) {
 
     const FormNav = {
         template: formNavTpl
@@ -34,16 +46,46 @@ define(['sv-app', 'sv-comp-form', 'text!sv-page-sales-orders-form-nav-tpl', 'tex
 		}
 	};
 	
+	var CommentSortBar = {
+		template: SvCompOrderCommentSortBar
+	};
 	
+	var CommentControlButtons = {
+		template: SvCompOrderCommentControlButtons
+	};
+	
+	var CommentPublic = {
+		template: SvCompOrderCommentPublic
+	};
 
-    return {
+	var CommentPrivate = {
+		template: SvCompOrderCommentPrivate
+	};
+
+	var CommentNew = {
+		template: SvCompOrderCommentNew
+	};
+
+	var OrderComments = {
+		template: SvCompOrderComments,
+		components: {
+			'sv-page-sales-orders-form-comments-sort-bar': CommentSortBar,
+			'sv-page-sales-orders-form-comments-control-buttons': CommentControlButtons,
+			'sv-page-sales-orders-form-comments-comment-public': CommentPublic,
+			'sv-page-sales-orders-form-comments-comment-private': CommentPrivate,
+			'sv-page-sales-orders-form-comments-comment-new': CommentNew,
+		}
+	};
+	
+	return {
         components: {
             'sv-comp-form': SvCompForm,
             'sv-page-sales-orders-form-nav': FormNav,
             'sv-page-sales-orders-form-info': FormInfo,
             'sv-page-sales-orders-form-control-buttons': ControlButtons,
             'sv-page-sales-orders-form-general-info': GeneralInfo,
-			
-        }
+			'sv-page-sales-orders-form-comments': OrderComments,
+		
+		}
     };
 });
