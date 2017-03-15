942a45dff02a6a8195d4111e524e287b04a6d4b3
nadiavasylenko <nv@doitua.com>
2017-03-15 19:44:11 +0200

    sn1 first commit

--- core/FCom/AdminSPA/AdminSPA/vue/page/dashboard.js
+++ core/FCom/AdminSPA/AdminSPA/vue/page/dashboard.js
@@ -2,12 +2,13 @@ define(['sv-hlp'], function (SvHlp) {
 
     return {
         store: SvHlp.store,
-        mounted: function () {
+		mounted: function () {
             this.$store.commit('setData', {curPage: {
                 link: '/',
                 label: 'Dashboard',
-                icon_class: 'fa fa-tachometer',
-                breadcrumbs: [
+              /*  icon_class: 'fa fa-tachometer', */
+				icon_link: '<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/icons.svg#icon-dashboard"></use>',
+				breadcrumbs: [
                 ]
             }});
         },
