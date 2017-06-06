fa032e44a4a246b7290975b20f1fe1de65a18102
Boris Gurvich <unirgy@gmail.com>
2017-06-06 07:17:05 -0700

    Added showing column options value in grid Fixed migration error

--- core/FCom/AdminSPA/AdminSPA/vue/component/grid/data/cell-default.js
+++ core/FCom/AdminSPA/AdminSPA/vue/component/grid/data/cell-default.js
@@ -1,6 +1,20 @@
 define([], function () {
     return {
         props: ['grid', 'row', 'col'],
-        template: '<td>{{row[col.field]}}</td>'
+        template: '<td>{{outputValue}}</td>',
+        computed: {
+            outputValue: function () {
+                var v = this.row[this.col.field], f;
+
+                if (this.grid.config.columns_by_name) {
+                    f = this.grid.config.columns_by_name[this.col.field];
+                    if (f && f.options && f.options[v]) {
+                        return f.options[v];
+                    }
+                }
+
+                return v;
+            }
+        }
     };
 });
