ef2632ed38a66cc59ee6f49fdd0b5e4158d63665
Boris Gurvich <unirgy@gmail.com>
2017-06-19 22:52:22 -0700

    Fixed CatalogFields migration; added responsive tabs js

--- core/FCom/AdminSPA/AdminSPA/vue/component/tabs.js
+++ core/FCom/AdminSPA/AdminSPA/vue/component/tabs.js
@@ -1,9 +1,9 @@
-define(['vue', 'sv-hlp', 'text!sv-comp-tabs-tpl'], function (Vue, SvHlp, tabsTpl) {
+define(['jquery', 'vue', 'sv-hlp', 'text!sv-comp-tabs-tpl'], function ($, Vue, SvHlp, tabsTpl) {
     var SvCompFormTabs = {
         mixins: [SvHlp.mixins.common],
         props: {
             'config': {type: Object},
-            'container-class': {type: String, default: 'f-tabs-container'},
+            'container-class': {type: String, default: ''},
             'tab': {type: [Object, Boolean]}
         },
         template: tabsTpl,
@@ -16,7 +16,89 @@ define(['vue', 'sv-hlp', 'text!sv-comp-tabs-tpl'], function (Vue, SvHlp, tabsTpl
             switchTab: function (tab) {
                 this.$emit('event', 'tab_switch', tab);
             }
+        },
+        mounted: function () {
+            var $menuWrapper = $(this.$refs.menuWrapper);
+            var $menu = $(this.$refs.menu);
+            var $item = $(this.$refs.item);
+            var $leftPaddle = $(this.$refs.leftPaddle);
+            var $rightPaddle = $(this.$refs.rightPaddle);
+
+            // duration of scroll animation
+            var scrollDuration = 300;
+// get items dimensions
+            var itemsLength = this.formTabs.length;
+            var itemSize = $item.outerWidth(true);
+// get some relevant size for the paddle triggering point
+            var paddleMargin = 20;
+
+// get wrapper width
+            var getMenuWrapperSize = function() {
+                return $menuWrapper.outerWidth();
+            };
+            var menuWrapperSize = getMenuWrapperSize();
+// the wrapper is responsive
+            $(window).on('resize', function() {
+                menuWrapperSize = getMenuWrapperSize();
+            });
+// size of the visible part of the menu is equal as the wrapper size
+            var menuVisibleSize = menuWrapperSize;
+
+// get total width of all menu items
+            var getMenuSize = function() {
+                return itemsLength * itemSize;
+            };
+            var menuSize = getMenuSize();
+// get how much of menu is invisible
+            var menuInvisibleSize = menuSize - menuWrapperSize;
+
+// get how much have we scrolled to the left
+            var getMenuPosition = function() {
+                return $menu.scrollLeft();
+            };
+
+            var onScroll = function() {
+                // get how much of menu is invisible
+                menuInvisibleSize = menuSize - menuWrapperSize;
+                // get how much have we scrolled so far
+                var menuPosition = getMenuPosition();
+
+                var menuEndOffset = menuInvisibleSize - paddleMargin;
+
+                // show & hide the paddles
+                // depending on scroll position
+                if (menuPosition <= paddleMargin) {
+                    $leftPaddle.addClass('hidden');
+                    $rightPaddle.removeClass('hidden');
+                } else if (menuPosition < menuEndOffset) {
+                    // show both paddles in the middle
+                    $leftPaddle.removeClass('hidden');
+                    $rightPaddle.removeClass('hidden');
+                } else if (menuPosition >= menuEndOffset) {
+                    $leftPaddle.removeClass('hidden');
+                    $rightPaddle.addClass('hidden');
+                }
+            };
+
+            onScroll();
+// finally, what happens when we are actually scrolling the menu
+            $menu.on('scroll', onScroll);
+
+// scroll to left
+            $rightPaddle.on('click', function() {
+                $menu.animate( { scrollLeft: menuInvisibleSize}, scrollDuration);
+            });
+
+// scroll to right
+            $leftPaddle.on('click', function() {
+                $menu.animate( { scrollLeft: '0' }, scrollDuration);
+            });
         }
+        // , watch: {
+        //     '$store.state.ui.windowWidth': function (width) {
+        //
+        //     }
+        // }
     };
 
     Vue.component('sv-comp-tabs', SvCompFormTabs);
