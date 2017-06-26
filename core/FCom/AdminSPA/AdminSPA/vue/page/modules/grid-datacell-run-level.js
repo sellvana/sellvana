5d0be9dc75d0adbaad57b317eb6f26f2327df742
nadiavasylenko <nv@doitua.com>
2017-06-26 13:32:35 +0300

    Run Level column data - fix toggler

--- core/FCom/AdminSPA/AdminSPA/vue/page/modules/grid-datacell-run-level.js
+++ core/FCom/AdminSPA/AdminSPA/vue/page/modules/grid-datacell-run-level.js
@@ -1,5 +1,6 @@
-define(['text!sv-page-modules-grid-datacell-run-level-tpl'], function (tpl) {
+define(['sv-hlp', 'text!sv-page-modules-grid-datacell-run-level-tpl'], function (SvHlp, tpl) {
     var Component = {
+        mixins: [SvHlp.mixins.formTab],
         props: ['grid', 'row', 'col'],
         template: tpl
     };
