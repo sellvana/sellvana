18f62a695f0a5449462ffa6731b8d7120c8def7a
Petar Dzhambazov <pdzhambazov@vayant.com>
2017-07-25 17:33:49 +0300

    separated html

--- core/Sellvana/Catalog/AdminSPA/vue/page/catalog/import-products/states/upload.js
+++ core/Sellvana/Catalog/AdminSPA/vue/page/catalog/import-products/states/upload.js
@@ -1,6 +1,30 @@
-define(['sv-mixin-common', 'text!sv-page-catalog-import-products-upload-tpl'], function (SvMixinCommon, tpl) {
+define(['sv-mixin-common', 'vue-dropzone', 'text!sv-page-catalog-import-products-upload-tpl'], function (SvMixinCommon, VueDropzone, tpl) {
+
     return {
         mixins: [SvMixinCommon],
+        data: function () {
+            return {
+                upload_url: '/catalog/products/import/config',
+                dropzone_options: {
+                    'acceptedFileTypes': 'text/txt,text/csv',
+                    'maxFileSizeInMB': 100,
+                    'autoProcessQueue': true,
+                    'headers': {'X-CSRF-TOKEN': this.$store.state.csrfToken}
+                }
+            }
+        },
+        components: {
+            dropzone: VueDropzone
+        },
+        methods: {
+
+            dropzoneSending: function (file, xhr, formData) {
+                console.log(file, xhr, formData);
+            },
+            dropzoneSuccess: function () {
+
+            }
+        },
         template: tpl
     }
 });
