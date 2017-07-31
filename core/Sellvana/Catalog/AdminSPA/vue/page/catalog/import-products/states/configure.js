22b9d56243961276f2c6a17e77708f1751c0c3a5
Petar Dzhambazov <jamby77@gmail.com>
2017-07-31 20:58:57 +0300

    implemented file config submit 2

--- core/Sellvana/Catalog/AdminSPA/vue/page/catalog/import-products/states/configure.js
+++ core/Sellvana/Catalog/AdminSPA/vue/page/catalog/import-products/states/configure.js
@@ -1,8 +1,24 @@
-define(['sv-mixin-common', 'text!sv-page-catalog-import-products-configure-tpl'], function (SvMixinCommon, tpl) {
+define(['lodash', 'sv-mixin-common', 'text!sv-page-catalog-import-products-configure-tpl'], function (_, SvMixinCommon, tpl) {
     return {
         data: function () {
             return {
-                config: {}
+                isLoaded: false,
+                isError: false,
+                error: '',
+                fileConfig: [
+                    {label: 'Field Delimiter', field: 'delim', 'default': ','},
+                    {label: 'Skip First Lines', field: 'skip_first', 'default': 1},
+                    {label: 'Batch Size', field: 'batch_size', 'default': 100},
+                    {label: 'Multi Value Delimiter', field: 'multivalue_separator', 'default': ';'},
+                    {label: 'Nesting Level Delimiter', field: 'nesting_separator', 'default': '>'}
+                ],
+                config: {
+                    'delim': ',',
+                    'skip_first': 1,
+                    'batch_size': 100,
+                    'multivalue_separator': ';',
+                    'nesting_separator': '>'
+                }
             }
         },
         mixins: [SvMixinCommon],
@@ -16,14 +32,26 @@ define(['sv-mixin-common', 'text!sv-page-catalog-import-products-configure-tpl']
         },
         template: tpl,
         mounted: function () {
-            this.sendRequest('GET', 'import-products/config', {file: this.file.file_name}, this.onConfig.bind(this), this.onConfigError.bind(this));
+            this.sendRequest('GET', 'import-products/config',
+                {file: this.file.file_name},
+                this.onConfig.bind(this),
+                this.onConfigError.bind(this)
+            );
         },
         methods: {
             onConfig: function (result) {
-                this.config = result;
+                this.isLoaded = true;
+                _.assign(this.config, result);
+                if (this.config.skip_first === true) {
+                    this.config.skip_first = 1;
+                }
+                if (this.config.skip_first === false) {
+                    this.config.skip_first = 0;
+                }
             },
             onConfigError: function (error) {
-                console.log('error', error);
+                this.error = error;
+                this.isError = true;
             }
         }
     }
