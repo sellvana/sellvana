0109c6e41d0cfa7642e404f310d9e6dcbf61de2d
Petar Dzhambazov <pdzhambazov@vayant.com>
2017-08-01 16:32:14 +0300

    status template updates

--- core/Sellvana/Catalog/AdminSPA/vue/page/catalog/import-products/states/status.js
+++ core/Sellvana/Catalog/AdminSPA/vue/page/catalog/import-products/states/status.js
@@ -1,6 +1,9 @@
 define(['sv-mixin-common', 'text!sv-page-catalog-import-products-status-tpl'], function (SvMixinCommon, tpl) {
     return {
         mixins: [SvMixinCommon],
+        props: {
+            c: Object
+        },
         template: tpl
     }
 });
